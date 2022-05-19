bf2e89a48f1981f9a931220ec9a221af4fee3db6
GutierrezGo <virginia@carforyou.ch>
2022-05-19 14:24:16 +0200

    fix: adjusted palette with the primary colors

--- src/theme/as24/colors.ts
+++ src/theme/as24/colors.ts
@@ -3,11 +3,13 @@ import { Colors } from '@chakra-ui/react';
 
 import { colors as sharedColors } from '../shared';
 
+const brandPrimary = '#F5F200';
+
 const colors: Colors = {
   ...sharedColors,
   brand: {
     50: '#FEFCD0',
-    100: '#F5F200',
+    100: brandPrimary,
     200: '#DCD500',
     300: '#C1B900',
     400: '#9E9600',
@@ -16,6 +18,7 @@ const colors: Colors = {
     700: '#665E00',
     800: '#4C4500',
     900: '#3A3400',
+    primary: brandPrimary,
   },
 };
 
--- src/theme/ms24/colors.ts
+++ src/theme/ms24/colors.ts
@@ -3,6 +3,8 @@ import { Colors } from '@chakra-ui/react';
 
 import { colors as sharedColors } from '../shared';
 
+const brandPrimary = '#FF4C52';
+
 const colors: Colors = {
   ...sharedColors,
   brand: {
@@ -10,12 +12,13 @@ const colors: Colors = {
     100: '#FFE4E5',
     200: '#FFC0C2',
     300: '#FD7978',
-    400: '#FF4C52',
+    400: brandPrimary,
     500: '#EF474D',
     600: '#D64045',
     700: '#A83236',
     800: '#7D2528',
     900: '#5F1C1E',
+    primary: brandPrimary,
   },
 };
 
