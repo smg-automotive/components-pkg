94f07891c8526eb7bbbd38d17a4c9c552b3b4a73
GutierrezGo <virginia@carforyou.ch>
2022-05-23 17:32:47 +0200

    feat: added border radius tokens

---
+++ src/theme/shared/borderRadius.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+const borderRadius = {
+  xs: '2px',
+  sm: '4px',
+  lg: '8px',
+  circle: '9999px',
+};
+
+export default borderRadius;
--- src/theme/shared/index.ts
+++ src/theme/shared/index.ts
@@ -1,2 +1,3 @@
 export { default as basis } from './basis';
 export { default as colors } from './colors';
+export { default as borderRadius } from './borderRadius';
