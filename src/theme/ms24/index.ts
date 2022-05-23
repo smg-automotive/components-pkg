c13490a3c7902c8703732895e17c40c57a69b3b4
GutierrezGo <virginia@carforyou.ch>
2022-05-23 16:17:35 +0200

    feat: added spacing tokens in REM

--- src/theme/ms24/index.ts
+++ src/theme/ms24/index.ts
@@ -1,11 +1,12 @@
 import { extendTheme } from '@chakra-ui/react';
 
 import colors from './colors';
-import { basis } from '../shared';
+import { basis, spacing } from '../shared';
 
 export const theme = {
   ...basis,
   colors,
+  spacing,
   name: 'MotoScout 24',
 };
 
