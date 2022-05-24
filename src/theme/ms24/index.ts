b01c3b250dc11953629364eea6aa51aa6883d026
Virginia <37380787+GutierrezGo@users.noreply.github.com>
2022-05-24 13:55:20 +0200

    feat: added border radius tokens (#36)

--- src/theme/ms24/index.ts
+++ src/theme/ms24/index.ts
@@ -1,11 +1,12 @@
 import { extendTheme } from '@chakra-ui/react';
 
 import colors from './colors';
-import { basis } from '../shared';
+import { basis, borderRadius } from '../shared';
 
 export const theme = {
   ...basis,
   colors,
+  borderRadius,
   name: 'MotoScout 24',
 };
 
