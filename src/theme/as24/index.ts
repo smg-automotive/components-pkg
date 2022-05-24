63dd1876a69e81d3c2ba295b1728e96093b38d5f
Virginia <37380787+GutierrezGo@users.noreply.github.com>
2022-05-24 17:11:28 +0200

    feat: added spacing tokens in REM (#35)

--- src/theme/as24/index.ts
+++ src/theme/as24/index.ts
@@ -1,11 +1,12 @@
 import { extendTheme } from '@chakra-ui/react';
 
 import colors from './colors';
-import { basis, borderRadius } from '../shared';
+import { basis, borderRadius, spacing } from '../shared';
 
 export const theme = {
   ...basis,
   colors,
+  spacing,
   borderRadius,
   name: 'AutoScout 24',
 };
