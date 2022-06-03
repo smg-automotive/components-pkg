5db86255eedee5c19f030689ebbd60206de8e11e
Quang Pham <quangphamit@gmail.com>
2022-06-03 11:23:33 +0700

    chore: merge with main

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,13 @@
-import React from 'react'
-import { addDecorator } from '@storybook/react'
-import { ThemeProvider as StorybookThemeProvider, ensure as ensureTheme } from '@storybook/theming';
-import { CSSReset, ThemeProvider } from '@chakra-ui/react'
-import { withThemes } from '@react-theming/storybook-addon'
-import { autoScout24Theme, motoScout24Theme } from '../src/theme'
-import breakpoints from '../src/theme/shared/breakpoints'
+import React from 'react';
+import { addDecorator } from '@storybook/react';
+import {
+  ThemeProvider as StorybookThemeProvider,
+  ensure as ensureTheme,
+} from '@storybook/theming';
+import { CSSReset, ThemeProvider } from '@chakra-ui/react';
+import { withThemes } from '@react-theming/storybook-addon';
+import { autoScout24Theme, motoScout24Theme } from '../src/theme';
+import breakpoints from '../src/theme/shared/breakpoints';
 
 const providerFn = ({ theme = autoScout24Theme, children }) => {
   return (
@@ -21,16 +24,19 @@ addDecorator(
   withThemes(null, [autoScout24Theme, motoScout24Theme], { providerFn })
 );
 
-const customViewports = Object.entries(breakpoints).reduce((acc, [key, value]) => {
-  acc[key] = {
-    name: key,
-    styles: {
-      width: value,
-      height: "100%"
-    }
-  }
-  return acc
-}, {})
+const customViewports = Object.entries(breakpoints).reduce(
+  (acc, [key, value]) => {
+    acc[key] = {
+      name: key,
+      styles: {
+        width: value,
+        height: '100%',
+      },
+    };
+    return acc;
+  },
+  {}
+);
 
 export const parameters = {
   controls: {
@@ -41,5 +47,5 @@ export const parameters = {
   },
   viewport: {
     viewports: customViewports,
-  }
-}
+  },
+};
