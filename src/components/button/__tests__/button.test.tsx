df87056991ee268db1f9a81b319484fb29712c21
Quang Pham <quangphamit@gmail.com>
2022-06-09 09:13:29 +0700

    refactor: clean test & stories

--- src/components/button/__tests__/button.test.tsx
+++ src/components/button/__tests__/button.test.tsx
@@ -6,18 +6,29 @@ import { expect } from '@storybook/jest';
 
 import Button from '../index';
 
+const renderWrapper = ({
+  onClick = jest.fn(),
+  isDisabled = false,
+  label = 'Button Label',
+} = {}) =>
+  render(
+    <Button onClick={onClick} isDisabled={isDisabled}>
+      {label}
+    </Button>
+  );
+
 describe('<Button>', () => {
   beforeEach(cleanup);
 
   it('should render button with label', () => {
-    render(<Button onClick={() => undefined}>Button Label</Button>);
+    renderWrapper({ label: 'Button Label' });
     const button = screen.getByRole('button', { name: 'Button Label' });
     expect(button).toBeInTheDocument();
   });
 
   it('should trigger onClick event when clicking on button', () => {
     const onClick = jest.fn();
-    render(<Button onClick={onClick}>Button Label</Button>);
+    renderWrapper({ label: 'Button Label', onClick });
 
     userEvent.click(screen.getByRole('button', { name: 'Button Label' }));
 
@@ -25,11 +36,7 @@ describe('<Button>', () => {
   });
 
   it('should add disabled attr when isDisabled was passed to component', () => {
-    render(
-      <Button onClick={() => undefined} isDisabled={true}>
-        Button Label
-      </Button>
-    );
+    renderWrapper({ label: 'Button Label', isDisabled: true });
     const button = screen.getByRole('button', { name: 'Button Label' });
 
     expect(button).toHaveAttribute('disabled');
