7138a07bebe974b1cde4efcd1f592c8d84e509d9
Krzysztof Sakwerda <ksakwerda@gmail.com>
2022-06-10 11:39:10 +0200

    fix: postinstall script syntax

--- src/lib/cli/setup/addPostinstallScript.ts
+++ src/lib/cli/setup/addPostinstallScript.ts
@@ -5,7 +5,7 @@ const { mergeJSONSync } = require('merge-json-file');
 export default (fontsRoot: string) => {
   const scriptEntry = {
     scripts: {
-      postinstall: `components copy-fonts -- --path=${fontsRoot}`,
+      postinstall: `components copy-fonts --path=${fontsRoot}`,
     },
   };
   console.info('adding script to package.json');
