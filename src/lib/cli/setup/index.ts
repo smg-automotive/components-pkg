a4d0868d081191326a7c39e7e19dff0131a80428
Krzysztof Sakwerda <ksakwerda@gmail.com>
2022-06-10 11:38:50 +0200

    fix: run copy-fonts on setup too

--- src/lib/cli/setup/index.ts
+++ src/lib/cli/setup/index.ts
@@ -1,8 +1,10 @@
 import appendGitignore from './appendGitignore';
 import addPostinstallScript from './addPostinstallScript';
 import { destination } from '../paths';
+import copyFonts from '../copy-fonts';
 
 export default ({ path: destinationRoot }: { path: string }) => {
   appendGitignore(destination(destinationRoot));
   addPostinstallScript(destinationRoot);
+  copyFonts({ path: destinationRoot });
 };
