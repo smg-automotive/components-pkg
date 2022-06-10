490d4f4f19bfeaca263a5bd36f8d77dcfcdf98ce
Krzysztof Sakwerda <ksakwerda@gmail.com>
2022-06-10 14:02:25 +0200

    fix: copy-fonts info message

--- src/lib/cli/copy-fonts.ts
+++ src/lib/cli/copy-fonts.ts
@@ -7,7 +7,7 @@ const fs = require('fs-extra');
 export default ({ path: destinationRoot }: { path: string }) => {
   const resolvedDestination = destination(destinationRoot);
 
-  console.info('copying fonts from to', resolvedDestination);
+  console.info('copying fonts from the package to', resolvedDestination);
   fs.copySync(source, resolvedDestination);
   console.info('fonts copied');
 };
