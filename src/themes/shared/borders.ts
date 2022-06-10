c33581c2ee012d8adff890675d8322e80909283a
Lorenzo Kappeler <me@lnk.codes>
2022-06-10 16:18:44 +0200

    fix(borders): Make config work with chakra

--- src/themes/shared/borders.ts
+++ src/themes/shared/borders.ts
@@ -1,5 +1,8 @@
+const defaultBorder = '1px solid';
+
 export const borders = {
   none: 0,
+  default: defaultBorder,
   // eslint-disable-next-line @typescript-eslint/naming-convention
-  'default': '1px solid',
+  '1px': defaultBorder,
 };
