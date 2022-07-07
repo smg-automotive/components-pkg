281eb070baf5ec23b8c03b1c3b43d6591a315f37
Krzysztof Sakwerda <ksakwerda@gmail.com>
2022-07-07 17:15:58 +0200

    feat: style form error message

--- src/themes/components/index.ts
+++ src/themes/components/index.ts
@@ -2,12 +2,14 @@ import Table from './table';
 import Link from './link';
 import Input from './input';
 import FormLabel from './formLabel';
+import FormError from './formError';
 import Form from './form';
 import Button from './button';
 
 export const components = {
   Button,
   Form,
+  FormError,
   FormLabel,
   Input,
   Link,
