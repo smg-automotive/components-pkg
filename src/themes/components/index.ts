cfe22084fa69ea9e5900ba4ba2e0696e9c23cf67
Krzysztof Sakwerda <ksakwerda@gmail.com>
2022-07-07 17:11:04 +0200

    feat: form hint styling

--- src/themes/components/index.ts
+++ src/themes/components/index.ts
@@ -2,10 +2,12 @@ import Table from './table';
 import Link from './link';
 import Input from './input';
 import FormLabel from './formLabel';
+import Form from './form';
 import Button from './button';
 
 export const components = {
   Button,
+  Form,
   FormLabel,
   Input,
   Link,
