863501207367bf57bf65f7a65d46c9ca67a92360
Krzysztof Sakwerda <ksakwerda@gmail.com>
2022-07-13 15:23:46 +0200

    feat: release Stack component & export FormControl (#115)
    fix: it works better when component is exported

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,5 @@ export { default as Button } from './button';
 export { default as Link } from './link';
 export { default as Input } from './input';
 export { default as Checkbox } from './checkbox';
+export { default as Stack } from './stack';
 export * from './icons';
