cb57026d3cb237ab8e15943a6d3cf717695c7426
Krzysztof Sakwerda <ksakwerda@gmail.com>
2022-07-13 16:42:21 +0200

    fix: export FormControl

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,9 @@
-export { default as ThemeProvider } from './themeProvider';
 export { default as Button } from './button';
-export { default as Link } from './link';
-export { default as Input } from './input';
 export { default as Checkbox } from './checkbox';
+export { default as FormControl } from './formControl';
+export { default as Input } from './input';
+export { default as Link } from './link';
 export { default as Stack } from './stack';
+export { default as ThemeProvider } from './themeProvider';
+
 export * from './icons';
