ae3486e2a789038a0ad9fd990ceb13463945ef8d
GutierrezGo <virginia@carforyou.ch>
2022-07-14 14:48:03 +0200

    fix: focused

--- src/themes/components/textarea.ts
+++ src/themes/components/textarea.ts
@@ -8,6 +8,11 @@ import Input from './input';
 const baseStyle: SystemStyleObject = {
   px: 'lg',
   pt: 'md',
+  outline: 0,
+  position: 'relative',
+  appearance: 'none',
+  transitionProperty: 'common',
+  transitionDuration: 'normal',
 };
 
 const variants: Record<string, SystemStyleInterpolation> = {
