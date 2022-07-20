7ea108ca4f9828830aa955be5efe612971a40f2d
GutierrezGo <virginia@carforyou.ch>
2022-07-20 10:20:35 +0200

    fix: review-K-1

--- src/components/section/index.tsx
+++ src/components/section/index.tsx
@@ -5,19 +5,20 @@ import { Box, Heading, Text } from '@chakra-ui/react';
 import Stack from '../stack';
 
 interface Props {
+  variant?: 'hero';
   title: string;
   text?: string;
-  image?: ReactNode;
+  image?: () => ReactNode;
 }
 
-const Section: FC<Props> = ({ title, text, image }) => {
+const Section: FC<Props> = ({ title, text, image, variant = 'hero' }) => {
   return (
     <Stack
       direction={{ xs: 'column', lg: 'row' }}
       spacing="xl"
       align={{ xs: 'center', lg: 'baseline' }}
     >
-      {image ? <Box>{image}</Box> : null}
+      {variant === 'hero' && image ? <Box>{image()}</Box> : null}
       <Stack spacing="md">
         <Heading textStyle="heading1">{title}</Heading>
         {text ? <Text textStyle="body-large">{text}</Text> : null}
