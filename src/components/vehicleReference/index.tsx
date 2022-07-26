b0ff785be4242ccfe07ab29513c09272f9ee4e66
GutierrezGo <virginia@carforyou.ch>
2022-07-26 18:49:13 +0200

    fix: removed extra box

--- src/components/vehicleReference/index.tsx
+++ src/components/vehicleReference/index.tsx
@@ -27,26 +27,19 @@ const VehicleReference: FC<Props> = ({
   const styles = useMultiStyleConfig(`VehicleReference`);
 
   return (
-    <Box maxW={{ xs: 'full', lg: '453px' }}>
-      <Stack direction={{ xs: 'row', lg: 'column' }} spacing="md">
-        <AspectRatio
-          minW="2xl"
-          ratio={4 / 3}
-          borderRadius="sm"
-          overflow="hidden"
-        >
-          {renderImage}
-        </AspectRatio>
-        <Stack spacing={{ xs: 'xs', lg: 'md' }} justify="center">
-          <chakra.h1 __css={styles.carTitle}>{carTitle}</chakra.h1>
-          <chakra.span __css={styles.price}>{price}</chakra.span>
-          <Box>
-            <chakra.p __css={styles.dealerName}>{dealerName}</chakra.p>
-            <chakra.p __css={styles.dealerAddress}>{dealerAddress}</chakra.p>
-          </Box>
-        </Stack>
+    <Stack direction={{ xs: 'row', lg: 'column' }} spacing="md">
+      <AspectRatio minW="2xl" ratio={4 / 3} borderRadius="sm" overflow="hidden">
+        {renderImage}
+      </AspectRatio>
+      <Stack spacing={{ xs: 'xs', lg: 'md' }} justify="center">
+        <chakra.h1 __css={styles.carTitle}>{carTitle}</chakra.h1>
+        <chakra.span __css={styles.price}>{price}</chakra.span>
+        <Box>
+          <chakra.p __css={styles.dealerName}>{dealerName}</chakra.p>
+          <chakra.p __css={styles.dealerAddress}>{dealerAddress}</chakra.p>
+        </Box>
       </Stack>
-    </Box>
+    </Stack>
   );
 };
 
