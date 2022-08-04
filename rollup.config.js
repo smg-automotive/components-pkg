d84dd5bc5eb57aa38c52010abc0396ba1e85b701
Krzysztof Sakwerda <ksakwerda@gmail.com>
2022-08-04 15:59:17 +0200

    chore(build): fail build if there are circular dependencies as it messes up treeshaking

--- rollup.config.js
+++ rollup.config.js
@@ -14,6 +14,10 @@ const external = [
   ...Object.keys(packageJson.dependencies || {}),
   ...Object.keys(packageJson.peerDependencies || {}),
 ];
+const onwarn = (warning, warn) => {
+  if (warning.code === 'CIRCULAR_DEPENDENCY') throw new Error(warning.message);
+  warn(warning);
+};
 
 export default [
   {
@@ -40,6 +44,7 @@ export default [
       }),
     ],
     external,
+    onwarn,
   },
   {
     input: 'src/index.ts',
@@ -75,6 +80,7 @@ export default [
       }),
     ],
     external,
+    onwarn,
   },
   {
     input: 'dist/esm/types/index.d.ts',
@@ -101,5 +107,6 @@ export default [
       }),
       executable(),
     ],
+    onwarn,
   },
 ];
