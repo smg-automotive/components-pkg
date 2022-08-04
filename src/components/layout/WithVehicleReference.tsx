29ac86acde005d93a08cac4aa258c1eeb57fe8cb
talamcol <talamcol>
2022-08-04 10:59:13 +0200

    chore: extract base layout component with spacing

--- src/components/layout/WithVehicleReference.tsx
+++ src/components/layout/WithVehicleReference.tsx
@@ -1,14 +1,8 @@
 import React, { FC, PropsWithChildren } from 'react';
 
-import {
-  Center,
-  chakra,
-  Container,
-  Grid,
-  GridItem,
-  Heading,
-} from '@chakra-ui/react';
+import { GridItem, Heading } from '@chakra-ui/react';
 
+import BaseGridLayout, { repeatArea } from './BaseGrid';
 import VehicleReference, { VehicleProps } from '../vehicleReference';
 import Stack from '../stack';
 import Link from '../link';
@@ -29,51 +23,35 @@ const LayoutWithVehicleReference: FC<PropsWithChildren<Props>> = ({
   vehicle,
   children,
 }) => {
-  const repeatArea = (count: number, area: string) => {
-    return new Array(count).fill(area).join(' ');
-  };
-
   return (
-    <Center>
-      <Container
-        as="main"
-        width="full"
-        maxWidth="container.xl"
-        paddingY={{ xs: 'xl', lg: '2xl' }}
-        paddingX={{ xs: 'md', lg: '4xl' }}
-      >
-        <Grid
-          templateAreas={{
-            xs: `"backlink" "vehicle" "main"`,
-            lg: `
+    <BaseGridLayout
+      templateAreas={{
+        xs: `"backlink" "vehicle" "main"`,
+        lg: `
             "${repeatArea(12, 'backlink')}"
             "${repeatArea(6, 'main')} . ${repeatArea(5, 'vehicle')}"`,
-          }}
-          gridTemplateColumns={{ lg: 'repeat(12, 1fr)' }}
-          gridTemplateRows="minmax(min-content, max-content) 1fr"
-          gap="xl"
-        >
-          <GridItem area="backlink">
-            {backLink ? (
-              <Link href={backLink.url} leftIcon={<ArrowLeftIcon />}>
-                {backLink.text}
-              </Link>
-            ) : null}
-          </GridItem>
-          <GridItem area="vehicle">
-            <VehicleReference {...vehicle} />
-          </GridItem>
-          <GridItem area="main">
-            <Heading as="h1" textStyle="heading1" marginBottom="xl">
-              {title}
-            </Heading>
-            <Stack direction="column" spacing="2xl">
-              {children}
-            </Stack>
-          </GridItem>
-        </Grid>
-      </Container>
-    </Center>
+      }}
+      gridTemplateRows="minmax(min-content, max-content) 1fr"
+    >
+      <GridItem area="backlink">
+        {backLink ? (
+          <Link href={backLink.url} leftIcon={<ArrowLeftIcon />}>
+            {backLink.text}
+          </Link>
+        ) : null}
+      </GridItem>
+      <GridItem area="vehicle">
+        <VehicleReference {...vehicle} />
+      </GridItem>
+      <GridItem area="main">
+        <Heading as="h1" textStyle="heading1" marginBottom="xl">
+          {title}
+        </Heading>
+        <Stack direction="column" spacing="2xl">
+          {children}
+        </Stack>
+      </GridItem>
+    </BaseGridLayout>
   );
 };
 
