733b3384534eaa1886f8fc1d98e29b690e6bbe13
talamcol <talamcol>
2022-08-08 14:17:29 +0200

    feat: create component

---
+++ src/select/index.tsx
@@ -0,0 +1,37 @@
+import React, { FC } from 'react';
+import { Select as ChakraSelect, SelectProps } from '@chakra-ui/react';
+
+type Option = {
+  value: string | number;
+  option: string;
+};
+
+type Props = Pick<
+  SelectProps,
+  | 'placeholder'
+  | 'isInvalid'
+  | 'isDisabled'
+  | 'onBlur'
+  | 'onFocus'
+  | 'onChange'
+  | 'autoFocus'
+> & {
+  size?: 'md' | 'lg';
+  value?: string | number;
+  name: string;
+  options: Option[];
+};
+
+const Select: FC<Props> = (props) => {
+  return (
+    <ChakraSelect {...props}>
+      {props.options.map((option) => (
+        <option value={option.value} key={option.value}>
+          {option.option}
+        </option>
+      ))}
+    </ChakraSelect>
+  );
+};
+
+export default Select;
