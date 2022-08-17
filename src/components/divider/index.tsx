7fddfadc87f089e7fa9b110d70a06841288cccf3
GutierrezGo <virginia@carforyou.ch>
2022-08-17 13:55:36 +0200

    feat: added divider component

---
+++ src/components/divider/index.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import { Divider as ChakraDivider } from '@chakra-ui/react';
+
+const Divider: FC = () => {
+  return <ChakraDivider />;
+};
+
+export default Divider;
---
+++ src/themes/components/divider.ts
@@ -0,0 +1,11 @@
+import type { SystemStyleObject } from '@chakra-ui/styled-system';
+
+const baseStyle: SystemStyleObject = {
+  borderColor: 'gray.100',
+  borderStyle: 'solid',
+  borderWidth: '1px',
+};
+
+export default {
+  baseStyle,
+};
