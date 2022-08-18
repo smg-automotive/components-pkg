47518c26f3fd60a0b43fe5749b8f5baf5bc00efd
Krzysztof Sakwerda <ksakwerda@gmail.com>
2022-08-18 15:30:05 +0200

    chore: parseFloat

--- src/themes/shared/breakpoints.ts
+++ src/themes/shared/breakpoints.ts
@@ -22,8 +22,8 @@ export const breakpoints = Object.fromEntries(
   Object.keys(emBreakpoints).map((name) => [
     name,
     {
-      em: parseInt(emBreakpoints[name as BreakpointName], 10),
-      px: parseInt(pxBreakpoints[name as BreakpointName], 10),
+      em: parseFloat(emBreakpoints[name as BreakpointName], 10),
+      px: parseFloat(pxBreakpoints[name as BreakpointName], 10),
     },
   ])
 ) as Record<
