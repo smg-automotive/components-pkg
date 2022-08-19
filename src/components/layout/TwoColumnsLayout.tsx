88969907d1dc365804c1213f3f145eb554735049
GutierrezGo <virginia@carforyou.ch>
2022-08-19 11:24:31 +0200

    fix: extracted baseLayout

--- src/components/layout/TwoColumnsLayout.tsx
+++ src/components/layout/TwoColumnsLayout.tsx
@@ -2,10 +2,10 @@ import React, { FC, ReactNode } from 'react';
 
 import { GridItem, Heading } from '@chakra-ui/react';
 
+import BaseLayout from './BaseLayout';
 import BaseGridLayout, { repeatArea } from './BaseGrid';
 import Link from '../link';
 import { ArrowLeftIcon } from '../icons';
-import Divider from '../divider';
 
 interface Props {
   header: ReactNode;
@@ -26,45 +26,42 @@ const TwoColumnsLayout: FC<Props> = ({
   rightContent,
 }) => {
   return (
-    <BaseGridLayout
-      templateAreas={{
-        xs: `"header" "backlink" "heading" "rightContent" "leftContent"`,
-        lg: `
-            "${repeatArea(12, 'header')}"
+    <BaseLayout header={header} footer={null}>
+      <BaseGridLayout
+        templateAreas={{
+          xs: `"backlink" "heading" "rightContent" "leftContent"`,
+          lg: `
             "${repeatArea(12, 'backlink')}"
             "${repeatArea(6, 'heading')} . ${repeatArea(5, 'rightContent')}"
             "${repeatArea(6, 'leftContent')} . ${repeatArea(
-          5,
-          'rightContent'
-        )}"`,
-      }}
-      gridTemplateRows="minmax(min-content, max-content) minmax(min-content, max-content) minmax(min-content, max-content) 1fr"
-    >
-      <GridItem area="header">
-        {header}
-        <Divider />
-      </GridItem>
-      {backLink ? (
-        <GridItem area="backlink">
-          <Link href={backLink.url} leftIcon={<ArrowLeftIcon />}>
-            {backLink.text}
-          </Link>
-        </GridItem>
-      ) : null}
-      {title ? (
-        <GridItem area="heading">
-          <Heading as="h1" textStyle="heading1">
-            {title}
-          </Heading>
-        </GridItem>
-      ) : null}
-      {rightContent ? (
-        <GridItem area="rightContent">{rightContent}</GridItem>
-      ) : null}
-      {leftContent ? (
-        <GridItem area="leftContent">{leftContent}</GridItem>
-      ) : null}
-    </BaseGridLayout>
+            5,
+            'rightContent'
+          )}"`,
+        }}
+        gridTemplateRows="minmax(min-content, max-content) minmax(min-content, max-content) 1fr"
+      >
+        {backLink ? (
+          <GridItem area="backlink">
+            <Link href={backLink.url} leftIcon={<ArrowLeftIcon />}>
+              {backLink.text}
+            </Link>
+          </GridItem>
+        ) : null}
+        {title ? (
+          <GridItem area="heading">
+            <Heading as="h1" textStyle="heading1">
+              {title}
+            </Heading>
+          </GridItem>
+        ) : null}
+        {rightContent ? (
+          <GridItem area="rightContent">{rightContent}</GridItem>
+        ) : null}
+        {leftContent ? (
+          <GridItem area="leftContent">{leftContent}</GridItem>
+        ) : null}
+      </BaseGridLayout>
+    </BaseLayout>
   );
 };
 
--- src/components/simpleHeader/index.tsx
+++ src/components/simpleHeader/index.tsx
@@ -1,6 +1,13 @@
 import React, { FC } from 'react';
 
-import { chakra, Flex, useMultiStyleConfig } from '@chakra-ui/react';
+import {
+  Center,
+  chakra,
+  Container,
+  Flex,
+  Grid,
+  useMultiStyleConfig,
+} from '@chakra-ui/react';
 
 import { CloseIcon } from '../icons';
 
@@ -16,19 +23,33 @@ const SimpleHeader: FC<Props> = ({ title, url }) => {
 
   return (
     <Component>
-      <Flex
-        minH={{ xs: 'xl', lg: '2xl' }}
-        paddingY="xl"
-        align="center"
-        justify="space-between"
-      >
-        <chakra.h1 __css={styles.title}>{title}</chakra.h1>
-        <Flex alignSelf="baseline" paddingLeft="md" paddingTop={{ lg: 'md' }}>
-          <a href={url}>
-            <CloseIcon />
-          </a>
-        </Flex>
-      </Flex>
+      <Center>
+        <Container
+          width="full"
+          maxWidth="container.xl"
+          minHeight={{ xs: 'xl', lg: '2xl' }}
+          paddingX={{ xs: 'md', lg: '4xl' }}
+          paddingY="xl"
+        >
+          <Grid
+            gridTemplateColumns="minmax(min-content, max-content) minmax(min-content, max-content) 1fr"
+            gap="xl"
+          >
+            <Flex justify="space-between" alignContent="space-between">
+              <chakra.h1 __css={styles.title}>{title}</chakra.h1>
+              <Flex
+                alignSelf="baseline"
+                paddingLeft="md"
+                paddingTop={{ lg: 'sm' }}
+              >
+                <a href={url}>
+                  <CloseIcon />
+                </a>
+              </Flex>
+            </Flex>
+          </Grid>
+        </Container>
+      </Center>
     </Component>
   );
 };
