d7b07d40bffaccb565ef4ada8dd132bbb2971589
talamcol <talamcol>
2022-09-05 17:04:56 +0200

    feat: message success icon and icon overview story

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -2,3 +2,4 @@ export { ArrowLeftIcon } from './ArrowLeftIcon';
 export { ChevronIcon } from './ChevronIcon';
 export { HeartIcon } from './HeartIcon';
 export { CloseIcon } from './CloseIcon';
+export { MessageSuccessIcon } from './MessageSuccessIcon';
