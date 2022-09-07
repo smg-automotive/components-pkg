21fcddd16b512a4aa036dc8364ad7bc3b02a60d2
talamcol <talamcol>
2022-09-07 13:37:33 +0200

    chore: remove divider spacing again

--- src/components/divider/index.tsx
+++ src/components/divider/index.tsx
@@ -1,14 +1,8 @@
 import React, { FC } from 'react';
-import { Divider as ChakraDivider, ResponsiveValue } from '@chakra-ui/react';
+import { Divider as ChakraDivider } from '@chakra-ui/react';
 
-import { Sizes } from '../../themes';
-
-type Props = {
-  verticalSpacing?: ResponsiveValue<Sizes>;
-};
-
-const Divider: FC<Props> = ({ verticalSpacing }) => {
-  return <ChakraDivider my={verticalSpacing} />;
+const Divider: FC = () => {
+  return <ChakraDivider />;
 };
 
 export default Divider;
