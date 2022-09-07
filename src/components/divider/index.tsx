20b528a5c735c549772de96b07a8b192e0e5e9ee
talamcol <talamcol>
2022-09-07 09:58:20 +0200

    fix: typecheck

--- src/components/divider/index.tsx
+++ src/components/divider/index.tsx
@@ -3,7 +3,7 @@ import { Divider as ChakraDivider, ResponsiveValue } from '@chakra-ui/react';
 import { Sizes } from '../../themes';
 
 type Props = {
-  verticalSpacing: ResponsiveValue<Sizes>;
+  verticalSpacing?: ResponsiveValue<Sizes>;
 };
 
 const Divider: FC<Props> = ({ verticalSpacing }) => {
