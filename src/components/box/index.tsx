03f66e971fdd566de2afab2cdbdfa653c7642ce7
Marine-Berthier <marine154@hotmail.fr>
2022-09-12 11:48:29 +0200

    fix: make it more generic

--- src/components/box/index.tsx
+++ src/components/box/index.tsx
@@ -1,25 +1,2 @@
-import React, { FC } from 'react';
-import { Box as ChakraBox, BoxProps as ChakraBoxProps } from '@chakra-ui/react';
-
-const Box: FC<ChakraBoxProps> = ({
-  children,
-  background,
-  borderRadius = 'sm',
-  color,
-  padding,
-  ...rest
-}) => {
-  return (
-    <ChakraBox
-      bg={background}
-      color={color}
-      borderRadius={borderRadius}
-      padding={padding}
-      {...rest}
-    >
-      {children}
-    </ChakraBox>
-  );
-};
-
+import { Box } from '@chakra-ui/react';
 export default Box;
