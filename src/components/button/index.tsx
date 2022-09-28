dae96e92d8032ee269d8b102bffef8f702e689ff
GutierrezGo <virginia@carforyou.ch>
2022-09-28 17:37:55 +0200

    fix: added buttonLink as a separate component

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -4,14 +4,14 @@ import {
   ButtonProps as ChakraButtonProps,
 } from '@chakra-ui/react';
 
+type WithOnClick<T extends 'submit' | 'button'> = T extends 'submit'
+  ? Pick<ChakraButtonProps, 'onClick'>
+  : { onClick: Exclude<ChakraButtonProps['onClick'], undefined> };
+
 type SharedProps = {
   variant?: 'primary' | 'secondary';
   size?: 'md' | 'lg';
 } & Pick<ChakraButtonProps, 'isDisabled' | 'children'>;
-
-type WithOnClick<T extends 'submit' | 'button'> = T extends 'submit'
-  ? Pick<ChakraButtonProps, 'onClick'>
-  : { onClick: Exclude<ChakraButtonProps['onClick'], undefined> };
 
 type ButtonTypeProps = SharedProps & {
   type?: 'button';
@@ -31,17 +31,10 @@ const Button: FC<ButtonProps> = ({
   isDisabled = false,
   children,
   ...rest
-}) => {
-  return (
-    <ChakraButton
-      isDisabled={isDisabled}
-      variant={variant}
-      size={size}
-      {...rest}
-    >
-      {children}
-    </ChakraButton>
-  );
-};
+}) => (
+  <ChakraButton isDisabled={isDisabled} variant={variant} size={size} {...rest}>
+    {children}
+  </ChakraButton>
+);
 
 export default Button;
--- src/components/buttonLink/index.tsx
+++ src/components/buttonLink/index.tsx
@@ -8,6 +8,8 @@ type SharedProps = {
   variant?: 'primary' | 'secondary';
   size?: 'md' | 'lg';
   url: string;
+  isExternal?: boolean;
+  rel?: string;
 } & Pick<ChakraButtonProps, 'children'>;
 
 export type ButtonProps = SharedProps;
@@ -15,14 +17,21 @@ export type ButtonProps = SharedProps;
 const Button: FC<ButtonProps> = ({
   variant = 'primary',
   size = 'lg',
-  children,
   url,
-}) => {
-  return (
-    <ChakraButton as="a" href={url} variant={variant} size={size}>
-      {children}
-    </ChakraButton>
-  );
-};
+  isExternal,
+  rel,
+  children,
+}) => (
+  <ChakraButton
+    as="a"
+    href={url}
+    variant={variant}
+    size={size}
+    target={isExternal ? '_blank' : undefined}
+    rel={rel || isExternal ? 'noopener noreferrer' : undefined}
+  >
+    {children}
+  </ChakraButton>
+);
 
 export default Button;
