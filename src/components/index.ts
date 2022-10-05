76802dc33871245b3ba97dfd1c873acfdfecf0ad
Krzysztof Sakwerda <ksakwerda@gmail.com>
2022-10-05 13:08:46 +0200

    fix: export datepicker

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,6 +4,7 @@ export { default as Button } from './button';
 export { default as Box } from './box';
 export { default as Center } from './center';
 export { default as Checkbox } from './checkbox';
+export { default as DatePicker } from './datePicker';
 export { default as DevOverlay } from './devOverlay';
 export { default as Divider } from './divider';
 export { default as Flex } from './flex';
