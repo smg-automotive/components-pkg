dbccc18565f5c37a620728b363bd5017167d4ee2
talamcol <talamcol>
2022-10-19 14:56:11 +0200

    feat: missing image component

--- src/components/vehicleReference/index.tsx
+++ src/components/vehicleReference/index.tsx
@@ -4,8 +4,8 @@ import { Box, chakra, useMultiStyleConfig } from '@chakra-ui/react';
 
 import Stack from '../stack';
 
+import MissingImage from '../missingImage';
 import AspectRatio from '../aspectRatio';
-import missingImage from '../../assets/images/missingImage.png';
 
 interface Props {
   image?: ReactNode;
@@ -33,11 +33,7 @@ const VehicleReference: FC<Props> = ({
           borderRadius="sm"
           overflow="hidden"
         >
-          {image ? (
-            image
-          ) : (
-            <img data-testid="missing-image" src={missingImage} />
-          )}
+          {image ? image : <MissingImage />}
         </AspectRatio>
         <Stack spacing={{ '2xs': 'xs', md: 'md' }} justify="center">
           <chakra.h1 __css={styles.carTitle}>{vehicleTitle}</chakra.h1>
