c2d7c1be5f70d4a2c69c873300c1c3306edc6425
Krzysztof Sakwerda <ksakwerda@gmail.com>
2022-10-19 11:08:23 +0200

    chore: update dts plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,14 @@
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import image from 'rollup-plugin-img';
 import executable from 'rollup-plugin-executable';
-import dts from 'rollup-plugin-dts';
 import copy from 'rollup-plugin-copy';
 import shebang from 'rollup-plugin-add-shebang';
 import { dirname, join } from 'path';
 import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+
+const dts = require('rollup-plugin-dts').default;
 
 const packageJson = require('./package.json');
 const external = [
