9eb13c2f881af642984e7efb1dcf979c6b8b0a54
Krzysztof Sakwerda <ksakwerda@gmail.com>
2022-10-28 14:23:22 +0200

    feat: whitelist alignItems

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -3,7 +3,13 @@ import { SimpleGrid, SimpleGridProps } from '@chakra-ui/react';
 
 type Props = Pick<
   SimpleGridProps,
-  'minChildWidth' | 'columns' | 'spacing' | 'spacingX' | 'spacingY' | 'children'
+  | 'minChildWidth'
+  | 'columns'
+  | 'spacing'
+  | 'spacingX'
+  | 'spacingY'
+  | 'children'
+  | 'alignItems'
 >;
 
 const Grid: FC<Props> = (props) => <SimpleGrid {...props} />;
