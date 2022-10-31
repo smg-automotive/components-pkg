552c289ab9742deeb9621cd56934777e2800802c
Lorenzo Kappeler <me@lnk.codes>
2022-10-31 09:46:51 +0100

    feat(RadioList): Create a list view for radio buttons

---
+++ src/components/radio/RadioListItem.tsx
@@ -0,0 +1,33 @@
+import React, { FC, PropsWithChildren } from 'react';
+import { useRadio, UseRadioProps } from '@chakra-ui/react';
+
+import { Box } from '../index';
+
+export const RadioListItem: FC<PropsWithChildren<UseRadioProps>> = (props) => {
+  const { getInputProps, getCheckboxProps } = useRadio(props);
+
+  const input = getInputProps();
+  const checkbox = getCheckboxProps();
+
+  return (
+    <Box as="label" width="full">
+      <input {...input} />
+      <Box
+        {...checkbox}
+        cursor="pointer"
+        background="white"
+        borderRadius="md"
+        _checked={{
+          bg: 'blue.100',
+        }}
+        _hover={{
+          bg: 'blue.100',
+        }}
+        px="lg"
+        py="sm"
+      >
+        {props.children}
+      </Box>
+    </Box>
+  );
+};
