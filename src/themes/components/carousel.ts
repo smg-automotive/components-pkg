aabdce8d0dd083be1fe71f51cd335337a76330f2
Krzysztof Sakwerda <ksakwerda@gmail.com>
2022-11-01 09:53:33 +0100

    fix: missing style

---
+++ src/themes/components/carousel.ts
@@ -0,0 +1,84 @@
+import { ComponentStyleConfig } from '@chakra-ui/react';
+
+const parts = [
+  'container',
+  'slide',
+  'slideContainer',
+  'buttonContainer',
+  'button',
+  'icon',
+];
+
+const fullScreen = {
+  container: {
+    height: '100%',
+    paddingX: '5xl',
+    backgroundColor: 'black',
+  },
+  buttonContainer: {
+    backgroundColor: 'black',
+    width: 'xl',
+    visibility: {
+      md: 'visible',
+    },
+  },
+  button: {
+    opacity: '100%',
+    backgroundColor: 'black',
+  },
+  icon: {
+    boxSize: 'md',
+  },
+};
+
+const Carousel: ComponentStyleConfig = {
+  parts,
+  baseStyle: {
+    container: {
+      overflow: 'hidden',
+      position: 'relative',
+    },
+    slide: {
+      flexGrow: '0',
+      flexShrink: '0',
+      flexBasis: 'full',
+    },
+    slideContainer: {
+      height: 'full',
+      alignItems: 'center',
+      display: 'flex',
+    },
+    buttonContainer: {
+      position: 'absolute',
+      top: '0',
+      width: 'lg',
+      height: 'full',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      color: 'white',
+      visibility: 'hidden',
+      _groupHover: { md: { visibility: 'visible' } },
+    },
+    button: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: 'md',
+      height: 'md',
+      borderRadius: 'sm',
+      backgroundColor: 'gray.900',
+      color: 'white',
+      opacity: '40%',
+      _hover: { opacity: '80%' },
+    },
+    icon: {
+      boxSize: 'sm',
+    },
+  },
+  variants: {
+    fullScreen,
+  },
+};
+
+export default Carousel;
