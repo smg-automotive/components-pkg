9885dc849a45e90af0db37bda8bcbff8932ad397
Lorenzo Kappeler <lorenzo@carforyou.ch>
2022-11-02 13:47:38 +0100

    feat(grid) Export grid component (#319)
    * feat(grid) Export grid component
    
    * chore: replace grid component in error page
    
    * feat: allow gridTemplateRows
    
    * chore: type gap options
    
    * feat: Use grid in BaseLayout component
    
    BREAKING CHANGE: export SimpleGrid component as SimpleGrid and not as Grid

---
+++ src/components/simpleGrid/index.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+import {
+  SimpleGrid as ChakraSimpleGrid,
+  SimpleGridProps,
+} from '@chakra-ui/react';
+
+type Props = Pick<
+  SimpleGridProps,
+  'minChildWidth' | 'columns' | 'spacing' | 'spacingX' | 'spacingY' | 'children'
+>;
+
+const SimpleGrid: FC<Props> = (props) => <ChakraSimpleGrid {...props} />;
+
+export default SimpleGrid;
+export { Props as GridProps };
