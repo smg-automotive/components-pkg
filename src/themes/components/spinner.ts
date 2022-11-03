2a3c83c1e91518c977c7446fa5565e7e3f740fde
Lorenzo Kappeler <lorenzo@carforyou.ch>
2022-11-03 17:08:38 +0100

    feat(Spinner): Add spinner component (#324)
    * feat(Spinner): Add spinner component
    
    * chore: set default size to sm
    
    * chore: Add sizes to storybook

---
+++ src/themes/components/spinner.ts
@@ -0,0 +1,34 @@
+import { cssVar } from '@chakra-ui/theme-tools';
+import { defineStyle, defineStyleConfig } from '@chakra-ui/styled-system';
+
+const $size = cssVar('spinner-size');
+
+const baseStyle = defineStyle({
+  width: [$size.reference],
+  height: [$size.reference],
+});
+
+const sizes = {
+  xs: defineStyle({
+    [$size.variable]: 'sizes.xs',
+  }),
+  sm: defineStyle({
+    [$size.variable]: 'sizes.sm',
+  }),
+  md: defineStyle({
+    [$size.variable]: 'sizes.md',
+  }),
+  lg: defineStyle({
+    [$size.variable]: 'sizes.lg',
+  }),
+};
+
+const spinnerTheme = defineStyleConfig({
+  baseStyle,
+  sizes,
+  defaultProps: {
+    size: 'sm',
+  },
+});
+
+export default spinnerTheme;
