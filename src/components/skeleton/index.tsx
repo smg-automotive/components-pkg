4191e2f228356951ffad9f5f6557c1af86cca208
Lorenzo Kappeler <me@lnk.codes>
2022-11-04 13:56:59 +0100

    fix: component export

--- src/components/skeleton/index.tsx
+++ src/components/skeleton/index.tsx
@@ -1,2 +1,8 @@
-import { Skeleton } from '@chakra-ui/react';
+import React, { FC } from 'react';
+import { Skeleton as ChakraSkeleton, SkeletonProps } from '@chakra-ui/react';
+
+const Skeleton: FC<SkeletonProps> = ({ children, ...props }) => {
+  return <ChakraSkeleton {...props}>{children}</ChakraSkeleton>;
+};
+
 export default Skeleton;
