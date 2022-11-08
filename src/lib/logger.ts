6a84797cdf767f1a01ed83d94bfac5a4bdda89fb
talamcol <talamcol>
2022-11-08 16:59:19 +0100

    chore: review - satisfy linter

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,6 +1,5 @@
 class Logger {
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  onMissingTranslation: (error: Error) => void = () => {};
+  onMissingTranslation: (error: Error) => void = () => null;
 }
 
 const logger = new Logger();
