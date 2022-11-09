e104bf9aa7835ff6520d3ae51b1d498001060563
Lorenzo Kappeler <me@lnk.codes>
2022-11-09 16:40:06 +0100

    feat(Inpupt) debounce handling

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,43 +1,62 @@
+import { useDebouncedCallback } from 'use-debounce';
 import React, {
   ChangeEventHandler,
   FocusEventHandler,
   forwardRef,
+  useEffect,
+  useState,
 } from 'react';
 import { Input as ChakraInput } from '@chakra-ui/react';
 
-import { useDebouncedOnChange } from '../hooks';
-
-type Props = {
+type SharedProps = {
   placeholder?: string;
   isInvalid?: boolean;
   isDisabled?: boolean;
   size?: 'md' | 'lg';
   onBlur?: FocusEventHandler<HTMLInputElement>;
   onFocus?: FocusEventHandler<HTMLInputElement>;
-  onChange?: ChangeEventHandler<HTMLInputElement>;
   value?: string | number;
   autoFocus?: boolean;
   name: string;
-  debounce?: number;
+  debounce?: boolean;
 };
 
-const Input = forwardRef<HTMLInputElement, Props>(
-  ({ onChange, onBlur, debounce, ...props }, ref) => {
-    const { onBlur: onBlurHandler, onChange: onChangeHandler } =
-      useDebouncedOnChange({
-        onBlur,
-        onChange,
-        debounce,
-      });
+type InputPros = {
+  onChange?: ChangeEventHandler<HTMLInputElement>;
+  setInputValue: never;
+} & SharedProps;
 
-    return (
-      <ChakraInput
-        {...props}
-        onChange={onChangeHandler}
-        onBlur={onBlurHandler}
-        ref={ref}
-      />
-    );
+type DebouncedInputPros = {
+  onChange?: never;
+  setInputValue: (value: string) => void;
+} & SharedProps;
+
+type Props = InputPros | DebouncedInputPros;
+
+const Input = forwardRef<HTMLInputElement, Props>(
+  ({ onChange, value, debounce = false, setInputValue, ...props }, ref) => {
+    const [internalUIValue, setInternalUIValue] = useState(value);
+    // https://lawsofux.com/doherty-threshold/
+    const debounceThreshold = debounce ? 400 : 0;
+
+    const setValueDebounced = useDebouncedCallback((newValue) => {
+      setInputValue(newValue);
+    }, debounceThreshold);
+
+    useEffect(() => {
+      if (internalUIValue !== value && !setValueDebounced.isPending()) {
+        setInternalUIValue(value);
+      }
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [value]);
+
+    const onChangeHandler: ChangeEventHandler<HTMLInputElement> = (event) => {
+      const targetValue = event.target.value;
+      setInternalUIValue(targetValue);
+      setValueDebounced(targetValue);
+    };
+
+    return <ChakraInput {...props} onChange={onChangeHandler} ref={ref} />;
   }
 );
 Input.displayName = 'Input';
