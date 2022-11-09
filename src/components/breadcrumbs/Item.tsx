925e5d588f642cd2b5845aea4495eeef72067a02
Krzysztof Sakwerda <ksakwerda@gmail.com>
2022-11-09 14:30:26 +0100

    chore: export chakra item directly

--- src/components/breadcrumbs/Item.tsx
+++ src/components/breadcrumbs/Item.tsx
@@ -1,15 +1,3 @@
-import React, { FC, PropsWithChildren } from 'react';
-
-import {
-  BreadcrumbItem as ChakraBreadcrumbItem,
-  BreadcrumbItemProps as ChakraBreadcrumbItemProps,
-} from '@chakra-ui/react';
-
-const BreadcrumbItem: FC<PropsWithChildren<ChakraBreadcrumbItemProps>> = ({
-  children,
-  ...itemProps
-}) => {
-  return <ChakraBreadcrumbItem {...itemProps}>{children}</ChakraBreadcrumbItem>;
-};
+import { BreadcrumbItem } from '@chakra-ui/react';
 
 export default BreadcrumbItem;
