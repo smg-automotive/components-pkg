88e73c1dca2046a923504bee9a68ba190c89f387
talamcol <talamcol>
2022-11-10 16:26:41 +0100

    fix: unit tests

--- src/components/carousel/__tests__/Index.Test.tsx
+++ src/components/carousel/__tests__/Index.Test.tsx
@@ -86,50 +86,6 @@ describe('<Carousel />', () => {
     expect(mockOnClick).toHaveBeenCalledWith(0);
   });
 
-  it('should prerender a fallback slide on the server (emblaRef is undefined) when the start index is different than 0', () => {
-    (useEmblaCarousel as unknown as jest.Mock).mockReturnValue([
-      undefined,
-      undefined,
-    ]);
-
-    render(
-      <Carousel onSlideClick={jest.fn} startIndex={1}>
-        <div>slide 1</div>
-        <div>slide 2</div>
-        <div>slide 3</div>
-      </Carousel>
-    );
-
-    expect(screen.queryByLabelText('Carousel')).toBeNull();
-    expect(screen.getByLabelText('2 of 3')).toHaveAttribute(
-      'aria-current',
-      'true'
-    );
-    expect(screen.queryByLabelText('1 of 3')).toBeNull();
-    expect(screen.queryByLabelText('3 of 3')).toBeNull();
-  });
-
-  it('should prerender the carousel on the server if the start index is 0', () => {
-    (useEmblaCarousel as unknown as jest.Mock).mockReturnValue([
-      undefined,
-      undefined,
-    ]);
-
-    render(
-      <Carousel onSlideClick={jest.fn} startIndex={0}>
-        <div>slide 1</div>
-        <div>slide 2</div>
-        <div>slide 3</div>
-      </Carousel>
-    );
-
-    expect(screen.getByLabelText('Carousel')).toBeInTheDocument();
-    expect(screen.getByLabelText('1 of 3')).toHaveAttribute(
-      'aria-current',
-      'true'
-    );
-  });
-
   it('should have thumbnails on the fullscreen gallery', () => {
     render(
       <Carousel fullScreen={true}>
@@ -186,6 +142,50 @@ describe('<Carousel />', () => {
     );
     await userEvent.click(screen.getByLabelText('next slide'));
     expect(screen.getByLabelText('thumbnail 2 of 3')).toHaveAttribute(
+      'aria-current',
+      'true'
+    );
+  });
+
+  it('should prerender a fallback slide on the server (emblaRef is undefined) when the start index is different than 0', () => {
+    (useEmblaCarousel as unknown as jest.Mock).mockReturnValue([
+      undefined,
+      undefined,
+    ]);
+
+    render(
+      <Carousel onSlideClick={jest.fn} startIndex={1}>
+        <div>slide 1</div>
+        <div>slide 2</div>
+        <div>slide 3</div>
+      </Carousel>
+    );
+
+    expect(screen.queryByLabelText('Carousel')).toBeNull();
+    expect(screen.getByLabelText('2 of 3')).toHaveAttribute(
+      'aria-current',
+      'true'
+    );
+    expect(screen.queryByLabelText('1 of 3')).toBeNull();
+    expect(screen.queryByLabelText('3 of 3')).toBeNull();
+  });
+
+  it('should prerender the carousel on the server if the start index is 0', () => {
+    (useEmblaCarousel as unknown as jest.Mock).mockReturnValue([
+      undefined,
+      undefined,
+    ]);
+
+    render(
+      <Carousel onSlideClick={jest.fn} startIndex={0}>
+        <div>slide 1</div>
+        <div>slide 2</div>
+        <div>slide 3</div>
+      </Carousel>
+    );
+
+    expect(screen.getByLabelText('Carousel')).toBeInTheDocument();
+    expect(screen.getByLabelText('1 of 3')).toHaveAttribute(
       'aria-current',
       'true'
     );
