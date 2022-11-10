5956f0f7c8107438ec178a08162fe3ae2ed0a3b2
talamcol <talamcol>
2022-11-10 16:14:11 +0100

    chore: add tests for pagination component

---
+++ src/components/carousel/__tests__/ThumbnailPagination.Test.tsx
@@ -0,0 +1,203 @@
+import React from 'react';
+import { EmblaCarouselType } from 'embla-carousel-react';
+import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
+
+import ThumbnailPagination from '../ThumbnailPagination';
+
+const thumbnails = [
+  <div key="thumbnail-1">thumbnail 1</div>,
+  <div key="thumbnail-2">thumbnail 2</div>,
+  <div key="thumbnail-3">thumbnail 3</div>,
+];
+
+const mockSlidesNotInView = jest.fn().mockReturnValue([]);
+const mockScrollProgress = jest.fn().mockReturnValue(0);
+const mockScrollNext = jest.fn();
+const mockScrollPrev = jest.fn();
+const paginationCarousel = {
+  slidesNotInView: mockSlidesNotInView,
+  scrollProgress: mockScrollProgress,
+  scrollNext: mockScrollNext,
+  scrollPrev: mockScrollPrev,
+  clickAllowed: jest.fn().mockReturnValue(true),
+  on: jest.fn(),
+} as unknown as EmblaCarouselType;
+
+const mockMainScrollTo = jest.fn();
+const mainCarousel = {
+  scrollTo: mockMainScrollTo,
+} as unknown as EmblaCarouselType;
+
+const paginationCarouselRef = () => null;
+
+describe('<ThumbnailPagination/>', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should render the thumbnails', () => {
+    render(
+      <ThumbnailPagination
+        currentSlide={0}
+        thumbnails={thumbnails}
+        paginationCarouselRef={paginationCarouselRef}
+        paginationCarousel={paginationCarousel}
+        mainCarousel={mainCarousel}
+      />
+    );
+    expect(screen.getByText('thumbnail 1')).toBeInTheDocument();
+    expect(screen.getByText('thumbnail 2')).toBeInTheDocument();
+    expect(screen.getByText('thumbnail 3')).toBeInTheDocument();
+  });
+
+  it('should NOT show the thumbnail navigation buttons if all thumbnails are visible', () => {
+    render(
+      <ThumbnailPagination
+        currentSlide={0}
+        thumbnails={thumbnails}
+        paginationCarouselRef={paginationCarouselRef}
+        paginationCarousel={paginationCarousel}
+        mainCarousel={mainCarousel}
+      />
+    );
+    expect(
+      screen.queryByLabelText('scroll to next thumbnail group')
+    ).toBeNull();
+    expect(
+      screen.queryByLabelText('scroll to previous thumbnail group')
+    ).toBeNull();
+  });
+
+  it('should show the NEXT thumbnail navigation button if some are outside the viewport on the right', () => {
+    mockSlidesNotInView.mockReturnValueOnce([1, 2]);
+    mockScrollProgress.mockReturnValueOnce(0.2);
+    render(
+      <ThumbnailPagination
+        currentSlide={0}
+        thumbnails={thumbnails}
+        paginationCarouselRef={paginationCarouselRef}
+        paginationCarousel={paginationCarousel}
+        mainCarousel={mainCarousel}
+      />
+    );
+    expect(
+      screen.getByLabelText('scroll to next thumbnail group')
+    ).toBeInTheDocument();
+    expect(
+      screen.queryByLabelText('scroll to previous thumbnail group')
+    ).toBeNull();
+  });
+
+  it('should show the PREVIOUS thumbnail navigation button if some are outside the viewport on the left', () => {
+    mockSlidesNotInView.mockReturnValueOnce([1, 2]);
+    mockScrollProgress.mockReturnValueOnce(0.8);
+    render(
+      <ThumbnailPagination
+        currentSlide={0}
+        thumbnails={thumbnails}
+        paginationCarouselRef={paginationCarouselRef}
+        paginationCarousel={paginationCarousel}
+        mainCarousel={mainCarousel}
+      />
+    );
+    expect(
+      screen.queryByLabelText('scroll to next thumbnail group')
+    ).toBeNull();
+    expect(
+      screen.getByLabelText('scroll to previous thumbnail group')
+    ).toBeInTheDocument();
+  });
+
+  it('should show BOTH thumbnail navigation buttons if some are outside the viewport on the left and on the right and scroll position is 0.21', () => {
+    mockSlidesNotInView.mockReturnValueOnce([1, 2]);
+    mockScrollProgress.mockReturnValueOnce(0.21);
+    render(
+      <ThumbnailPagination
+        currentSlide={0}
+        thumbnails={thumbnails}
+        paginationCarouselRef={paginationCarouselRef}
+        paginationCarousel={paginationCarousel}
+        mainCarousel={mainCarousel}
+      />
+    );
+    expect(
+      screen.getByLabelText('scroll to next thumbnail group')
+    ).toBeInTheDocument();
+    expect(
+      screen.getByLabelText('scroll to previous thumbnail group')
+    ).toBeInTheDocument();
+  });
+
+  it('should show BOTH thumbnail navigation buttons if some are outside the viewport on the left and on the right and scroll position is 0.79', () => {
+    mockSlidesNotInView.mockReturnValueOnce([1, 2]);
+    mockScrollProgress.mockReturnValueOnce(0.79);
+    render(
+      <ThumbnailPagination
+        currentSlide={0}
+        thumbnails={thumbnails}
+        paginationCarouselRef={paginationCarouselRef}
+        paginationCarousel={paginationCarousel}
+        mainCarousel={mainCarousel}
+      />
+    );
+    expect(
+      screen.getByLabelText('scroll to next thumbnail group')
+    ).toBeInTheDocument();
+    expect(
+      screen.getByLabelText('scroll to previous thumbnail group')
+    ).toBeInTheDocument();
+  });
+
+  it('should scroll to the NEXT thumbnail group WITHOUT changing slide', async () => {
+    mockSlidesNotInView.mockReturnValueOnce([1, 2]);
+    mockScrollProgress.mockReturnValueOnce(0.21);
+    render(
+      <ThumbnailPagination
+        currentSlide={0}
+        thumbnails={thumbnails}
+        paginationCarouselRef={paginationCarouselRef}
+        paginationCarousel={paginationCarousel}
+        mainCarousel={mainCarousel}
+      />
+    );
+    await userEvent.click(
+      screen.getByLabelText('scroll to next thumbnail group')
+    );
+    expect(mockScrollNext).toHaveBeenCalled();
+    expect(mockMainScrollTo).not.toHaveBeenCalled();
+  });
+
+  it('should scroll to the PREVIOUS thumbnail group WITHOUT changing slide', async () => {
+    mockSlidesNotInView.mockReturnValueOnce([1, 2]);
+    mockScrollProgress.mockReturnValueOnce(0.21);
+    render(
+      <ThumbnailPagination
+        currentSlide={0}
+        thumbnails={thumbnails}
+        paginationCarouselRef={paginationCarouselRef}
+        paginationCarousel={paginationCarousel}
+        mainCarousel={mainCarousel}
+      />
+    );
+    await userEvent.click(
+      screen.getByLabelText('scroll to previous thumbnail group')
+    );
+    expect(mockScrollPrev).toHaveBeenCalled();
+    expect(mockMainScrollTo).not.toHaveBeenCalled();
+  });
+
+  it('should change the main carousel slide when a thumbnail is clicked', async () => {
+    render(
+      <ThumbnailPagination
+        currentSlide={0}
+        thumbnails={thumbnails}
+        paginationCarouselRef={paginationCarouselRef}
+        paginationCarousel={paginationCarousel}
+        mainCarousel={mainCarousel}
+      />
+    );
+    await userEvent.click(screen.getByText('thumbnail 2'));
+    expect(mockMainScrollTo).toHaveBeenCalledWith(1);
+  });
+});
