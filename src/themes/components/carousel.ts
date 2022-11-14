9a73adb883e2042a3e7d0422b3dccad109302e71
talamcol <talamcol>
2022-11-14 10:45:07 +0100

    fix: fixed position on mobile fullscreen

--- src/themes/components/carousel.ts
+++ src/themes/components/carousel.ts
@@ -21,6 +21,10 @@ const parts = [
 const fullScreen = {
   container: {
     backgroundColor: 'black',
+    position: {
+      base: 'fixed',
+      md: 'static',
+    },
   },
   carousel: {
     paddingX: {
