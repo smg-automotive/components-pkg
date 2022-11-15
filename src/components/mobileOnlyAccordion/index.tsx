ec712283920583bb9a9b92ca454d0c05a6d7411b
boyarskiy <gekabrs@gmail.com>
2022-11-15 15:34:55 +0100

    fix: variant typing

--- src/components/mobileOnlyAccordion/index.tsx
+++ src/components/mobileOnlyAccordion/index.tsx
@@ -3,7 +3,9 @@ import { AccordionProps } from '@chakra-ui/react';
 
 import Accordion from '../accordion';
 
-type Props = Pick<AccordionProps, 'allowMultiple' | 'allowToggle'>;
+type Props = {
+  variant?: 'light' | 'dark';
+} & Pick<AccordionProps, 'allowMultiple' | 'allowToggle'>;
 
 const MobileOnlyAccordion: FC<PropsWithChildren<Props>> = (props) => {
   const { children, ...restProps } = props;
