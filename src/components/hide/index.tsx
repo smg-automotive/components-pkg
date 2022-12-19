b953d5fb4fad8ff4c454f7a94a8b7920a773cacd
talamcol <talamcol>
2022-12-19 08:23:26 +0100

    fix: the hide component

---
+++ src/components/hide/index.tsx
@@ -0,0 +1,23 @@
+import React, { FC, PropsWithChildren } from 'react';
+import { HideProps, useQuery } from '@chakra-ui/react';
+
+import Box from '../box';
+
+const Hide: FC<PropsWithChildren<HideProps>> = ({ children, ...props }) => {
+  const query = useQuery(props);
+  const media = `@media ${query}`;
+
+  return (
+    <Box
+      sx={{
+        [media]: {
+          display: 'none',
+        },
+      }}
+    >
+      {children}
+    </Box>
+  );
+};
+
+export default Hide;
