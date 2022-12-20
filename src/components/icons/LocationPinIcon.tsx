6d370f2de519d60092037792471a0e6770f1b3ee
mmataja <mmataja92@gmail.com>
2022-12-20 22:37:26 +0100

    feat: add time icon

--- src/components/icons/LocationPinIcon.tsx
+++ src/components/icons/LocationPinIcon.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createIcon } from '@chakra-ui/react';
 
 export const LocationPinIcon = createIcon({
-  displayName: 'Location Pin',
+  displayName: 'Location',
   viewBox: '0 0 24 24',
   path: (
     <>
