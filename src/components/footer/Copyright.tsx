e8eba9afcffda423584782d6d7f87e9fb39601e7
boyarskiy <gekabrs@gmail.com>
2022-12-21 14:24:13 +0100

    fix(footer): adjust copyright styles

--- src/components/footer/Copyright.tsx
+++ src/components/footer/Copyright.tsx
@@ -1,18 +1,16 @@
 import React, { FC } from 'react';
 import { useI18n } from '@smg-automotive/i18n-pkg';
 
-import Text from '../text';
-import Center from '../center';
+import Box from '../box';
+import { opacity } from '../../themes/shared/opacity';
 
 const FooterCopyright: FC = () => {
   const { t } = useI18n();
 
   return (
-    <Center>
-      <Text textStyle="body-small">
-        {t('footer.copyright', { year: new Date().getFullYear() })}
-      </Text>
-    </Center>
+    <Box textStyle="body-small" textAlign="center" opacity={opacity[80]}>
+      {t('footer.copyright', { year: new Date().getFullYear() })}
+    </Box>
   );
 };
 
