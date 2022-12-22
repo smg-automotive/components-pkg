509e9aea12042706a6c9b8d9f4a54a0d4fd2c3d5
Krzysztof Sakwerda <ksakwerda@gmail.com>
2022-12-22 10:33:36 +0100

    fix: only export hosted fonts through submodule

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export { useTheme } from '@chakra-ui/react';
 export { motoScout24Theme, autoScout24Theme, breakpoints } from './themes';
 export * from './components';
-export * from './fonts';
 export * from './utilities/convertRemEmToPx';
 export { default as logger } from './lib/logger';
