de83ee2784ef88c85b153838d8a26a77c974f14f
Lorenzo Kappeler <lorenzo@carforyou.ch>
2023-01-25 13:18:18 +0100

    fix(Accordion): render CSS based instead of conditional (#467)
    * fix: render accordion CSS based
    
    * chore: adapt test
    
    * fix: chevron icon rendering
    
    * fix: try accordion styles
    
    * fix: overlapping breakpoint
    
    * fix: invert test case

--- src/components/accordion/AccordionIcon.tsx
+++ src/components/accordion/AccordionIcon.tsx
@@ -1,11 +1,18 @@
 import React, { FC } from 'react';
-import { useAccordionItemState } from '@chakra-ui/react';
+import { useAccordionItemState, useAccordionStyles } from '@chakra-ui/react';
 
-import { ChevronDownLargeIcon, ChevronUpLargeIcon } from '../icons';
+import { ChevronUpLargeIcon } from '../icons';
 
 const AccordionIcon: FC = () => {
   const { isOpen } = useAccordionItemState();
-  return isOpen ? <ChevronUpLargeIcon /> : <ChevronDownLargeIcon />;
+  const styles = useAccordionStyles();
+
+  const iconStyles = {
+    transform: isOpen ? 'rotate(-180deg)' : undefined,
+    ...styles.icon,
+  };
+
+  return <ChevronUpLargeIcon __css={iconStyles} />;
 };
 
 export default AccordionIcon;
--- src/components/mobileOnlyAccordion/MobileOnlyAccordionButton.tsx
+++ src/components/mobileOnlyAccordion/MobileOnlyAccordionButton.tsx
@@ -1,24 +1,27 @@
 import React, { FC, PropsWithChildren } from 'react';
 
-import { useMediaQuery, useMultiStyleConfig } from '@chakra-ui/react';
+import { useMultiStyleConfig } from '@chakra-ui/react';
 
+import Show from '../show';
+import Hide from '../hide';
 import Box from '../box';
 import AccordionButton from '../accordion/AccordionButton';
-import { breakpoints } from '../../themes';
 
 const MobileOnlyAccordionButton: FC<PropsWithChildren> = (props) => {
   const { children, ...rest } = props;
   const { titleOnDesktop } = useMultiStyleConfig('Accordion');
-  const [isLargerThanMd] = useMediaQuery(`(min-width: ${breakpoints.md.px}px)`);
-
-  if (!isLargerThanMd) {
-    return <AccordionButton {...rest}>{children}</AccordionButton>;
-  }
 
   return (
-    <Box __css={titleOnDesktop} {...rest}>
-      {children}
-    </Box>
+    <>
+      <Show above="md">
+        <Box __css={titleOnDesktop} {...rest}>
+          {children}
+        </Box>
+      </Show>
+      <Hide above="md">
+        <AccordionButton {...rest}>{children}</AccordionButton>
+      </Hide>
+    </>
   );
 };
 
