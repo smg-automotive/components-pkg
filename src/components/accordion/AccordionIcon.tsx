0a7352c9850b8d1c27619706cdd0475fa716a469
Lorenzo Kappeler <me@lnk.codes>
2023-01-25 07:58:01 +0100

    fix: chevron icon rendering

--- src/components/accordion/AccordionIcon.tsx
+++ src/components/accordion/AccordionIcon.tsx
@@ -1,11 +1,13 @@
 import React, { FC } from 'react';
 import { useAccordionItemState } from '@chakra-ui/react';
 
-import { ChevronDownLargeIcon, ChevronUpLargeIcon } from '../icons';
+import { ChevronUpLargeIcon } from '../icons';
 
 const AccordionIcon: FC = () => {
   const { isOpen } = useAccordionItemState();
-  return isOpen ? <ChevronUpLargeIcon /> : <ChevronDownLargeIcon />;
+  const iconStyles = { transform: isOpen ? 'rotate(-180deg)' : undefined };
+
+  return <ChevronUpLargeIcon __css={iconStyles} />;
 };
 
 export default AccordionIcon;
