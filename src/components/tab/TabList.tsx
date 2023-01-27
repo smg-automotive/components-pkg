a8fe2cd6c35346c474dfba928b26ecb1500aff51
Marine-Berthier <marine154@hotmail.fr>
2023-01-27 16:25:18 +0100

    test

--- src/components/tab/TabList.tsx
+++ src/components/tab/TabList.tsx
@@ -1,8 +1,19 @@
 import React, { FC, PropsWithChildren } from 'react';
 import { TabList as ChakraTabList } from '@chakra-ui/react';
 
-const TabList: FC<PropsWithChildren> = (props) => {
-  return <ChakraTabList>{props.children}</ChakraTabList>;
+interface Props {
+  justifyContent?: 'start' | 'space-between' | 'space-around';
+}
+
+const TabList: FC<PropsWithChildren<Props>> = ({
+  justifyContent = 'start',
+  ...props
+}) => {
+  return (
+    <ChakraTabList justifyContent={justifyContent}>
+      {props.children}
+    </ChakraTabList>
+  );
 };
 
 export default TabList;
