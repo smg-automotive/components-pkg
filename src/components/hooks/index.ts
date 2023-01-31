740fabec49555cc57472be3f41dc498bdf151803
Mateo Mataja <mmataja92@gmail.com>
2023-01-31 12:40:59 +0100

    feat: expose accordion state (#475)
    * fix: accordion icon display rotation
    
    * feat: expose useAccordionItemState hook

--- src/components/hooks/index.ts
+++ src/components/hooks/index.ts
@@ -1,3 +1,4 @@
+export { default as useAccordionItemState } from './useAccordionItemState';
 export { default as useDebouncedOnChange } from './useDebouncedOnChange';
-export { default as useMediaQuery } from './useMediaQuery';
 export { default as useDisclosure } from './useDisclosure';
+export { default as useMediaQuery } from './useMediaQuery';
