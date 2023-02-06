5afb8bd0196d39520a22512978e7737fe34f3152
Marine-Berthier <marine154@hotmail.fr>
2023-02-06 18:13:18 +0100

    fix: add on change and index props

--- src/components/tab/index.tsx
+++ src/components/tab/index.tsx
@@ -1,7 +1,7 @@
 import React, { FC, PropsWithChildren } from 'react';
 import { Tabs as ChakraTabs, TabsProps } from '@chakra-ui/react';
 
-type Props = Pick<TabsProps, 'defaultIndex' | 'isLazy'>;
+type Props = Pick<TabsProps, 'defaultIndex' | 'isLazy' | 'onChange' | 'index'>;
 
 const Tabs: FC<PropsWithChildren<Props>> = (props) => {
   return <ChakraTabs {...props}>{props.children}</ChakraTabs>;
