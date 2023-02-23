d93204f54ee2b95b7f237b1dc3cd88afd93cc331
Krzysztof Sakwerda <ksakwerda@gmail.com>
2023-02-23 15:01:02 +0100

    chore: use mach-media mock

--- .jest/setup/mockMatchMedia.ts
+++ .jest/setup/mockMatchMedia.ts
@@ -1,13 +1,1 @@
-Object.defineProperty(window, 'matchMedia', {
-  writable: true,
-  value: jest.fn().mockImplementation((query) => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: jest.fn(), // deprecated
-    removeListener: jest.fn(), // deprecated
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
-  })),
-});
+import 'mock-match-media/polyfill';
