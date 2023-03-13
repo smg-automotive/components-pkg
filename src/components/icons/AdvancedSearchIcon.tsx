7f918b7b340f6510703686e734953d3889c19452
“ilay149” <ilay.bicer@gmail.com>
2023-03-13 09:32:12 +0100

    feat: new AdvancedSearchIcon.tsx

---
+++ src/components/icons/AdvancedSearchIcon.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createIcon } from '@chakra-ui/react';
+
+export const AdvancedSearchIcon = createIcon({
+  displayName: 'Advanced search',
+  viewBox: '0 0 24 24',
+  path: (
+    <>
+      <title>Advanced Search Icon</title>
+      <path fill="currentColor" d="" />
+    </>
+  ),
+  defaultProps: {
+    boxSize: 'sm',
+  },
+});
