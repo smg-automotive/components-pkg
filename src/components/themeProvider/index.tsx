ade7e42fa425309866df02aa09667821140440a1
Mateo Mataja <mmataja92@gmail.com>
2023-03-23 15:10:33 +0100

    BREAKING CHANGE: align brand name with backend (#517)
    * feat: align brand name with backend
    
    * fix: story name

--- src/components/themeProvider/index.tsx
+++ src/components/themeProvider/index.tsx
@@ -1,16 +1,18 @@
 import React, { FC, PropsWithChildren } from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 
+import { Brand } from 'src/types/brand';
+
 import { autoScout24Theme, motoScout24Theme } from 'src/themes';
 
 export type Props = {
   // Theme to use
-  theme: 'as24' | 'ms24';
+  theme: Brand;
 };
 
 const themes = {
-  as24: autoScout24Theme,
-  ms24: motoScout24Theme,
+  [Brand.AutoScout24]: autoScout24Theme,
+  [Brand.MotoScout24]: motoScout24Theme,
 };
 
 const ThemeProvider: FC<PropsWithChildren<Props>> = ({ theme, children }) => {
