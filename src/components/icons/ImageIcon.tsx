d6bfade5c368ca2102b77f9757c052bc1b79d707
Lorenzo Kappeler <me@lnk.codes>
2023-04-06 15:59:15 +0200

    fix: rename and export

--- src/components/icons/ImageIcon.tsx
+++ src/components/icons/ImageIcon.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createIcon } from '@chakra-ui/react';
 
-export const Image = createIcon({
+export const ImageIcon = createIcon({
   displayName: 'Image',
   viewBox: '0 0 24 24',
   path: (
