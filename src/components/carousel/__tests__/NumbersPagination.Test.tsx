efe2c2a9225d95852007f8b0d064f197fa024fca
boyarskiy <gekabrs@gmail.com>
2023-04-12 07:34:54 +0200

    fix: add tests

---
+++ src/components/carousel/__tests__/NumbersPagination.Test.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { EmblaCarouselType } from 'embla-carousel-react';
+import userEvent from '@testing-library/user-event';
+import { render, screen, waitFor } from '@testing-library/react';
+
+import NumbersPagination from '../NumbersPagination';
+
+const numbers = [0, 1, 2, 3, 4, 5];
+
+const mockMainScrollTo = jest.fn();
+const mainCarousel = {
+  scrollTo: mockMainScrollTo,
+} as unknown as EmblaCarouselType;
+
+describe('<NumbersPagination />', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should render the numbers pagination', () => {
+    render(
+      <NumbersPagination
+        currentSlideIndex={0}
+        numbers={numbers}
+        mainCarousel={mainCarousel}
+      />
+    );
+    expect(screen.getByText('1')).toBeInTheDocument();
+    expect(screen.getByText('2')).toBeInTheDocument();
+    expect(screen.getByText('3')).toBeInTheDocument();
+  });
+
+  it('should change the main carousel slide when a numbers button is clicked', async () => {
+    render(
+      <NumbersPagination
+        currentSlideIndex={0}
+        numbers={numbers}
+        mainCarousel={mainCarousel}
+      />
+    );
+    userEvent.click(screen.getByText('2'));
+    await waitFor(() => expect(mockMainScrollTo).toHaveBeenCalledWith(1));
+  });
+});
