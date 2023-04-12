12f027b2f495d3ff427049ca6f40e0814a696caf
GutierrezGo <virginia@carforyou.ch>
2023-04-12 14:04:55 +0200

    fix: adjusted icon

--- src/components/icons/StarIcon.tsx
+++ src/components/icons/StarIcon.tsx
@@ -8,12 +8,14 @@ export const StarIcon = createIcon({
     <>
       <title>Star icon</title>
       <path
-        d="M19 21L12 17.26L5 21L6.81 13.53L2 8H9L12 2L15 8H22L17.19 13.53L19 21ZM12 15L16 17.21L15 13L17.64 10H13.77L12 6.49L10.23 10H6.35L9 13L8 17.19L12 15Z"
-        fill="currentColor"
+        strokeWidth="2"
+        d="M19 21L12 17.26L5 21L6.81 13.53L2 8H9L12 2L15 8H22L17.19 13.53L19 21Z"
       />
     </>
   ),
   defaultProps: {
     boxSize: 'sm',
+    fill: 'none',
+    stroke: 'currentColor',
   },
 });
