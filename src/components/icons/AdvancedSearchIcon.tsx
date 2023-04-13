4e7b950e4f14c47eb9ab51db780bf756895aa03d
“ilay149” <ilay.bicer@gmail.com>
2023-04-13 13:28:39 +0200

    fix: fix

--- src/components/icons/AdvancedSearchIcon.tsx
+++ src/components/icons/AdvancedSearchIcon.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createIcon } from '@chakra-ui/react';
 
 export const AdvancedSearchIcon = createIcon({
-  displayName: 'AdvancedSearchh',
+  displayName: 'AdvancedSearch',
   viewBox: '0 0 22 22',
   path: (
     <>
