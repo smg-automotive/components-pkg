4836ad7a53ef18d7daa91186b3f1fd2ff618fdbf
talamcol <colin@talamona.ch>
2023-04-13 09:00:31 +0200

    fix: increase zIndex of popover

--- src/components/popoverFilter/Popover.tsx
+++ src/components/popoverFilter/Popover.tsx
@@ -4,8 +4,10 @@ import {
   PopoverContent,
   PopoverFooter,
   PopoverHeader,
+  Portal,
 } from '@chakra-ui/react';
 
+import Box from '../box';
 import { PopoverFilterProps } from './props';
 import FilterHeading from './Heading';
 import FilterActionButton from './ActionButton';
@@ -32,34 +34,37 @@ const Popover: FC<Props> = ({
   children,
 }) => {
   return (
-    <PopoverContent
-      backgroundColor="white"
-      borderRadius="sm"
-      padding="2xl"
-      shadow="xs"
-      w="6xl"
-      zIndex="popover"
-    >
-      <PopoverHeader paddingBottom="2xl">
-        <FilterHeading
-          isApplied={isApplied}
-          label={label}
-          numberOfAppliedFilters={numberOfAppliedFilters}
-          onClose={onClose}
-          onResetFilter={onResetFilter}
-        />
-      </PopoverHeader>
-      <PopoverBody maxH="6xl" overflowY="scroll">
-        {children}
-      </PopoverBody>
-      <PopoverFooter paddingTop="2xl">
-        <FilterActionButton
-          actionButton={actionButton}
-          isApplied={isApplied}
-          onClose={onClose}
-        />
-      </PopoverFooter>
-    </PopoverContent>
+    <Portal>
+      <Box zIndex="popover" w="full" h="full" position="relative">
+        <PopoverContent
+          backgroundColor="white"
+          borderRadius="sm"
+          padding="2xl"
+          shadow="xs"
+          w="6xl"
+        >
+          <PopoverHeader paddingBottom="2xl">
+            <FilterHeading
+              isApplied={isApplied}
+              label={label}
+              numberOfAppliedFilters={numberOfAppliedFilters}
+              onClose={onClose}
+              onResetFilter={onResetFilter}
+            />
+          </PopoverHeader>
+          <PopoverBody maxH="6xl" overflowY="scroll">
+            {children}
+          </PopoverBody>
+          <PopoverFooter paddingTop="2xl">
+            <FilterActionButton
+              actionButton={actionButton}
+              isApplied={isApplied}
+              onClose={onClose}
+            />
+          </PopoverFooter>
+        </PopoverContent>
+      </Box>
+    </Portal>
   );
 };
 
