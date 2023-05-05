60980a2cf1478ab0572a4448b73859ec812ad1a3
AdzicMilos <55357316+AdzicMilos@users.noreply.github.com>
2023-05-05 11:04:42 +0200

    fix: enable tooltiop close/open on touch screens (#599)
    * fix: enable tooltiop close/open on touch screens
    
    * fix: unit test

--- src/components/tooltip/index.tsx
+++ src/components/tooltip/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { cloneElement, FC, useState } from 'react';
 
 import { Tooltip as ChakraTooltip, TooltipProps } from '@chakra-ui/react';
 
@@ -7,9 +7,24 @@ type Props = {
 } & Pick<TooltipProps, 'label' | 'placement' | 'maxWidth'>;
 
 const Tooltip: FC<Props> = ({ children, ...props }) => {
+  const [isLabelOpen, setIsLabelOpen] = useState<boolean>(false);
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const childrenWithProps = cloneElement(children as React.ReactElement<any>, {
+    onMouseEnter: () => setIsLabelOpen(true),
+    onMouseLeave: () => setIsLabelOpen(false),
+    onClick: () => setIsLabelOpen(true),
+  });
+
   return (
-    <ChakraTooltip hasArrow placement="auto" maxWidth="6xl" {...props}>
-      {children}
+    <ChakraTooltip
+      hasArrow
+      placement="auto"
+      maxWidth="6xl"
+      isOpen={isLabelOpen}
+      {...props}
+    >
+      {childrenWithProps}
     </ChakraTooltip>
   );
 };
