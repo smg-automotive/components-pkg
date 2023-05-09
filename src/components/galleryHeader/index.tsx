c04c218b112e2659ce6528f0fc13d2d64ce92a70
Marine-Berthier <marine154@hotmail.fr>
2023-05-09 11:53:24 +0200

    fix: translation

--- src/components/galleryHeader/index.tsx
+++ src/components/galleryHeader/index.tsx
@@ -1,14 +1,15 @@
 import React, { FC } from 'react';
 
-import {
-  Box,
-  CloseIcon,
-  H1,
-  H2,
-  Link,
-  Show,
-  SimpleGrid,
-} from 'src/components/index';
+import { I18nContext } from '@smg-automotive/i18n-pkg';
+
+import { Language } from 'src/types/language';
+import TranslationProvider from 'src/components/translationProvider';
+import SimpleGrid from 'src/components/simpleGrid';
+import Show from 'src/components/show';
+import Link from 'src/components/link';
+import { CloseIcon } from 'src/components/icons';
+import { H1, H2 } from 'src/components/heading';
+import Box from 'src/components/box';
 
 const GalleryHeader: FC<{
   currentSlide: number;
@@ -16,50 +17,60 @@ const GalleryHeader: FC<{
   onClose: () => void;
   title?: string;
   subtitle?: string;
-}> = ({ currentSlide, slidesCount, onClose, title, subtitle }) => {
+  language: Language;
+}> = ({ currentSlide, slidesCount, onClose, title, subtitle, language }) => {
   return (
-    <Box
-      color="white"
-      bg="black"
-      px="2xl"
-      py="lg"
-      position={{
-        base: 'fixed',
-        md: 'static',
-      }}
-      top={0}
-      left={0}
-      right={0}
-      zIndex={1}
-    >
-      <SimpleGrid
-        columns={{
-          base: 2,
-          md: 3,
-        }}
-        alignItems="center"
-      >
-        <Show above="md">
-          <>
-            {title && <H1 textStyle="body-large">title</H1>}
-            {subtitle && <H2 textStyle="body-large">subtitle</H2>}
-          </>
-        </Show>
-        <Box
-          textAlign={{
-            base: 'left',
-            md: 'center',
-          }}
-        >
-          {currentSlide} / {slidesCount}
-        </Box>
-        <Box textAlign="right">
-          <Link onClick={() => onClose()}>
-            <CloseIcon color="white" />
-          </Link>
-        </Box>
-      </SimpleGrid>
-    </Box>
+    <TranslationProvider language={language} scopes={['galleryHeader']}>
+      <I18nContext.Consumer>
+        {({ t }) => (
+          <Box
+            color="white"
+            bg="black"
+            px="2xl"
+            py="lg"
+            position={{
+              base: 'fixed',
+              md: 'static',
+            }}
+            top={0}
+            left={0}
+            right={0}
+            zIndex={1}
+          >
+            <SimpleGrid
+              columns={{
+                base: 2,
+                md: 3,
+              }}
+              alignItems="center"
+            >
+              <Show above="md">
+                <>
+                  {title && <H1 textStyle="body-large">title</H1>}
+                  {subtitle && <H2 textStyle="body-large">subtitle</H2>}
+                </>
+              </Show>
+              <Box
+                textAlign={{
+                  base: 'left',
+                  md: 'center',
+                }}
+              >
+                {t('galleryHeader.imageCount', {
+                  x: currentSlide,
+                  of: slidesCount,
+                })}
+              </Box>
+              <Box textAlign="right">
+                <Link onClick={() => onClose()}>
+                  <CloseIcon color="white" />
+                </Link>
+              </Box>
+            </SimpleGrid>
+          </Box>
+        )}
+      </I18nContext.Consumer>
+    </TranslationProvider>
   );
 };
 
