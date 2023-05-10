ba16210c12bcaa518204032484a8e8bd7b130c67
Milos Zbiljic <milos.zbiljic@swissmarketplace.group>
2023-05-10 15:46:06 +0200

    fix: custom drawer

--- src/components/navigation/header/drawer/index.tsx
+++ src/components/navigation/header/drawer/index.tsx
@@ -1,12 +1,12 @@
 import React, { FC } from 'react';
 
-import { DrawerBody } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 
 import Grid from 'src/components/grid';
 
-import DrawerOverlay from 'src/components/drawer/DrawerOverlay';
-import DrawerContent from 'src/components/drawer/DrawerContent';
-import Drawer from 'src/components/drawer';
+// import DrawerOverlay from 'src/components/drawer/DrawerOverlay';
+// import DrawerContent from 'src/components/drawer/DrawerContent';
+// import Drawer from 'src/components/drawer';
 
 import { Drawer as useNavigationDrawerType } from '../hooks/useNavigationDrawer';
 import { DrawerMenu } from './DrawerMenu';
@@ -21,32 +21,52 @@ interface NavigationDrawerProps {
 export const NavigationDrawer: FC<NavigationDrawerProps> = ({
   drawer,
   isOpen,
-  onClose,
-  menuHeight,
 }) => {
-  return (
-    <Drawer isOpen={isOpen} placement="top" onClose={onClose}>
-      <DrawerOverlay />
-      <DrawerContent marginTop={menuHeight}>
-        <DrawerBody
-          data-testid="drawer-body"
-          py={{ md: '2xl' }}
-          px={{ md: 'xs' }}
-          maxWidth="container.2xl"
-          width="full"
-          margin={'auto'}
+  return isOpen ? (
+    <div>
+      <Box
+        data-testid="drawer-body"
+        py={{ md: '2xl' }}
+        px={{ md: 'xs' }}
+        maxWidth="container.2xl"
+        width="full"
+        margin={'auto'}
+      >
+        <Grid
+          height="full"
+          templateColumns={{ '2xs': '1fr', md: 'repeat(5, 1fr)' }}
+          gridGap={{ md: '3xl' }}
         >
-          <Grid
-            height="full"
-            templateColumns={{ '2xs': '1fr', md: 'repeat(5, 1fr)' }}
-            gridGap={{ md: '3xl' }}
-          >
-            {drawer?.nodes.map((node, index) => (
-              <DrawerMenu key={`node-${index}`} node={node} />
-            ))}
-          </Grid>
-        </DrawerBody>
-      </DrawerContent>
-    </Drawer>
-  );
+          {drawer?.nodes.map((node, index) => (
+            <DrawerMenu key={`node-${index}`} node={node} />
+          ))}
+        </Grid>
+      </Box>
+    </div>
+  ) : null;
+  // return (
+  //   <Drawer isOpen={isOpen} placement="top" onClose={onClose}>
+  //     <DrawerOverlay />
+  //     <DrawerContent marginTop={menuHeight}>
+  //       <DrawerBody
+  //         data-testid="drawer-body"
+  //         py={{ md: '2xl' }}
+  //         px={{ md: 'xs' }}
+  //         maxWidth="container.2xl"
+  //         width="full"
+  //         margin={'auto'}
+  //       >
+  //         <Grid
+  //           height="full"
+  //           templateColumns={{ '2xs': '1fr', md: 'repeat(5, 1fr)' }}
+  //           gridGap={{ md: '3xl' }}
+  //         >
+  //           {drawer?.nodes.map((node, index) => (
+  //             <DrawerMenu key={`node-${index}`} node={node} />
+  //           ))}
+  //         </Grid>
+  //       </DrawerBody>
+  //     </DrawerContent>
+  //   </Drawer>
+  // );
 };
