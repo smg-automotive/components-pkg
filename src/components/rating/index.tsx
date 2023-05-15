717e975c5e109e5b50683c5a49eced3e81533f34
talamcol <colin@talamona.ch>
2023-05-15 15:34:27 +0200

    feat: optimize sizes

--- src/components/rating/index.tsx
+++ src/components/rating/index.tsx
@@ -3,32 +3,32 @@ import { Box } from '@chakra-ui/react';
 
 type Props = {
   rating: number;
+  size: 'large' | 'small';
 };
 
-const Rating: FC<Props> = ({ rating }) => {
+const Rating: FC<Props> = ({ rating, size }) => {
   return (
     <Box
       aria-label={`Rating is ${rating} out of 5`}
       sx={{
         '--rating': rating.toString(),
-        '--star-size': '60px',
         '--star-color': 'white',
-        '--star-background': 'black',
+        '--star-background': '#333',
         '--percent': 'calc(var(--rating) / 5 * 100%)',
       }}
       display="inline-block"
-      fontSize="var(--star-size)"
-      fontFamily="Times"
-      lineHeight="1"
+      fontSize={size === 'large' ? 'md' : 'sm'}
+      fontFamily="Arial"
+      lineHeight="xs"
       _before={{
         content: '"★★★★★"',
-        letterSpacing: '3px',
+        letterSpacing: size === 'large' ? '10px' : '5px',
         color: 'var(--star-background)',
         background:
           'linear-gradient(90deg, var(--star-background) var(--percent), var(--star-color) var(--percent))',
         '-webkit-background-clip': 'text',
         '-webkit-text-fill-color': 'transparent',
-        '-webkit-text-stroke': '3px black',
+        '-webkit-text-stroke': `${size === 'large' ? '2px' : '1px'} #333`,
       }}
     />
   );
