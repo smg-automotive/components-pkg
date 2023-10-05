851d4854ce7b03455221999a1d2adf29e7d17f6a
Krzysztof Sakwerda <ksakwerda@gmail.com>
2023-10-05 10:50:52 +0200

    fix: re-style input to support the group and elements

--- src/themes/components/input.ts
+++ src/themes/components/input.ts
@@ -1,47 +1,64 @@
-import type { PartsStyleObject, SystemStyleObject } from '@chakra-ui/react';
+import {
+  createMultiStyleConfigHelpers,
+  cssVar,
+  defineStyle,
+} from '@chakra-ui/styled-system';
 import { inputAnatomy as parts } from '@chakra-ui/anatomy';
 
-const baseStyle: PartsStyleObject<typeof parts> = {
+const { definePartsStyle, defineMultiStyleConfig } =
+  createMultiStyleConfigHelpers(parts.keys);
+
+const $height = cssVar('input-height');
+const $paddingX = cssVar('input-padding-x');
+const $paddingY = cssVar('input-padding-y');
+
+const baseStyle = definePartsStyle({
   field: {
     width: 'full',
+    height: $height.reference,
+    px: $paddingX.reference,
+    py: $paddingY.reference,
+    borderRadius: 'sm',
     minWidth: 0,
     outline: 0,
     position: 'relative',
     appearance: 'none',
     transitionProperty: 'common',
     transitionDuration: 'normal',
-    px: 'md',
-    py: 'xs',
   },
-};
+});
 
-const size: Record<string, SystemStyleObject> = {
-  md: {
-    textStyle: 'body-small',
-    h: 'md',
-  },
-  lg: {
+const size = {
+  lg: defineStyle({
     textStyle: 'body',
-    h: 'lg',
-  },
+    [$paddingX.variable]: 'space.md',
+    [$paddingY.variable]: 'space.xs',
+    [$height.variable]: 'sizes.lg',
+  }),
+  md: defineStyle({
+    textStyle: 'body-small',
+    [$paddingX.variable]: 'space.md',
+    [$paddingY.variable]: 'space.xs',
+    [$height.variable]: 'sizes.md',
+  }),
 };
 
-const sizes: Record<string, PartsStyleObject<typeof parts>> = {
-  md: {
-    field: size.md,
-  },
-  lg: {
+const sizes = {
+  lg: definePartsStyle({
     field: size.lg,
-  },
+    group: size.lg,
+  }),
+  md: definePartsStyle({
+    field: size.md,
+    group: size.md,
+  }),
 };
 
-const variantOutline: PartsStyleObject<typeof parts> = {
+const variantOutline = definePartsStyle({
   field: {
     border: '1px solid',
     borderColor: 'gray.400',
-    borderRadius: 'sm',
     bg: 'inherit',
-    color: 'gray.900',
     _placeholder: {
       color: 'gray.400',
     },
@@ -61,21 +78,18 @@ const variantOutline: PartsStyleObject<typeof parts> = {
       borderColor: 'gray.200',
     },
   },
-};
+});
 
 const variants = {
   outline: variantOutline,
 };
 
-const defaultProps = {
-  size: 'md',
-  variant: 'outline',
-};
-
-export default {
-  parts: parts.keys,
+export default defineMultiStyleConfig({
   baseStyle,
   sizes,
   variants,
-  defaultProps,
-};
+  defaultProps: {
+    size: 'md',
+    variant: 'outline',
+  },
+});
