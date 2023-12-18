e68465e80d7ef1c5038d38a0a26af812c0fab7ae
filip-kis-smg <141041162+filip-kis-smg@users.noreply.github.com>
2023-12-18 14:09:29 +0100

    feat: allow optional rendering of modal body wrapper (#842)
    * feat: allow optional rendering of modal body wrapper
    
    * feat: add story
    
    * fix: linter
    
    * refactor: add no body padding modal variant
    
    * chore: cleanup
    
    * chore: cleanup
    
    * refactor: add `disableBodyPadding` prop

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -26,6 +26,7 @@ interface Props
   secondaryActionButton?: ActionButton;
   variant?: 'fullScreen' | 'base' | 'topScroll';
   size?: 'md' | 'lg' | 'full';
+  disableBodyPadding?: boolean;
 }
 
 const Modal: FC<PropsWithChildren<Props>> = ({
@@ -36,11 +37,14 @@ const Modal: FC<PropsWithChildren<Props>> = ({
   size,
   motionPreset = 'scale',
   variant = 'base',
+  disableBodyPadding = false,
   ...modalProps
 }) => {
   const modalSize =
     size || variant !== 'fullScreen' ? { xs: 'full', sm: size } : 'full';
   const bothActionButtons = primaryActionButton && secondaryActionButton;
+
+  const bodyPadding = disableBodyPadding ? { p: 0 } : {};
 
   return (
     <ChakraModal
@@ -62,7 +66,7 @@ const Modal: FC<PropsWithChildren<Props>> = ({
           </>
         )}
 
-        <ModalBody>{children}</ModalBody>
+        <ModalBody {...bodyPadding}>{children}</ModalBody>
 
         {(primaryActionButton || secondaryActionButton) && (
           <>
