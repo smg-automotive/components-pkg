3d487d5b534688808d3b7f7fc2b543fb7a3433c4
Radoš Tošković <rashmasch@users.noreply.github.com>
2024-01-12 17:01:40 +0100

    "fix: revert override for tooltip stacking order (#873)" (#878)
    This reverts commit 0a7847ea31f8d536704c53791209f855c0f94d9d.

--- src/components/tooltip/index.tsx
+++ src/components/tooltip/index.tsx
@@ -1,17 +1,12 @@
 import React, { cloneElement, FC, useState } from 'react';
 
-import {
-  Tooltip as ChakraTooltip,
-  Portal,
-  TooltipProps,
-} from '@chakra-ui/react';
+import { Tooltip as ChakraTooltip, TooltipProps } from '@chakra-ui/react';
 
 type Props = {
   children: React.ReactNode;
-  containerRef?: React.RefObject<HTMLElement | null>;
 } & Pick<TooltipProps, 'label' | 'placement' | 'maxWidth'>;
 
-const Tooltip: FC<Props> = ({ children, containerRef, ...props }) => {
+const Tooltip: FC<Props> = ({ children, ...props }) => {
   const [isLabelOpen, setIsLabelOpen] = useState<boolean>(false);
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -22,17 +17,15 @@ const Tooltip: FC<Props> = ({ children, containerRef, ...props }) => {
   });
 
   return (
-    <Portal containerRef={containerRef}>
-      <ChakraTooltip
-        hasArrow
-        placement="auto"
-        maxWidth="6xl"
-        isOpen={isLabelOpen}
-        {...props}
-      >
-        {childrenWithProps}
-      </ChakraTooltip>
-    </Portal>
+    <ChakraTooltip
+      hasArrow
+      placement="auto"
+      maxWidth="6xl"
+      isOpen={isLabelOpen}
+      {...props}
+    >
+      {childrenWithProps}
+    </ChakraTooltip>
   );
 };
 
