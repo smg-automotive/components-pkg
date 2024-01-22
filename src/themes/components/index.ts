d2caa632c7e3383d4c27498bc53da1a536d9bcd9
Filip Kiš <filip.kis@swissmarketplace.group>
2024-01-22 16:04:23 +0100

    feat: export popover tooltip component

--- src/themes/components/index.ts
+++ src/themes/components/index.ts
@@ -13,6 +13,7 @@ import SimpleHeader from './simpleHeader';
 import Select from './select';
 import Section from './section';
 import Radio from './radio';
+import Popover from './popover';
 import Pagination from './pagination';
 import NumberInput from './numberInput';
 import Modal from './modal';
@@ -61,6 +62,7 @@ export const components: Record<string, ComponentStyleConfig> = {
   Modal,
   NumberInput,
   Pagination,
+  Popover,
   Radio,
   Section,
   Select,
