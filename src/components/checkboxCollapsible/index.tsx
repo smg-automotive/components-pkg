56803dcf8c151fa68bb1f775b4273ed591dae6dd
VladanSMG <155618745+VladanSMG@users.noreply.github.com>
2024-02-07 10:47:17 +0100

    chore: remove unused code

--- src/components/checkboxCollapsible/index.tsx
+++ src/components/checkboxCollapsible/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, Fragment, useState } from 'react';
+import React, { FC, Fragment } from 'react';
 
 import { Box, Collapse, IconButton, useDisclosure } from '@chakra-ui/react';
 
