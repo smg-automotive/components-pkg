e8f74a1f55927157c77042471a42b6b4e6d8d499
mmataja <mmataja92@gmail.com>
2024-02-28 21:02:09 +0100

    chore: change tab prop

--- src/components/tab/Tab.tsx
+++ src/components/tab/Tab.tsx
@@ -3,22 +3,13 @@ import { Tab as ChakraTab, TabProps } from '@chakra-ui/react';
 
 import { Space } from 'src/themes';
 
-import Link, { LinkProps } from '../link';
-
 type Props = {
   marginX?: Space;
-  isLink?: boolean;
-  linkProps?: LinkProps;
+  href?: string;
 } & Pick<TabProps, 'isDisabled' | 'as'>;
 
 const Tab: FC<PropsWithChildren<Props>> = (props) => {
-  return props.isLink ? (
-    <ChakraTab {...props}>
-      <Link {...props.linkProps}>{props.children}</Link>
-    </ChakraTab>
-  ) : (
-    <ChakraTab {...props}>{props.children}</ChakraTab>
-  );
+  return <ChakraTab {...props}>{props.children}</ChakraTab>;
 };
 
 export default Tab;
