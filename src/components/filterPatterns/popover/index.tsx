f9f3901c6f71d655b27821e29567b2ff142fbfd0
mmataja <mmataja92@gmail.com>
2024-03-07 09:37:31 +0100

    fix: add height prop for popover filters

--- src/components/filterPatterns/popover/index.tsx
+++ src/components/filterPatterns/popover/index.tsx
@@ -33,6 +33,7 @@ export const PopoverFilter: FC<PopoverFilterProps> = ({
   showCallToActionButton = true,
   header,
   children,
+  height = 'md',
 }) => {
   const { onOpen, onClose, isOpen } = useDisclosure({
     defaultIsOpen: initialPopoverState === 'open',
@@ -84,7 +85,7 @@ export const PopoverFilter: FC<PopoverFilterProps> = ({
                   borderRightWidth={displayValue ? '1px' : undefined}
                   display="flex"
                   flex="1"
-                  h="md"
+                  height={height}
                   justifyContent="space-between"
                   minW="0"
                   paddingX="md"
