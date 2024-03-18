d9df829c1ddd2677f971e4ef7746186c09f6050d
Milos Zbiljic <milos.zbiljic@swissmarketplace.group>
2024-03-18 14:41:24 +0100

    fix: input radio props

--- src/components/radio/index.tsx
+++ src/components/radio/index.tsx
@@ -1,10 +1,10 @@
-import React, { ChangeEvent, forwardRef, ReactNode } from 'react';
+import React, { ChangeEvent, forwardRef } from 'react';
 import { Radio as ChakraRadio, RadioGroup } from '@chakra-ui/react';
 
 interface Props {
   name?: string;
   value: string;
-  label?: ReactNode;
+  label?: string;
   size?: 'sm' | 'md';
   onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
   isChecked?: boolean;
