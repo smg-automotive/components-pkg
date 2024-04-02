e1de2ce0f41df87dace80dfaf569aa29b144eacd
Krzysztof Sakwerda <ksakwerda@gmail.com>
2024-04-02 15:43:55 +0200

    chore: missing image

--- src/components/missingImage/index.stories.tsx
+++ src/components/missingImage/index.stories.tsx
@@ -1,21 +1,37 @@
 import React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+import { Description, Primary, Subtitle, Title } from '@storybook/blocks';
 
 import AspectRatio from '../aspectRatio';
 
 import MissingImageComponent from './index';
 
-const Template = (args) => (
-  <AspectRatio ratio={4 / 3} width="500px">
-    <MissingImageComponent {...args} />
-  </AspectRatio>
-);
-
-export default {
+const meta: Meta<typeof MissingImageComponent> = {
   title: 'Patterns/Data display/Missing image',
   component: MissingImageComponent,
+  parameters: {
+    docs: {
+      page: () => (
+        <>
+          <Title />
+          <Subtitle />
+          <Description />
+          <Primary />
+        </>
+      ),
+    },
+  },
 };
+export default meta;
 
-export const MissingImage = {
-  render: Template.bind({}),
+export const MissingImage: StoryObj<typeof MissingImageComponent> = {
   name: 'Missing image',
+
+  decorators: [
+    (Story) => (
+      <AspectRatio ratio={4 / 3} width="500px">
+        <Story />
+      </AspectRatio>
+    ),
+  ],
 };
