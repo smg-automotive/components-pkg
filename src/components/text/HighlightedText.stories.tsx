8fab39e4c50ca4738a5e287cd2054c2d6cd8de42
Krzysztof Sakwerda <ksakwerda@gmail.com>
2024-04-03 16:03:12 +0200

    chore: text and hightlighted text stories

---
+++ src/components/text/HighlightedText.stories.tsx
@@ -0,0 +1,19 @@
+import { Meta } from '@storybook/react';
+
+import HighlightedTextComponent from './HighlightedText';
+
+const meta: Meta<typeof HighlightedTextComponent> = {
+  title: 'Components/Utils/HighlightedText',
+  component: HighlightedTextComponent,
+
+  args: {
+    text: 'Mercedes',
+    highlightIndices: [
+      [1, 3],
+      [6, 6],
+    ],
+  },
+};
+export default meta;
+
+export const HighlightedText = {};
