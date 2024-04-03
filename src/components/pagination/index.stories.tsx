fff69adb052f09cd4e14d846ee22b7c308147580
Krzysztof Sakwerda <ksakwerda@gmail.com>
2024-04-03 09:54:23 +0200

    chore: pagination

--- src/components/pagination/index.stories.tsx
+++ src/components/pagination/index.stories.tsx
@@ -1,41 +1,58 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Meta } from '@storybook/react';
+import { useArgs } from '@storybook/client-api';
+import { action } from '@storybook/addon-actions';
+import { Box } from '@chakra-ui/react';
 
-import PaginationComponent from './index';
+import PaginationComponent, { Props } from './index';
 
-const Template = ({ count }) => {
-  const [currentPage, setPage] = useState(0);
+const Template = (props: Props) => {
+  const [args, updateArgs] = useArgs<Props>();
   return (
-    <div>
-      <div>{`Page index for API contract: ${currentPage}`}</div>
-      <PaginationComponent
-        marginTop="sm"
-        totalPages={count}
-        currentPage={currentPage}
-        onChange={(page) => setPage(page)}
-      />
-    </div>
+    <PaginationComponent
+      {...props}
+      {...args}
+      onChange={(page) => {
+        updateArgs({ currentPage: page });
+        action('onChange')(page);
+      }}
+    />
   );
 };
 
-export default {
+const meta: Meta<typeof PaginationComponent> = {
   title: 'Patterns/Navigation/Pagination',
   component: PaginationComponent,
-};
-
-export const Pagination = {
   render: Template.bind({}),
-  name: 'Pagination',
+
+  decorators: [
+    (Story) => {
+      const [args] = useArgs();
+
+      return (
+        <Box>
+          <div>{`Page index for API contract: ${args.currentPage}`}</div>
+          <Story />
+        </Box>
+      );
+    },
+  ],
 
   args: {
-    count: 10,
+    totalPages: 10,
+    currentPage: 0,
   },
 
   argTypes: {
-    count: {
+    totalPages: {
       control: {
         type: 'number',
         min: 0,
+        step: 1,
       },
     },
   },
 };
+export default meta;
+
+export const Pagination = {};
