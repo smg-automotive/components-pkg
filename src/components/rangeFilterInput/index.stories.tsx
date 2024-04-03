2d683a7cb6163eb4052f41ed251fa0c185f38f69
Krzysztof Sakwerda <ksakwerda@gmail.com>
2024-04-03 10:56:37 +0200

    chore: range input

--- src/components/rangeFilterInput/index.stories.tsx
+++ src/components/rangeFilterInput/index.stories.tsx
@@ -1,46 +1,22 @@
+import React from 'react';
+import { Meta } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
 import { Box } from '@chakra-ui/react';
 
-import RangeFilterInput from './index';
+import RangeFilterInputComponent from './index';
 
-const Template = ({ from, to, unit, ...rest }) => {
-  return (
-    <Box maxW={270}>
-      <RangeFilterInput
-        from={from}
-        to={to}
-        handleChange={(event) => {
-          action('change')(event);
-        }}
-        onBlur={(event) => {
-          action('onBlur')(event);
-        }}
-        unit={unit}
-        {...rest}
-      />
-    </Box>
-  );
-};
-
-export default {
+const meta: Meta<typeof RangeFilterInputComponent> = {
   title: 'Components/Filter/Range Input',
-  component: RangeFilterInput,
+  component: RangeFilterInputComponent,
 
-  argTypes: {
-    isDisabled: {
-      control: 'boolean',
-    },
-  },
-
-  parameters: {
-    actions: ['change', 'onBlur'],
-  },
-};
-
-export const Overview = {
-  render: Template.bind({}),
-  name: 'Overview',
+  decorators: [
+    (Story) => (
+      <Box maxW={270}>
+        <Story />
+      </Box>
+    ),
+  ],
 
   args: {
     unit: 'CHF',
@@ -54,41 +30,43 @@ export const Overview = {
       name: 'priceTo',
       placeholder: 'To',
     },
+
+    handleChange: action('handleChange'),
+    onBlur: action('onBlur'),
+  },
+
+  argTypes: {
+    isDisabled: {
+      control: 'boolean',
+    },
   },
 };
+export default meta;
+
+export const RangeFilterInput = {};
 
 export const WithInitialValue = {
-  render: Template.bind({}),
   name: 'With initial value',
 
   args: {
-    unit: 'CHF',
-
     from: {
       name: 'priceFrom',
+      placeholder: 'From',
       value: 100,
     },
 
     to: {
       name: 'priceTo',
+      placeholder: 'To',
       value: 200,
     },
   },
 };
 
 export const WithoutUnit = {
-  render: Template.bind({}),
   name: 'Without unit',
 
   args: {
-    from: {
-      name: 'priceFrom',
-      value: 100,
-    },
-
-    to: {
-      name: 'priceTo',
-      value: 200,
-    },
+    unit: undefined,
   },
 };
