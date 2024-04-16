5b5a81909d801080c347886af623be2f24f7f87a
Krzysztof Sakwerda <ksakwerda@gmail.com>
2024-04-16 09:41:34 +0200

    chore: try if display name have impact on docs

--- src/components/avatar/index.tsx
+++ src/components/avatar/index.tsx
@@ -20,5 +20,6 @@ const Avatar: FC<Props> = ({ withNotification, color }) => {
     <AvatarIcon __css={baseStyle} {...(color && { color })} />
   );
 };
+Avatar.displayName = 'Avatar';
 
 export default Avatar;
