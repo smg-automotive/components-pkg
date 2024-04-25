284e9bf1771b8b244958304e1892cf7030306517
Colin Talamona <71456764+talamcol@users.noreply.github.com>
2024-04-25 11:55:58 +0200

    feat: add tracking to main header items (#1014)
    * feat: add tracking to main header items
    
    * feat: add tracking to main header items
    
    * chore: add to story
    
    * chore: simplify advanced search link
    
    * fix: add tracking to the ones below the menu on small screens
    
    * fix: remove event label
    
    * fix: wrong visibility setting

---
+++ src/types/tracking.ts
@@ -0,0 +1,7 @@
+export type CustomEvent = {
+  eventAction: string;
+  eventCategory: string;
+  eventLabel?: string | null;
+};
+
+export const navigationEventCategory = 'www_Interaction_PI-Navigation';
