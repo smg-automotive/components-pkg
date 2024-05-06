83f2bf4d6b64b7e58dc0da0c043afe8ebd505ac9
Marine Berthier <marineberthier@w1960.home>
2024-05-06 16:51:55 +0200

    fix: link alignment in Alert box

--- src/components/alert/index.tsx
+++ src/components/alert/index.tsx
@@ -45,7 +45,7 @@ const Alert: FC<AlertProps> = ({
         <AlertDescription>{description}</AlertDescription>
         {link ? (
           link.as === 'button' ? (
-            <Link onClick={() => link.onClick?.()}>{link.text}</Link>
+            <Link as="button" onClick={() => link.onClick?.()} textAlign='left'>{link.text}</Link>
           ) : (
             <Link href={link.url} isExternal={link.isExternal}>
               {link.text}
