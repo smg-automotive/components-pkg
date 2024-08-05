1e102499f623c8b8ea600562392cca50aee53260
Milos Zbiljic <milos.zbiljic@swissmarketplace.group>
2024-08-05 17:11:54 +0200

    feat: tabpanel margin

--- src/components/tab/TabPanel.tsx
+++ src/components/tab/TabPanel.tsx
@@ -1,8 +1,11 @@
 import React, { FC, PropsWithChildren } from 'react';
-import { TabPanel as ChakraTabPanel } from '@chakra-ui/react';
+import { TabPanel as ChakraTabPanel, TabPanelProps } from '@chakra-ui/react';
 
-const TabPanel: FC<PropsWithChildren> = (props) => {
-  return <ChakraTabPanel>{props.children}</ChakraTabPanel>;
+const TabPanel: FC<PropsWithChildren<Pick<TabPanelProps, 'margin'>>> = ({
+  margin = 'sm',
+  children,
+}) => {
+  return <ChakraTabPanel margin={margin}>{children}</ChakraTabPanel>;
 };
 
 export default TabPanel;
