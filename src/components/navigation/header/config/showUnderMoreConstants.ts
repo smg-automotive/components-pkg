52839b6e51c02473fa6f724aa94ca3d84e2a27d4
talamcol <colin@talamona.ch>
2024-08-07 13:38:22 +0200

    fix: optimize numbers

--- src/components/navigation/header/config/showUnderMoreConstants.ts
+++ src/components/navigation/header/config/showUnderMoreConstants.ts
@@ -1,9 +1,14 @@
+import { breakpoints } from 'src/themes';
+
 export type ShowUnderMoreBreakpoint = `(max-width: ${number}px)`;
 
-export const sell: ShowUnderMoreBreakpoint = '(max-width: 500px)';
-export const estimate: ShowUnderMoreBreakpoint = '(max-width: 800px)';
-export const assure: ShowUnderMoreBreakpoint = '(max-width: 900px)';
-export const electromobility: ShowUnderMoreBreakpoint = '(max-width: 1200px)';
-export const magazine: ShowUnderMoreBreakpoint = '(max-width: 1200px)';
-export const showMoreDrawer = '(min-width: 768px) and (max-width: 1024px)';
-export const showMoreDrawerInsideSearchDrawer = '(max-width: 768px)';
+const minWidthMore = 500;
+const maxWidthMore = breakpoints.md.px;
+
+export const sell: ShowUnderMoreBreakpoint = `(max-width: ${breakpoints.sm.px}px)`;
+export const estimate: ShowUnderMoreBreakpoint = `(max-width: ${maxWidthMore}px)`;
+export const assure: ShowUnderMoreBreakpoint = `(max-width: ${maxWidthMore}px)`;
+export const electromobility: ShowUnderMoreBreakpoint = `(max-width: ${maxWidthMore}px)`;
+export const magazine: ShowUnderMoreBreakpoint = `(max-width: ${maxWidthMore}px)`;
+export const showMoreDrawer = `(min-width: ${minWidthMore}px) and (max-width: ${maxWidthMore}px)`;
+export const showMoreDrawerInsideSearchDrawer = `(max-width: ${minWidthMore - 1}px)`;
