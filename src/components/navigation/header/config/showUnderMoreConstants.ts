37f39059dd289860eb27741ae4f3f797dccbc226
talamcol <colin@talamona.ch>
2024-08-07 16:00:22 +0200

    fix: optimize numbers

--- src/components/navigation/header/config/showUnderMoreConstants.ts
+++ src/components/navigation/header/config/showUnderMoreConstants.ts
@@ -2,13 +2,13 @@ import { breakpoints } from 'src/themes';
 
 export type ShowUnderMoreBreakpoint = `(max-width: ${number}px)`;
 
-const minWidthMore = 500;
-const maxWidthMore = breakpoints.md.px;
+const minWidthMore = breakpoints.sm.px;
+const maxWidthMore = breakpoints.lg.px;
 
 export const sell: ShowUnderMoreBreakpoint = `(max-width: ${breakpoints.sm.px}px)`;
-export const estimate: ShowUnderMoreBreakpoint = `(max-width: ${maxWidthMore}px)`;
+export const estimate: ShowUnderMoreBreakpoint = `(max-width: ${breakpoints.md.px}px)`;
 export const assure: ShowUnderMoreBreakpoint = `(max-width: ${maxWidthMore}px)`;
 export const electromobility: ShowUnderMoreBreakpoint = `(max-width: ${maxWidthMore}px)`;
 export const magazine: ShowUnderMoreBreakpoint = `(max-width: ${maxWidthMore}px)`;
 export const showMoreDrawer = `(min-width: ${minWidthMore}px) and (max-width: ${maxWidthMore}px)`;
-export const showMoreDrawerInsideSearchDrawer = `(max-width: ${minWidthMore - 1}px)`;
+export const showMoreDrawerInsideSearchDrawer: ShowUnderMoreBreakpoint = `(max-width: ${minWidthMore - 1}px)`;
