8edca672cd6d1f4035f37cb51767d4779b4bd92b
talamcol <colin@talamona.ch>
2024-08-13 07:10:29 +0200

    fix: make reference more configurable

--- src/components/vehicleReference/index.stories.tsx
+++ src/components/vehicleReference/index.stories.tsx
@@ -9,7 +9,7 @@ const meta: Meta<typeof VehicleReference> = {
   component: VehicleReference,
   decorators: [
     (Story) => (
-      <Box maxW={{ '2xs': '100%', md: '450px' }}>
+      <Box maxW={{ '2xs': '100%', md: '600px' }}>
         <Story />
       </Box>
     ),
@@ -35,7 +35,7 @@ export const VehicleReferenceWithImage: StoryType = {
     sellerName: `Auto-Center Grenchen AG 2540 Grenchen (SO)`,
     sellerAddress: `2540 Grenchen (SO)`,
     image: <img src="https://picsum.photos/400/400" />,
-    direction: { '2xs': 'row', md: 'column' },
+    templateColumns: { base: '96px 1fr', md: '240px 1fr' },
   },
 };
 
--- src/components/vehicleReference/index.tsx
+++ src/components/vehicleReference/index.tsx
@@ -5,6 +5,7 @@ import { Box, chakra, useMultiStyleConfig } from '@chakra-ui/react';
 import Stack from '../stack';
 
 import MissingImage from '../missingImage';
+import Grid from '../grid';
 import AspectRatio from '../aspectRatio';
 
 interface Props {
@@ -14,7 +15,7 @@ interface Props {
   sellerName?: string | null;
   sellerAddress?: string | null;
   callToAction?: ReactNode;
-  direction?: ComponentProps<typeof Stack>['direction'];
+  templateColumns?: ComponentProps<typeof Grid>['templateColumns'];
 }
 
 const VehicleReference: FC<Props> = ({
@@ -24,34 +25,27 @@ const VehicleReference: FC<Props> = ({
   sellerName,
   sellerAddress,
   callToAction,
-  direction = { '2xs': 'row', md: 'column' },
+  templateColumns = { base: '96px 1fr', md: '1fr' },
 }) => {
   const styles = useMultiStyleConfig(`VehicleReference`);
 
   return (
-    <article>
-      <Stack direction={direction} spacing="md">
-        <AspectRatio
-          minW="2xl"
-          ratio={4 / 3}
-          borderRadius="sm"
-          overflow="hidden"
-        >
-          {image ? image : <MissingImage />}
-        </AspectRatio>
-        <Stack spacing={{ '2xs': 'xs', md: 'md' }} justify="center">
-          <chakra.h1 __css={styles.carTitle}>{vehicleTitle}</chakra.h1>
-          <chakra.span __css={styles.price}>{price}</chakra.span>
-          <Box>
-            <chakra.p __css={styles.dealerName}>{sellerName}</chakra.p>
-            <chakra.p __css={styles.dealerAddress}>{sellerAddress}</chakra.p>
-          </Box>
-        </Stack>
+    <Grid as="article" templateColumns={templateColumns} gap="md">
+      <AspectRatio ratio={4 / 3} borderRadius="sm" overflow="hidden">
+        {image ? image : <MissingImage />}
+      </AspectRatio>
+      <Stack spacing={{ base: 'xs', md: 'md' }} justify="center">
+        <chakra.h1 __css={styles.carTitle}>{vehicleTitle}</chakra.h1>
+        <chakra.span __css={styles.price}>{price}</chakra.span>
+        <Box>
+          <chakra.p __css={styles.dealerName}>{sellerName}</chakra.p>
+          <chakra.p __css={styles.dealerAddress}>{sellerAddress}</chakra.p>
+        </Box>
       </Stack>
       {callToAction ? (
         <Box marginTop={{ base: 'lg', md: 'sm' }}>{callToAction}</Box>
       ) : null}
-    </article>
+    </Grid>
   );
 };
 
