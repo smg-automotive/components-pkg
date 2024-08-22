f573f7d242cfa1dc505d8628c395714683e053c7
Colin Talamona <71456764+talamcol@users.noreply.github.com>
2024-08-22 16:36:04 +0200

    fix: revert slides per view change (#1125)
    * Revert "feat: allow to show multiple slides per view in the carousel (#1119)"
    
    This reverts commit ad26fdf72a090aee3aa640192a224013207e7be6.
    
    * fix: trigger version

--- src/components/carousel/index.stories.tsx
+++ src/components/carousel/index.stories.tsx
@@ -117,14 +117,6 @@ export const StartingFromSpecificSlide: StoryType = {
   },
 };
 
-export const SlidesPerView: StoryType = {
-  args: {
-    slidesPerView: { base: 1, sm: 2, md: 3, lg: 4 },
-    loop: false,
-    slidesToScroll: 'auto',
-  },
-};
-
 export const FullScreen: StoryType = {
   parameters: {
     layout: 'fullscreen',
