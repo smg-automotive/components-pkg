4033277bb78fddc036bb99a5e90ab8ce6dde62e8
talamcol <colin@talamona.ch>
2024-08-26 11:02:55 +0200

    fix: simplify top listing badge

--- src/components/topListingBadge/index.tsx
+++ src/components/topListingBadge/index.tsx
@@ -1,8 +1,7 @@
 import React, { FC, PropsWithChildren } from 'react';
 import { Badge } from '@chakra-ui/react';
 
-import GridItem from '../grid/GridItem';
-import Grid from '../grid';
+import Box from '../box';
 import AspectRatio from '../aspectRatio';
 
 type Props = {
@@ -14,35 +13,29 @@ const TopListingBadge: FC<PropsWithChildren<Props>> = ({
   aspectRatio,
 }) => {
   return (
-    <Grid>
-      <GridItem gridColumn={1} gridRow={1}>
-        {aspectRatio ? (
-          <AspectRatio ratio={aspectRatio}>{children}</AspectRatio>
-        ) : (
-          children
-        )}
-      </GridItem>
-      <GridItem
-        gridColumn={1}
-        gridRow={1}
+    <Box position="relative" display="inline-block">
+      {aspectRatio ? (
+        <AspectRatio ratio={aspectRatio}>{children}</AspectRatio>
+      ) : (
+        children
+      )}
+      <Badge
+        position="absolute"
+        top={0}
+        left={0}
+        transform="rotate(-45deg) translateX(-50%) translateY(9px)"
+        transformOrigin="top left"
+        width="70px"
+        textAlign="center"
+        paddingLeft="sm"
+        color="gray.900"
         zIndex="docked"
-        overflow="hidden"
-        position="relative"
         pointerEvents="none"
         __css={{ touchAction: 'none' }}
       >
-        <Badge
-          transform="rotate(-45deg) translateX(-50%) translateY(9px)"
-          transformOrigin="top left"
-          width="70px"
-          textAlign="center"
-          paddingLeft="sm"
-          color="gray.900"
-        >
-          Top
-        </Badge>
-      </GridItem>
-    </Grid>
+        Top
+      </Badge>
+    </Box>
   );
 };
 
