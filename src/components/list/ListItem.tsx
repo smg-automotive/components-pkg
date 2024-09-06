5d31a3643b145a30484731d7b07f3c586cdf9d2e
Lorenzo Kappeler <me@lnk.codes>
2024-09-06 12:52:49 +0200

    chore: export list item from chakra

--- src/components/list/ListItem.tsx
+++ src/components/list/ListItem.tsx
@@ -1,11 +1,2 @@
-import React, { FC, PropsWithChildren } from 'react';
-import { ListItem as ChakraListItem, ListItemProps } from '@chakra-ui/react';
-
-const ListItem: FC<PropsWithChildren<ListItemProps>> = ({
-  children,
-  ...rest
-}) => {
-  return <ChakraListItem {...rest}>{children}</ChakraListItem>;
-};
-
+import { ListItem } from '@chakra-ui/react';
 export default ListItem;
