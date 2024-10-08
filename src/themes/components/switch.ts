a5fe3c9e41a5ba95192682498b173a5eaee84bef
Damir Zečević <153915313+damirzecevicsmg@users.noreply.github.com>
2024-10-08 15:17:16 +0200

    fix: Fix window is not defined and remove unnecessary svg. (#1156)
    * fix: Fix window is not defined and remove unnecessary svg.
    
    * fix: Fix color of checkmark from black to gray.900.

--- src/themes/components/switch.ts
+++ src/themes/components/switch.ts
@@ -56,10 +56,18 @@ const computedStyleTrack: SystemStyleFunction = (props) => {
   return baseStyleTrack({ ...props, bg, checkedBg });
 };
 
-const svgCheck = `<svg xmlns="http://www.w3.org/2000/svg" width="10" height="8"><path d="M3.389 7.834a.52.52 0 0 0 .722 0l5.743-5.742a.52.52 0 0 0 0-.723L9.15.666a.495.495 0 0 0-.703 0L3.76 5.354 1.553 3.166a.495.495 0 0 0-.703 0l-.704.703a.52.52 0 0 0 0 .723z"/></svg>`;
-const svgCheckHovered = `<svg xmlns="http://www.w3.org/2000/svg" width="10" height="8"><path fill="#5D5D5D" d="M3.389 7.834a.52.52 0 0 0 .722 0l5.743-5.742a.52.52 0 0 0 0-.723L9.15.666a.495.495 0 0 0-.703 0L3.76 5.354 1.553 3.166a.495.495 0 0 0-.703 0l-.704.703a.52.52 0 0 0 0 .723z"/></svg>`;
-const encodedSvg = window.btoa(svgCheck);
-const encodedSvgHovered = window.btoa(svgCheckHovered);
+const checkmark = {
+  content: '""',
+  display: 'block',
+  width: '25%',
+  height: '45%',
+  border: 'solid',
+  borderWidth: '0 2px 2px 0',
+  position: 'absolute',
+  top: '45%',
+  left: '50%',
+  transform: 'translate(-50%, -50%) rotate(45deg)',
+};
 
 const baseStyleThumb: SystemStyleObject = {
   bg: 'white',
@@ -70,19 +78,14 @@ const baseStyleThumb: SystemStyleObject = {
   height: [$height.reference],
   _checked: {
     transform: `translateX(${$translateX.reference})`,
-    '::after': {
-      content: `""`,
-      position: 'absolute',
-      top: '50%',
-      left: '50%',
-      transform: 'translate(-50%, -50%)',
-      width: '10px',
-      height: '7.46px',
-      background: `url("data:image/svg+xml;base64,${encodedSvg}") center / cover no-repeat`,
+    _after: {
+      ...checkmark,
+      borderColor: 'gray.900',
     },
     _hover: {
-      '::after': {
-        background: `url("data:image/svg+xml;base64,${encodedSvgHovered}") center / cover no-repeat`,
+      _after: {
+        ...checkmark,
+        borderColor: 'gray.700',
       },
     },
   },
