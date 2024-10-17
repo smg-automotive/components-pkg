e50e5a737eb3a40d3658147f99c01a549de8a059
boyarskiy <gekabrs@gmail.com>
2024-10-17 10:16:37 +0200

    fix: use current color, remove duplications

--- src/components/icons/NotAvailableIcon.tsx
+++ src/components/icons/NotAvailableIcon.tsx
@@ -8,7 +8,7 @@ export const NotAvailableIcon: ComponentWithAs<'svg', IconProps> = createIcon({
   path: (
     <>
       <title>Not Available</title>
-      <circle cx="12" cy="12" r="12" fill="#7F97A1" />
+      <circle cx="12" cy="12" r="12" fill="currentColor" />
       <path d="M18 11.3334H6V12.6667H18V11.3334Z" fill="white" />
     </>
   ),
