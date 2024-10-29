e75d533730fafd8dcc9dde5d401c02b00a94c273
Krzysztof Sakwerda <ksakwerda@gmail.com>
2024-10-29 10:34:15 +0100

    chore: extend the shared providers

--- .jest/utils.tsx
+++ .jest/utils.tsx
@@ -1,6 +1,12 @@
 /* eslint-disable unicorn/filename-case */
-import React, { FC, PropsWithChildren, ReactElement } from 'react';
-import { render as testingLibraryRender } from '@testing-library/react';
+import React, { FC, Fragment, PropsWithChildren, ReactElement } from 'react';
+import {
+  Queries,
+  RenderHookOptions,
+  RenderOptions,
+  render as testingLibraryRender,
+  renderHook as testingLibraryRenderHook,
+} from '@testing-library/react';
 
 import { Brand } from 'src/types/brand';
 
@@ -8,9 +14,21 @@ import ThemeProvider, {
   type Props as ThemeProviderProps,
 } from '../src/components/themeProvider';
 
-const Wrapper = (theme: ThemeProviderProps['theme']): FC<PropsWithChildren> => {
+type WrapperOptions = {
+  theme?: ThemeProviderProps['theme'];
+  wrapper?: RenderOptions['wrapper'];
+};
+
+const WrapperFactory = ({
+  theme = Brand.AutoScout24,
+  wrapper: Wrapper = Fragment,
+}: WrapperOptions) => {
   const ThemeRenderer: FC<PropsWithChildren> = ({ children }) => {
-    return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+    return (
+      <ThemeProvider theme={theme}>
+        <Wrapper>{children}</Wrapper>
+      </ThemeProvider>
+    );
   };
   return ThemeRenderer;
 };
@@ -21,9 +39,37 @@ export * from '@testing-library/react';
 // eslint-disable-next-line import/export
 export const render = (
   ui: ReactElement,
-  options = { theme: Brand.AutoScout24 as const },
+  options: RenderOptions & WrapperOptions = {},
 ) => {
-  const { theme, ...rest } = options;
-  return testingLibraryRender(ui, { wrapper: Wrapper(theme), ...rest });
+  const { theme, wrapper, ...renderOptions } = options;
+  return testingLibraryRender(ui, {
+    wrapper: WrapperFactory({
+      theme,
+      wrapper,
+    }),
+    ...renderOptions,
+  });
+};
+
+// eslint-disable-next-line import/export
+export const renderHook = <
+  Result,
+  Props,
+  Q extends Queries,
+  Container extends DocumentFragment,
+  BaseElement extends DocumentFragment,
+>(
+  hook: (props: Props) => Result,
+  options: RenderHookOptions<Props, Q, Container, BaseElement> &
+    WrapperOptions = {},
+) => {
+  const { theme, wrapper, ...renderOptions } = options;
+  return testingLibraryRenderHook(hook, {
+    wrapper: WrapperFactory({
+      theme,
+      wrapper,
+    }),
+    ...renderOptions,
+  });
 };
 export { testingLibraryRender };
