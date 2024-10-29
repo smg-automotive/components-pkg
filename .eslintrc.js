991c53bdb48d0c8827c0bdec7c7783e68245196c
Krzysztof Sakwerda <ksakwerda@gmail.com>
2024-10-29 10:56:14 +0100

    chore: remove leftovers of mdx setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,6 @@ module.exports = {
         devDependencies: [
           './**/stories/**/*',
           './**/*.stories.tsx',
-          './**/*.mdx',
           './**/*.Test.@(ts|tsx)',
           'rollup.config.mjs',
           'postcss.config.js',
@@ -33,28 +32,6 @@ module.exports = {
     ],
   },
   overrides: [
-    {
-      files: ['*.mdx'],
-      extends: [
-        'plugin:@typescript-eslint/recommended',
-        'plugin:import/typescript',
-        'plugin:mdx/recommended',
-      ],
-      settings: {
-        'mdx/code-blocks': true,
-        'import/resolver': {
-          typescript: {},
-        },
-        'import/ignore': ['@storybook/addon-docs'],
-      },
-      rules: {
-        'no-nested-ternary': 'off',
-        'react/jsx-props-no-spreading': 'off',
-        'react/prop-types': 'off',
-        'import/namespace': ['error', { allowComputed: true }],
-        'sonarjs/no-duplicate-string': 'off',
-      },
-    },
     {
       files: ['*.stories.@(ts|tsx)'],
       rules: {
