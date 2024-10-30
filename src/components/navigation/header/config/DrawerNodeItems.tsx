d3f1a3b0a558d5ed2fd06f5e898afc92bfa1587c
Marine-Berthier <52455155+Marine-Berthier@users.noreply.github.com>
2024-10-30 10:26:54 +0100

    fix: add infoportal link (#1179)

--- src/components/navigation/header/config/DrawerNodeItems.tsx
+++ src/components/navigation/header/config/DrawerNodeItems.tsx
@@ -1493,6 +1493,25 @@ export const drawerNodeItems = ({
             },
           },
         },
+        {
+          translationKey: 'header.userMenu.infoPortal',
+          link: {
+            de: 'https://b2b.autoscout24.ch/',
+            en: 'https://b2b.autoscout24.ch/',
+            fr: 'https://b2b.autoscout24.ch/fr/',
+            it: 'https://b2b.autoscout24.ch/it/',
+          },
+          visibilitySettings: {
+            userType: {
+              private: false,
+              professional: true,
+            },
+            brand: {
+              autoscout24: true,
+              motoscout24: false,
+            },
+          },
+        },
       ],
     },
   ],
