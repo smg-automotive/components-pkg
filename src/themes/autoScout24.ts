6a40af93a53159b6b3a799a6b1c807b324ae81fc
Krzysztof Sakwerda <ksakwerda@gmail.com>
2024-11-06 11:10:44 +0100

    chore: merge shared into the theme for now

--- src/themes/autoScout24.ts
+++ src/themes/autoScout24.ts
@@ -1,42 +1,41 @@
 import {
   createSystem,
   defaultBaseConfig,
-  defineConfig,
+  mergeConfigs,
 } from '@chakra-ui/react';
 
 import { sharedConfig } from './shared';
 
-const autoScout24Config = defineConfig({
-  ...sharedConfig,
-  theme: {
-    ...sharedConfig.theme,
-    semanticTokens: {
-      colors: {
-        brand: {
-          primary: { value: '{colors.brand.100}' },
-          btnShadow: { value: '{colors.brand.300}' },
+const autoScout24Config = mergeConfigs(
+  {
+    theme: {
+      semanticTokens: {
+        colors: {
+          brand: {
+            primary: { value: '{colors.brand.100}' },
+            btnShadow: { value: '{colors.brand.300}' },
+          },
         },
       },
-    },
-    tokens: {
-      ...sharedConfig.theme.tokens,
-      colors: {
-        ...sharedConfig.theme.tokens.colors,
-        brand: {
-          50: { value: '#FEFCD0' },
-          100: { value: '#F5F200' },
-          200: { value: '#DCD500' },
-          300: { value: '#C1B900' },
-          400: { value: '#9E9600' },
-          500: { value: '#908800' },
-          600: { value: '#827900' },
-          700: { value: '#665E00' },
-          800: { value: '#4C4500' },
-          900: { value: '#3A3400' },
+      tokens: {
+        colors: {
+          brand: {
+            50: { value: '#FEFCD0' },
+            100: { value: '#F5F200' },
+            200: { value: '#DCD500' },
+            300: { value: '#C1B900' },
+            400: { value: '#9E9600' },
+            500: { value: '#908800' },
+            600: { value: '#827900' },
+            700: { value: '#665E00' },
+            800: { value: '#4C4500' },
+            900: { value: '#3A3400' },
+          },
         },
       },
     },
   },
-});
+  sharedConfig,
+);
 
 export default createSystem(defaultBaseConfig, autoScout24Config);
--- src/themes/motoScout24.ts
+++ src/themes/motoScout24.ts
@@ -1,42 +1,41 @@
 import {
   createSystem,
   defaultBaseConfig,
-  defineConfig,
+  mergeConfigs,
 } from '@chakra-ui/react';
 
 import { sharedConfig } from './shared';
 
-const motoScout24Config = defineConfig({
-  ...sharedConfig,
-  theme: {
-    ...sharedConfig.theme,
-    semanticTokens: {
-      colors: {
-        brand: {
-          primary: { value: '{colors.brand.400}' },
-          btnShadow: { value: '{colors.brand.600}' },
+const motoScout24Config = mergeConfigs(
+  {
+    theme: {
+      semanticTokens: {
+        colors: {
+          brand: {
+            primary: { value: '{colors.brand.400}' },
+            btnShadow: { value: '{colors.brand.600}' },
+          },
         },
       },
-    },
-    tokens: {
-      ...sharedConfig.theme.tokens,
-      colors: {
-        ...sharedConfig.theme.tokens.colors,
-        brand: {
-          50: { value: '#FFF8F9' },
-          100: { value: '#FFE4E5' },
-          200: { value: '#FFC0C2' },
-          300: { value: '#FD7978' },
-          400: { value: '#FF4C52' },
-          500: { value: '#EF474D' },
-          600: { value: '#D64045' },
-          700: { value: '#A83236' },
-          800: { value: '#7D2528' },
-          900: { value: '#5F1C1E' },
+      tokens: {
+        colors: {
+          brand: {
+            50: { value: '#FFF8F9' },
+            100: { value: '#FFE4E5' },
+            200: { value: '#FFC0C2' },
+            300: { value: '#FD7978' },
+            400: { value: '#FF4C52' },
+            500: { value: '#EF474D' },
+            600: { value: '#D64045' },
+            700: { value: '#A83236' },
+            800: { value: '#7D2528' },
+            900: { value: '#5F1C1E' },
+          },
         },
       },
     },
   },
-});
+  sharedConfig,
+);
 
 export default createSystem(defaultBaseConfig, motoScout24Config);
