bb7040dea5b90b63cffec955b059919e54539a3a
Krzysztof Sakwerda <ksakwerda@gmail.com>
2024-11-13 13:38:01 +0100

    fix: migrate new icons

--- src/components/icons/ThumbsUpIcon.tsx
+++ src/components/icons/ThumbsUpIcon.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { ComponentWithAs } from '@chakra-ui/system';
-import { createIcon, IconProps } from '@chakra-ui/react';
+import { createIcon } from '@chakra-ui/react';
 
-export const ThumbsUpIcon: ComponentWithAs<'svg', IconProps> = createIcon({
+export const ThumbsUpIcon = createIcon({
   displayName: 'Thumbs up',
   viewBox: '0 0 24 24',
   path: (
