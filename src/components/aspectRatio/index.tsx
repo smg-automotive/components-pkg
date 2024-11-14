170e30edf7b6ba49ffe6a759ed82c56f348f9bfa
Krzysztof Sakwerda <ksakwerda@gmail.com>
2024-11-14 15:03:51 +0100

    fix: display name for AspectRatio

--- src/components/aspectRatio/index.tsx
+++ src/components/aspectRatio/index.tsx
@@ -1,1 +1,7 @@
-export { AspectRatio, type AspectRatioProps } from '@chakra-ui/react';
+export { type AspectRatioProps } from '@chakra-ui/react';
+import { AspectRatio as ChakraAspectRatio } from '@chakra-ui/react';
+
+const AspectRatio = ChakraAspectRatio;
+AspectRatio.displayName = 'AspectRatio';
+
+export { AspectRatio };
