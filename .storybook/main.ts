7aedb8e253a24071b26a86c1fd950c771271ccd3
Krzysztof Sakwerda <ksakwerda@gmail.com>
2024-11-21 09:45:57 +0100

    chore: better react-docgen setup

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,8 @@
 import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin';
 import type { StorybookConfig } from '@storybook/react-webpack5';
 
+import { autoScout24System } from '../src/themes';
+
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -24,6 +26,34 @@ const config: StorybookConfig = {
   },
   typescript: {
     reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      compilerOptions: {
+        esModuleInterop: false,
+        allowSyntheticDefaultImports: false,
+      },
+      shouldExtractLiteralValuesFromEnum: true,
+      shouldRemoveUndefinedFromOptional: true,
+      propFilter: (prop) => {
+        const isWhitelistedProp = ['variant', 'size'].includes(prop.name);
+
+        // Excludes styling props defined by chakra
+        const isExcludedProp = ['as', 'asChild', 'recipe'].includes(prop.name);
+        const isStyledSystemProp = autoScout24System.isValidProperty(prop.name);
+
+        // Excludes HTML attributes and DOM properties coming (mostly) from react
+        const isHTMLProp = prop.parent?.name?.match(/^(html|dom)/i) ?? false;
+        const isReactProp =
+          prop.parent?.fileName?.includes('node_modules/@types/react') ?? false;
+
+        return (
+          isWhitelistedProp ||
+          (!isExcludedProp &&
+            !isStyledSystemProp &&
+            !isHTMLProp &&
+            !isReactProp)
+        );
+      },
+    },
   },
   staticDirs: [
     {
