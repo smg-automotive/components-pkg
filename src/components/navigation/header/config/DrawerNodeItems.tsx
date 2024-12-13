a493af0c37d50a4f6fd2f4939ddd1f1d272e1f7e
Yevgeny <gekabrs@gmail.com>
2024-12-13 10:44:39 +0100

    feat: add partner hub link to user menu (#1239)

--- src/components/navigation/header/config/DrawerNodeItems.tsx
+++ src/components/navigation/header/config/DrawerNodeItems.tsx
@@ -72,10 +72,12 @@ const getComparisonNodeItem = ({
 
 export const drawerNodeItems = ({
   comparisonItemIds,
+  sellerId,
   trackEvent,
   onLogout,
 }: {
   comparisonItemIds?: number[] | null;
+  sellerId?: string;
   trackEvent?: (event: CustomEvent) => void;
   onLogout: () => void;
 }): DrawerNodeItemsConfig => ({
@@ -689,6 +691,25 @@ export const drawerNodeItems = ({
             brand: {
               autoscout24: true,
               motoscout24: true,
+            },
+          },
+        },
+        {
+          translationKey: 'header.userMenu.partnerHub',
+          link: {
+            de: `https://partnerhub.financescout24.ch/de/landing?dealerId=${sellerId}&utm_source=autoscout24.ch&utm_medium=web&utm_campaign=link_as24_cardealer_portal_headernavigation`,
+            en: `https://partnerhub.financescout24.ch/de/landing?dealerId=${sellerId}&utm_source=autoscout24.ch&utm_medium=web&utm_campaign=link_as24_cardealer_portal_headernavigation`,
+            fr: `https://partnerhub.financescout24.ch/fr/landing?dealerId=${sellerId}&utm_source=autoscout24.ch&utm_medium=web&utm_campaign=link_as24_cardealer_portal_headernavigation`,
+            it: `https://partnerhub.financescout24.ch/it/landing?dealerId=${sellerId}&utm_source=autoscout24.ch&utm_medium=web&utm_campaign=link_as24_cardealer_portal_headernavigation`,
+          },
+          visibilitySettings: {
+            userType: {
+              private: false,
+              professional: true,
+            },
+            brand: {
+              autoscout24: true,
+              motoscout24: false,
             },
           },
         },
