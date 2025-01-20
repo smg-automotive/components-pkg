300534ee5cb71becc7c31d7e7e5bacfa9c9a48e7
Marko Markovic <marko.markovic@swissmarketplace.group>
2025-01-20 13:52:11 +0100

    feat: migrate simple grid component to chakra v3

---
+++ src/components/simpleGrid/index.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from 'react';
+import {
+  SimpleGrid as ChakraSimpleGrid,
+  SimpleGridProps,
+} from '@chakra-ui/react';
+
+type Props = Pick<
+  SimpleGridProps,
+  | 'minChildWidth'
+  | 'columns'
+  | 'children'
+  | 'alignItems'
+  | 'width'
+  | 'rowGap'
+  | 'gap'
+>;
+
+const SimpleGrid: FC<Props> = (props) => <ChakraSimpleGrid {...props} />;
+
+export default SimpleGrid;
+export { type Props as GridProps };
