c7cea2e39f507ec4fd676dcaa5042330e6a209b3
Radoš Tošković <rashmasch@users.noreply.github.com>
2025-01-22 13:02:38 +0100

    feat: chakra v3 divider (#1263)
    * feat: migrate divider component
    
    ---------
    
    Co-authored-by: Marko Markovic <marko.markovic@swissmarketplace.group>

--- src/themes/shared/recipes/index.ts
+++ src/themes/shared/recipes/index.ts
@@ -1,7 +1,9 @@
+import { separatorRecipe as separator } from './separator';
 import { linkRecipe as link } from './link';
 import { badgeRecipe as badge } from './badge';
 
 export const recipes = {
   badge,
   link,
+  separator,
 };
