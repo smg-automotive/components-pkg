41ce078efb818f45d12ab7f349b0e14010ffb7e2
marko-markovic-smg <marko.markovic@swissmarketplace.group>
2025-01-27 17:05:44 +0100

    feat: migrate spinner to V3 (#1279)
    * feat: migrate spinner component

--- src/themes/shared/index.ts
+++ src/themes/shared/index.ts
@@ -5,6 +5,7 @@ import { shadows } from './tokens/shadows';
 import { radii } from './tokens/radii';
 import { opacity } from './tokens/opacity';
 import { lineHeights } from './tokens/lineHeights';
+import { keyframes } from './tokens/keyframes';
 import { fontWeights } from './tokens/fontWeights';
 import { fontSizes } from './tokens/fontSizes';
 import { fonts } from './tokens/fonts';
@@ -12,7 +13,7 @@ import { cursor } from './tokens/cursor';
 import { colors } from './tokens/colors';
 import { borders } from './tokens/borders';
 import { aspectRatios } from './tokens/aspectRatios';
-
+import { animations } from './tokens/animations';
 import { textStyles } from './textStyles';
 import { slotRecipes } from './slotRecipes';
 import { recipes } from './recipes';
@@ -25,6 +26,7 @@ export const getSharedConfig = () => ({
   theme: {
     breakpoints: emBreakpoints,
     textStyles,
+    keyframes: keyframes,
     tokens: {
       zIndex,
       spacing,
@@ -40,6 +42,7 @@ export const getSharedConfig = () => ({
       colors,
       borders,
       aspectRatios,
+      animations,
     },
     recipes,
     slotRecipes,
