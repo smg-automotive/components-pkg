a79e263ac4975d3c807ee1557c2e32870d2bb245
Marko Markovic <marko.markovic@swissmarketplace.group>
2025-01-31 15:52:57 +0100

    feat: apply avatar recipe to component

--- src/components/avatar/index.tsx
+++ src/components/avatar/index.tsx
@@ -1,8 +1,9 @@
 import React, { FC } from 'react';
 
-import { BoxProps } from '@chakra-ui/react';
+import { BoxProps, useRecipe  } from '@chakra-ui/react';
 
 import { AvatarIcon, AvatarWithNotificationIcon } from '../icons';
+import { avatarRecipe } from 'src/themes/shared/recipes/avatar';
 
 export type AvatarProps = {
   withNotification?: boolean;
@@ -12,10 +13,11 @@ export type AvatarProps = {
 export const Avatar: FC<AvatarProps> = ({ withNotification, color }) => {
   return withNotification ? (
     <AvatarWithNotificationIcon
+      css={avatarRecipe.base}
       data-testid="notification-icon"
       {...(color && { color })}
     />
   ) : (
-    <AvatarIcon {...(color && { color })} />
+    <AvatarIcon css={ avatarRecipe } {...(color && { color })} />
   );
 };
