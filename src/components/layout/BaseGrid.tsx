5514bd98a980e7f6fc3cb1976cf154c406b4ce94
Marko Markovic <marko.markovic@swissmarketplace.group>
2025-01-31 08:48:01 +0100

    feat: migrate base grid

--- src/components/layout/BaseGrid.tsx
+++ src/components/layout/BaseGrid.tsx
@@ -1,12 +1,14 @@
 import React, { FC, PropsWithChildren } from 'react';
 
-import Grid, { type GridProps } from '../grid';
+import { Grid, type GridProps } from 'src';
 
 export const repeatArea = (count: number, area: string) => {
   return new Array(count).fill(area).join(' ');
 };
 
-const BaseGridLayout: FC<PropsWithChildren<GridProps>> = (props) => {
+export type BaseGridLayoutProps = GridProps;
+
+export const BaseGridLayout: FC<PropsWithChildren<BaseGridLayoutProps>> = (props) => {
   const { children, ...gridProps } = props;
 
   return (
@@ -19,5 +21,3 @@ const BaseGridLayout: FC<PropsWithChildren<GridProps>> = (props) => {
     </Grid>
   );
 };
-
-export default BaseGridLayout;
