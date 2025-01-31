f57bdfbe6061f8536e1fb942d97390f723a8e5de
Marko Markovic <marko.markovic@swissmarketplace.group>
2025-01-31 15:22:25 +0100

    feat: export avatar recipe

--- src/themes/shared/recipes/avatar.ts
+++ src/themes/shared/recipes/avatar.ts
@@ -1,6 +1,6 @@
 import { defineRecipe } from '@chakra-ui/react';
 
-export const avatarSlotRecipe = defineRecipe({
+export const avatarRecipe = defineRecipe({
   className: 'chakra-avatar',
   base: {
     cursor: 'pointer',
