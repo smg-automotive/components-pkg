79270ae222645a77b4cceb572cdac7106fe2794e
Marko Markovic <marko.markovic@swissmarketplace.group>
2025-02-04 10:34:08 +0100

    feat: migrate layout/page component

---
+++ src/components/layout/Page.stories.tsx
@@ -0,0 +1,128 @@
+import React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+
+import { sizes } from 'src/themes/shared/tokens/sizes';
+
+import { Box, Separator, Flex, Text } from '../index';
+import { PageLayout } from './Page';
+
+const meta: Meta<typeof PageLayout> = {
+  title: 'Layout/Pages/Page',
+  component: PageLayout,
+
+  args: {
+    header: (
+      <Box h="2xl" bg="gray.50">
+        <Separator />
+        <Flex justifyContent="center" alignItems="center" h="full">
+          <Text p="2xl">I am the header</Text>
+        </Flex>
+      </Box>
+    ),
+
+    maxContentWidth: 'lg',
+
+    footer: (
+      <Box h="2xl" bg="gray.50">
+        <Separator />
+        <Flex justifyContent="center" alignItems="center" h="full">
+          <Text p="2xl">I am the footer</Text>
+        </Flex>
+      </Box>
+    ),
+
+    children: 'content', 
+  },
+
+  argTypes: {
+    header: {
+      table: {
+        disable: true,
+      },
+    },
+
+    footer: {
+      table: {
+        disable: true,
+      },
+    },
+
+    skyScraperAd: {
+      table: {
+        disable: true,
+      },
+    },
+
+    heroAd: {
+      table: {
+        disable: true,
+      },
+    },
+
+    children: {
+      mapping: {
+        content: (
+          <Flex
+            bg="green.100"
+            h="container.sm"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Text>Content</Text>
+          </Flex>
+        ),
+      },
+      table: {
+        disable: true,
+      },
+    },
+
+    maxContentWidth: {
+      options: Object.keys(sizes.container),
+
+      control: {
+        type: 'select',
+      },
+    },
+  },
+
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+export default meta;
+
+type StoryType = StoryObj<typeof PageLayout>;
+export const LayoutWithoutAds: StoryType = {
+  name: 'Layout without ads',
+};
+
+export const LayoutWithSidebarAds: StoryType = {
+  name: 'Layout with sidebar ads',
+
+  args: {
+    skyScraperAd: (
+      <Flex bg="red.100" h="8xl" justifyContent="center" alignItems="center">
+        <Text>Sidebar Ads</Text>
+      </Flex>
+    ),
+  },
+};
+
+export const LayoutWithHeroAndSidebarAds: StoryType = {
+  name: 'Layout with hero and sidebar ads',
+
+  args: {
+    skyScraperAd: (
+      <Flex bg="red.100" h="8xl" justifyContent="center" alignItems="center">
+        <Text>Sidebar Ads</Text>
+      </Flex>
+    ),
+
+    heroAd: (
+      <Flex bg="blue.100" h="6xl" justifyContent="center" alignItems="center">
+        <Text>Hero Ad</Text>
+      </Flex>
+    ),
+  },
+};
---
+++ src/components/layout/Page.tsx
@@ -0,0 +1,34 @@
+import React, { FC, PropsWithChildren, ReactNode } from 'react';
+
+import { sizes } from 'src/themes/shared/tokens/sizes';
+
+import { BaseLayout } from './BaseLayout';
+
+export interface Props {
+  header: ReactNode;
+  maxContentWidth: keyof typeof sizes.container;
+  skyScraperAd?: ReactNode;
+  heroAd?: ReactNode;
+  footer?: ReactNode;
+}
+
+export const PageLayout: FC<PropsWithChildren<Props>> = ({
+  header,
+  maxContentWidth,
+  skyScraperAd,
+  footer,
+  heroAd,
+  children,
+}) => {
+  return (
+    <BaseLayout
+      header={header}
+      footer={footer}
+      skyScraperAd={skyScraperAd}
+      heroAd={heroAd}
+      maxContentWidth={maxContentWidth}
+    >
+      {children}
+    </BaseLayout>
+  );
+};
