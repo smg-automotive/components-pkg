312e4a158f7a8316828f473b1aa2324edf9c6d35
Krzysztof Sakwerda <ksakwerda@gmail.com>
2025-02-12 11:22:49 +0100

    feat: remove icon-inside and icon-outside list variants in favor of CSS listStylePosition

--- src/components/list/index.stories.tsx
+++ src/components/list/index.stories.tsx
@@ -3,29 +3,88 @@ import { Meta, StoryObj } from '@storybook/react';
 
 import { getRecipeControls } from '.storybook/preview/controls/recipe';
 
-import { List, ListProps } from './index';
+import { Box } from '../box';
 
-const Template = ({ items, ...args }: ListProps & { items: number }) => (
-  <List.Root {...args}>
+import { List, ListRootProps } from './index';
+
+const Template = ({
+  items,
+  showContainer,
+  longText,
+  ...args
+}: ListRootProps & {
+  items: number;
+  showContainer: boolean;
+  longText: boolean;
+}) => (
+  <List.Root
+    {...args}
+    {...(showContainer
+      ? {
+          border: '1px',
+          borderColor: 'gray.900',
+          bg: 'gray.100',
+          display: 'inline-block',
+        }
+      : {})}
+  >
     {Array.from({ length: items }, (_, i) => (
-      <List.Item key={i}>Item {i + 1}</List.Item>
+      <List.Item key={i}>
+        Item {i + 1} {longText ? 'lorem ipsum dolor sit amet' : ''}
+      </List.Item>
     ))}
   </List.Root>
 );
 
 const meta: Meta<typeof Template> = {
-  title: 'Components/Data display/List/Unstyled list',
+  title: 'Components/Data display/List',
   render: Template.bind({}),
+
+  decorators: [
+    (Story) => (
+      <Box maxW="4xl">
+        <Story />
+      </Box>
+    ),
+  ],
 
   args: {
     size: 'md',
     items: 5,
+    showContainer: false,
+    longText: false,
   },
 
   argTypes: {
     ...getRecipeControls('list'),
+    as: {
+      control: 'select',
+      options: ['ul', 'ol'],
+    },
+    listStyleType: {
+      control: 'select',
+      options: [
+        'none',
+        'disc',
+        'circle',
+        'square',
+        'initial',
+        'decimal',
+        'lower-alpha',
+        'lower-roman',
+        'upper-alpha',
+        'upper-roman',
+      ],
+    },
+    listStylePosition: {
+      control: 'select',
+      options: ['inside', 'outside'],
+    },
   },
 };
 export default meta;
 
+/**
+ * Use combination of `showContainer` and `longText` to see the difference between `listStylePosition` `inside` and `outside`.
+ * */
 export const Overview: StoryObj<typeof Template> = {};
--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -1,21 +1,13 @@
-import React, { FC, PropsWithChildren } from 'react';
 import { List as ChakraList } from '@chakra-ui/react';
 
-export { ListRootProps, ListItemProps } from '@chakra-ui/react';
+export type { ListItemProps, ListRootProps } from '@chakra-ui/react';
 
 const { Root, Item } = ChakraList;
 
 Root.displayName = 'List.Root';
 Item.displayName = 'List.Item';
 
-export type ListProps = PropsWithChildren & {
-  variant?: 'icon-inside' | 'icon-outside';
-  size?: 'sm' | 'md';
-  start?: number;
-};
-
 export const List = {
   Root,
   Item,
 };
-
