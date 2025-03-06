880072bc248ebb54b83695cba3f113e3c64c6ec4
Krzysztof Sakwerda <ksakwerda@gmail.com>
2025-03-06 13:49:02 +0100

    feat: enable ref forwarding for search field and searchable list

--- src/components/input/SearchField.tsx
+++ src/components/input/SearchField.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useRef } from 'react';
+import React, { forwardRef, useEffect, useRef } from 'react';
 
 import useMediaQuery from 'src/hooks/useMediaQuery';
 
@@ -20,39 +20,54 @@ export type SearchFieldProps = {
   setSearchQuery: (newQuery: string) => void;
 } & SearchFieldOptions;
 
-export const SearchField: FC<SearchFieldProps> = ({
-  name,
-  placeholder = '',
-  ariaControls = '',
-  searchQuery,
-  setSearchQuery,
-  onFocus = () => null,
-  onBlur = () => null,
-  autofocusOnDesktop = true,
-}) => {
-  const isDesktopOnly = useMediaQuery({ above: 'md' });
-  const inputRef = useRef<HTMLInputElement>(null);
+export const SearchField = forwardRef<HTMLInputElement, SearchFieldProps>(
+  (
+    {
+      name,
+      placeholder = '',
+      ariaControls = '',
+      searchQuery,
+      setSearchQuery,
+      onFocus = () => null,
+      onBlur = () => null,
+      autofocusOnDesktop = true,
+    },
+    ref,
+  ) => {
+    const isDesktopOnly = useMediaQuery({ above: 'md' });
+    const inputRef = useRef<HTMLInputElement>();
 
-  useEffect(() => {
-    if (autofocusOnDesktop && isDesktopOnly) {
-      inputRef.current?.focus();
-    }
-  }, [isDesktopOnly, autofocusOnDesktop]);
+    useEffect(() => {
+      if (autofocusOnDesktop && isDesktopOnly) {
+        inputRef.current?.focus();
+      }
+    }, [isDesktopOnly, autofocusOnDesktop]);
 
-  return (
-    <Input
-      ref={inputRef}
-      icon={MagnifierIcon}
-      name={name}
-      placeholder={placeholder}
-      value={searchQuery}
-      setInputValue={setSearchQuery}
-      size="lg"
-      isClearable={true}
-      debounce={true}
-      onFocus={onFocus}
-      onBlur={onBlur}
-      aria-controls={ariaControls}
-    />
-  );
-};
+    return (
+      <Input
+        ref={(input) => {
+          if (!input) return;
+
+          inputRef.current = input;
+
+          if (typeof ref === 'function') {
+            ref(input);
+          } else if (ref) {
+            ref.current = input;
+          }
+        }}
+        icon={MagnifierIcon}
+        name={name}
+        placeholder={placeholder}
+        value={searchQuery}
+        setInputValue={setSearchQuery}
+        size="lg"
+        isClearable={true}
+        debounce={true}
+        onFocus={onFocus}
+        onBlur={onBlur}
+        aria-controls={ariaControls}
+      />
+    );
+  },
+);
--- src/components/list/SearchableList.tsx
+++ src/components/list/SearchableList.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useCallback, useEffect, useState } from 'react';
+import React, { FC, forwardRef, useCallback, useEffect, useState } from 'react';
 
 import Fuse, { FuseResult } from 'fuse.js';
 
@@ -92,123 +92,129 @@ const getFuseInstance = (listItems: ListItemWithChildren[]) => {
   return new Fuse(listItems, fuseOptions) as unknown as FuseSearch;
 };
 
-export const SearchableList: FC<Props> = ({
-  listItems,
-  NoResults = empty,
-  EmptyQueryPlaceholder = empty,
-  listAriaLabel = 'searchable list',
-  searchFieldOptions = {},
-  listOptions = { columns: 1, childrenSpacing: 'md' },
-}) => {
-  const [searchState, setSearchState] = useState<{
-    query: string;
-    listItems: typeof listItems;
-    fullListItems: typeof listItems;
-    fuse: FuseSearch;
-  }>({
-    query: '',
-    listItems,
-    fullListItems: [...listItems],
-    fuse: getFuseInstance(listItems),
-  });
-  const { placeholder = '', autofocusOnDesktop = true } = searchFieldOptions;
-  const { columns = 1, childrenSpacing = 'md' } = listOptions;
-  const areaId = 'searchableList';
-  const { query } = searchState;
+export const SearchableList = forwardRef<HTMLInputElement, Props>(
+  (
+    {
+      listItems,
+      NoResults = empty,
+      EmptyQueryPlaceholder = empty,
+      listAriaLabel = 'searchable list',
+      searchFieldOptions = {},
+      listOptions = { columns: 1, childrenSpacing: 'md' },
+    },
+    ref,
+  ) => {
+    const [searchState, setSearchState] = useState<{
+      query: string;
+      listItems: typeof listItems;
+      fullListItems: typeof listItems;
+      fuse: FuseSearch;
+    }>({
+      query: '',
+      listItems,
+      fullListItems: [...listItems],
+      fuse: getFuseInstance(listItems),
+    });
+    const { placeholder = '', autofocusOnDesktop = true } = searchFieldOptions;
+    const { columns = 1, childrenSpacing = 'md' } = listOptions;
+    const areaId = 'searchableList';
+    const { query } = searchState;
 
-  useEffect(() => {
-    setSearchState((currentState) => {
-      const fuse = getFuseInstance(listItems);
-      if (currentState.query === '') {
+    useEffect(() => {
+      setSearchState((currentState) => {
+        const fuse = getFuseInstance(listItems);
+        if (currentState.query === '') {
+          return {
+            listItems,
+            fullListItems: [...listItems],
+            fuse,
+            query: currentState.query,
+          };
+        }
+
+        const searchResults = search({ fuse, query: currentState.query });
+        const filteredListItems = mapItemsFromSearchResult(searchResults);
         return {
-          listItems,
-          fullListItems: [...listItems],
+          listItems: filteredListItems,
           fuse,
           query: currentState.query,
+          fullListItems: [...listItems],
         };
-      }
+      });
+    }, [listItems]);
 
-      const searchResults = search({ fuse, query: currentState.query });
-      const filteredListItems = mapItemsFromSearchResult(searchResults);
-      return {
-        listItems: filteredListItems,
-        fuse,
-        query: currentState.query,
-        fullListItems: [...listItems],
-      };
-    });
-  }, [listItems]);
+    const setSearchQuery = useCallback((newQuery: string) => {
+      setSearchState((currentState) => {
+        const trimmedQuery = newQuery.trim();
 
-  const setSearchQuery = useCallback((newQuery: string) => {
-    setSearchState((currentState) => {
-      const trimmedQuery = newQuery.trim();
+        if (!trimmedQuery) {
+          return {
+            ...currentState,
+            listItems: currentState.fullListItems,
+            query: trimmedQuery,
+          };
+        }
 
-      if (!trimmedQuery) {
+        const searchResults = search({
+          fuse: currentState.fuse,
+          query: trimmedQuery,
+        });
+        const filteredListItems = mapItemsFromSearchResult(searchResults);
         return {
           ...currentState,
-          listItems: currentState.fullListItems,
+          listItems: filteredListItems,
           query: trimmedQuery,
         };
-      }
-
-      const searchResults = search({
-        fuse: currentState.fuse,
-        query: trimmedQuery,
       });
-      const filteredListItems = mapItemsFromSearchResult(searchResults);
-      return {
-        ...currentState,
-        listItems: filteredListItems,
-        query: trimmedQuery,
-      };
-    });
-  }, []);
+    }, []);
 
-  return (
-    <Flex gridGap="md" direction="column" width="full">
-      <SearchField
-        name="searchableListSearchFiled"
-        searchQuery={query}
-        setSearchQuery={setSearchQuery}
-        ariaControls={areaId}
-        placeholder={placeholder}
-        autofocusOnDesktop={autofocusOnDesktop}
-      />
-      {searchState.query.length === 0 ? <EmptyQueryPlaceholder /> : null}
-      {searchState.listItems.length > 0 ? (
-        <List
-          width="full"
-          height="full"
-          id={areaId}
-          aria-live="polite"
-          sx={{
-            columns: { base: 1, md: columns },
-            columnGap: 'var(--chakra-space-4xl)',
-            columnRule: '1px solid var(--chakra-colors-gray-100)',
-          }}
-          aria-label={listAriaLabel}
-        >
-          {searchState.listItems.map((item, itemIndex) => {
-            const parentKey = `${itemIndex}-${item.value}`;
-            const children = item.children || [];
+    return (
+      <Flex gridGap="md" direction="column" width="full">
+        <SearchField
+          name="searchableListSearchFiled"
+          searchQuery={query}
+          setSearchQuery={setSearchQuery}
+          ariaControls={areaId}
+          placeholder={placeholder}
+          autofocusOnDesktop={autofocusOnDesktop}
+          ref={ref}
+        />
+        {searchState.query.length === 0 ? <EmptyQueryPlaceholder /> : null}
+        {searchState.listItems.length > 0 ? (
+          <List
+            width="full"
+            height="full"
+            id={areaId}
+            aria-live="polite"
+            sx={{
+              columns: { base: 1, md: columns },
+              columnGap: 'var(--chakra-space-4xl)',
+              columnRule: '1px solid var(--chakra-colors-gray-100)',
+            }}
+            aria-label={listAriaLabel}
+          >
+            {searchState.listItems.map((item, itemIndex) => {
+              const parentKey = `${itemIndex}-${item.value}`;
+              const children = item.children || [];
 
-            return (
-              <SearchableListItem {...item} key={parentKey}>
-                {children.length > 0 ? (
-                  <List width="full" paddingLeft={childrenSpacing}>
-                    {children.map((child, childIndex) => {
-                      const childKey = `${childIndex}-${child.value}`;
-                      return <SearchableListItem {...child} key={childKey} />;
-                    })}
-                  </List>
-                ) : null}
-              </SearchableListItem>
-            );
-          })}
-        </List>
-      ) : (
-        <NoResults />
-      )}
-    </Flex>
-  );
-};
+              return (
+                <SearchableListItem {...item} key={parentKey}>
+                  {children.length > 0 ? (
+                    <List width="full" paddingLeft={childrenSpacing}>
+                      {children.map((child, childIndex) => {
+                        const childKey = `${childIndex}-${child.value}`;
+                        return <SearchableListItem {...child} key={childKey} />;
+                      })}
+                    </List>
+                  ) : null}
+                </SearchableListItem>
+              );
+            })}
+          </List>
+        ) : (
+          <NoResults />
+        )}
+      </Flex>
+    );
+  },
+);
