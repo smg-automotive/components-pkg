8cae161e0518aa530cbccda23c09455207136c46
Nenad Rosuljas <nenad.rosuljas@swissmarketplace.group>
2025-03-21 22:53:30 +0100

    fix: solve lint errors

--- src/components/count/index.stories.tsx
+++ src/components/count/index.stories.tsx
@@ -24,21 +24,21 @@ export const Overview: StoryObj<typeof Count> = {
   },
 };
 
-export const Primary: StoryObj<typeof Count> = {
+export const PrimaryCount: StoryObj<typeof Count> = {
   args: {
     count: 1,
     variant: 'primary',
   },
 };
 
-export const Inverted: StoryObj<typeof Count> = {
+export const InvertedCount: StoryObj<typeof Count> = {
   args: {
     count: 2,
     variant: 'inverted',
   },
 };
 
-export const Info: StoryObj<typeof Count> = {
+export const InfoCount: StoryObj<typeof Count> = {
   args: {
     count: 3,
     variant: 'info',
@@ -52,14 +52,14 @@ export const Success: StoryObj<typeof Count> = {
   },
 };
 
-export const Warning: StoryObj<typeof Count> = {
+export const WarningCount: StoryObj<typeof Count> = {
   args: {
     count: 5,
     variant: 'warning',
   },
 };
 
-export const Error: StoryObj<typeof Count> = {
+export const ErrorCount: StoryObj<typeof Count> = {
   args: {
     count: 6,
     variant: 'error',
--- src/themes/shared/recipes/index.ts
+++ src/themes/shared/recipes/index.ts
@@ -3,9 +3,9 @@ import { spinnerRecipe as spinner } from './spinner';
 import { skeletonRecipe as skeleton } from './skeleton';
 import { separatorRecipe as separator } from './separator';
 import { linkRecipe as link } from './link';
+import { countRecipe as count } from './count';
 import { badgeRecipe as badge } from './badge';
 import { avatarRecipe as avatar } from './avatar';
-import { countRecipe as count } from './count';
 
 export const recipes = {
   avatar,
