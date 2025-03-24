871784aeedc68f4b92f76e5919abacff45db55cd
Radoš Tošković <rashmasch@users.noreply.github.com>
2025-03-24 14:31:30 +0100

    feat: migrate section (#1349)
    * feat: init component
    
    * feat: create component recipe
    
    * feat: create story for component without image
    
    * feat: add story for componnt with an image
    
    * test: add component test
    
    * chore: delete old component
    
    * fix: load recipe slot through the theme
    
    * feature: expose css prop for stack component
    
    * chore: move component styles to slot recipe
    
    * fix: expand section story to be able to change image size
    
    * chore: remove variant prop
    
    * chore: remove css prop from stack component

---
+++ src/components/section/index.tsx
@@ -0,0 +1,42 @@
+import React, { FC, ReactNode } from 'react';
+import {
+  Box,
+  BoxProps,
+  chakra,
+  RecipeVariantProps,
+  Stack,
+  useSlotRecipe,
+} from '@chakra-ui/react';
+
+import { sectionRecipe } from 'src/themes/shared/slotRecipes/section';
+
+type SectionVariantProps = RecipeVariantProps<typeof sectionRecipe>;
+
+export type SectionProps = SectionVariantProps & {
+  title: string;
+  text?: string;
+  image?: ReactNode;
+  maxImgW?: BoxProps['maxWidth'];
+};
+
+export const Section: FC<SectionProps> = ({ maxImgW = '2xl', ...props }) => {
+  const recipe = useSlotRecipe({ key: 'section' });
+  const [recipeProps, componentProps] = recipe.splitVariantProps(props);
+  const styles = recipe(recipeProps);
+
+  const { title, text, image } = componentProps;
+
+  return (
+    <Stack css={styles.root}>
+      {image ? (
+        <Box css={styles.imageContainer} maxW={maxImgW}>
+          {image}
+        </Box>
+      ) : null}
+      <Stack css={styles.textContainer}>
+        <chakra.span css={styles.title}>{title}</chakra.span>
+        {text ? <chakra.span css={styles.text}>{text}</chakra.span> : null}
+      </Stack>
+    </Stack>
+  );
+};
