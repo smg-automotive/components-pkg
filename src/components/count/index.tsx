6aa669664866c8cdd26a20056c9d3fd41b16b85c
Nenad Rosuljas <nenad.rosuljas@swissmarketplace.group>
2025-03-24 17:04:40 +0100

    fix: update types for props

--- src/components/count/index.tsx
+++ src/components/count/index.tsx
@@ -1,23 +1,19 @@
 import React, { FC } from 'react';
-import { chakra, useRecipe } from '@chakra-ui/react';
+import { chakra, RecipeVariantProps, useRecipe } from '@chakra-ui/react';
 
-export type CountVariant =
-  | 'primary'
-  | 'inverted'
-  | 'info'
-  | 'success'
-  | 'warning'
-  | 'error';
+import { countRecipe } from 'src/themes/shared/recipes/count';
 
-export type CountProps = {
+type CountVariantProps = RecipeVariantProps<typeof countRecipe>;
+
+export type CountProps = CountVariantProps & {
   count: number;
-  variant?: CountVariant;
   ariaLabel?: string;
 };
 
-export const Count: FC<CountProps> = ({ count, variant, ariaLabel }) => {
+export const Count: FC<CountProps> = ({ count, ariaLabel, ...props }) => {
   const recipe = useRecipe({ key: 'count' });
-  const styles = recipe({ variant });
+  const [recipeProps] = recipe.splitVariantProps(props);
+  const styles = recipe(recipeProps);
 
   return (
     <chakra.span css={styles} aria-label={ariaLabel}>
