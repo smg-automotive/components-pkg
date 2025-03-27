8c85a1f8e30229a56583cd6053928d072bcb7f3b
Lorenzo Kappeler <me@lnk.codes>
2025-03-27 17:39:45 +0100

    chore: remove dealerInfoSystem

--- src/components/navigation/header/config/DrawerNodeItems.tsx
+++ src/components/navigation/header/config/DrawerNodeItems.tsx
@@ -1008,25 +1008,6 @@ export const drawerNodeItems = ({
           },
         },
         {
-          translationKey: 'header.userMenu.dealerInfoSystem',
-          link: {
-            de: 'https://bi.scout24.ch',
-            en: 'https://bi.scout24.ch',
-            fr: 'https://bi.scout24.ch',
-            it: 'https://bi.scout24.ch',
-          },
-          visibilitySettings: {
-            userType: {
-              private: false,
-              professional: false,
-            },
-            brand: {
-              autoscout24: false,
-              motoscout24: false,
-            },
-          },
-        },
-        {
           translationKey: 'header.userMenu.marketPriceCheck',
           link: {
             de: '/de/member/vehicleacquisition',
