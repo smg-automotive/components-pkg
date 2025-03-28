9ba7141ef1f6f98773ece95a576636ca34d96417
GutierrezGo <virginia@carforyou.ch>
2025-03-28 15:08:26 +0100

    fix: typecheck & cleaned up

--- src/components/rating/index.tsx
+++ src/components/rating/index.tsx
@@ -1,32 +1,32 @@
 import React, { FC } from 'react';
 import { Box } from '@chakra-ui/react';
 
-export type Props = {
+export type RatingProps = {
   rating: number;
   size: 'large' | 'small';
 };
 
-export const Rating: FC<Props> = ({ rating, size }) => {
+export const Rating: FC<RatingProps> = ({ rating, size }) => {
   return (
     <Box
       aria-label={`Rating is ${rating} out of 5`}
       display="inline-block"
       fontSize={size === 'large' ? 'md' : 'sm'}
-      fontFamily="Arial"
+      fontFamily="makeItSans"
       lineHeight="xs"
       css={{
         '--rating': rating.toString(),
         '--percent': 'calc((var(--rating) - 0.16) / 5 * 100%)',
         '--star-color': 'colors.orange.300',
-      }}
-      _before={{
-        content: '"★★★★★"',
-        letterSpacing: '0.5em',
-        color: 'var(--star-color)',
-        background: `linear-gradient(90deg, var(--star-color) var(--percent), white var(--percent))`,
-        backgroundClip: 'text',
-        WebkitTextFillColor: 'transparent',
-        WebkitTextStroke: '0.1em var(--star-color)',
+        '&::before': {
+          content: '"★★★★★"',
+          letterSpacing: '0.5em',
+          color: 'var(--star-color)',
+          background: `linear-gradient(90deg, var(--star-color) var(--percent), white var(--percent))`,
+          backgroundClip: 'text',
+          WebkitTextFillColor: 'transparent',
+          WebkitTextStroke: '0.1em var(--star-color)',
+        },
       }}
     />
   );
