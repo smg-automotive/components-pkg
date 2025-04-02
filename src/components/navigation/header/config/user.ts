dbb0900d9003341d192c01a912f9a8c46803782f
Lorenzo Kappeler <me@lnk.codes>
2025-04-02 16:07:51 +0200

    fix: update account page links

--- src/components/navigation/header/config/user.ts
+++ src/components/navigation/header/config/user.ts
@@ -65,10 +65,10 @@ export const changeEmailLinkConfig: NavigationLinkConfigProps = {
 export const editUsersLinkConfig: NavigationLinkConfigProps = {
   translationKey: 'header.userMenu.editUser',
   link: {
-    de: '/de/member/users/list',
-    en: '/de/member/users/list',
-    fr: '/fr/member/users/list',
-    it: '/it/member/users/list',
+    de: '/de/account/user-list',
+    en: '/en/account/user-list',
+    fr: '/fr/account/user-list',
+    it: '/it/account/user-list',
   },
   visibilitySettings: {
     userType: {
@@ -105,10 +105,10 @@ export const accountSettingsLinkConfig: NavigationLinkConfigProps = {
 export const changeLanguageLinkConfig: NavigationLinkConfigProps = {
   translationKey: 'header.userMenu.userLanguage',
   link: {
-    de: '/de/member/masterdata/userlanguage',
-    en: '/de/member/masterdata/userlanguage',
-    fr: '/fr/member/masterdata/userlanguage',
-    it: '/it/member/masterdata/userlanguage',
+    de: '/de/account/change-language',
+    en: '/en/account/change-language',
+    fr: '/fr/account/change-language',
+    it: '/it/account/change-language',
   },
   visibilitySettings: {
     userType: {
