082e059ff30c0728e02bb99a99e4e2c885188162
Marine Berthier <marineberthier@w1960.home>
2025-06-20 14:47:31 +0200

    fix: add story

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,1 +1,1 @@
-export { default as useMediaQuery } from './useMediaQuery';
+export { default as useMediaQuery } from './useMediaQuery/index';
---
+++ src/hooks/useMediaQuery/index.stories.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+
+import { Box, Text } from '@chakra-ui/react';
+
+import useMediaQuery, { QueryProps } from './index';
+const queryToString = (query: QueryProps): string => {
+  const { above, below } = query;
+
+  if (above && below) {
+    return `is between ${above} and ${below}`;
+  }
+
+  if (above) {
+    return `is above ${above}`;
+  }
+
+  if (below) {
+    return `is below ${below}`;
+  }
+
+  return 'invalid query';
+};
+
+const MediaQueryDemo = ({ query }: { query: QueryProps }) => {
+  const normalizedQuery = { ...query };
+
+  const isMatch = useMediaQuery(normalizedQuery);
+
+  return (
+    <Box>
+      <Text>
+        {queryToString(query)}
+        <strong>{isMatch ? '✅ true' : '❌ false'}</strong>
+      </Text>
+    </Box>
+  );
+};
+
+const meta: Meta<typeof MediaQueryDemo> = {
+  title: 'Hooks/useMediaQuery',
+  component: MediaQueryDemo,
+  args: {
+    query: { below: 'sm' },
+  },
+};
+
+export default meta;
+
+export const BelowSm: StoryObj<typeof MediaQueryDemo> = {};
+
+export const AboveSm: StoryObj<typeof MediaQueryDemo> = {
+  args: {
+    query: { above: 'sm' },
+  },
+};
+
+export const BelowMd: StoryObj<typeof MediaQueryDemo> = {
+  args: {
+    query: { below: 'md' },
+  },
+};
+
+export const AboveMd: StoryObj<typeof MediaQueryDemo> = {
+  args: {
+    query: { above: 'md' },
+  },
+};
