57435307a01e30891f130a544671dbbb1297d0f8
Marine Berthier <marineberthier@w1960.home>
2025-06-25 13:37:25 +0200

    feat: usemediaQuery hook

--- src-v2/hooks/index.ts
+++ src-v2/hooks/index.ts
@@ -2,6 +2,5 @@ export { default as useAccordionItemState } from './useAccordionItemState';
 export { default as useBreakpointValue } from './useBreakpointValue';
 export { default as useDebouncedOnChange } from './useDebouncedOnChange';
 export { default as useDisclosure } from './useDisclosure';
-export { default as useMediaQuery } from './useMediaQuery';
 export { default as useToast } from './UseToast';
 export { default as useToken } from './useToken';
