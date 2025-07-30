73ea70dc44d684abd73ce127b20c9825cd78057d
Lorenzo Kappeler <me@lnk.codes>
2025-07-30 16:26:49 +0200

    fix: add return to vehicle pool

--- src/components/errorPage/actions/primary/BackToLogin.tsx
+++ src/components/errorPage/actions/primary/BackToLogin.tsx
@@ -7,7 +7,8 @@ import { ActionButtonInterface } from '../interface';
 const BackToLogin: FC<ActionButtonInterface> = ({ t, language = 'de' }) => {
   const languageToUse = language === 'en' ? 'de' : language;
   const logoutLink = `/seller-web/api/auth/logout`;
-  const loginLink = `/seller-web/api/auth/login?locale=${languageToUse}`;
+  const vehicleManagementLink = `${languageToUse}/vehicle-management`;
+  const loginLink = `/seller-web/api/auth/login?locale=${languageToUse}&returnTo=${encodeURIComponent(vehicleManagementLink)}`;
   const query = `?returnTo=${encodeURIComponent(loginLink)}`;
   const logoutBeforeLoginLink = `${logoutLink}${query}`;
 
