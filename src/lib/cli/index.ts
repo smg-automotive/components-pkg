84ed4dd1398f775a56b63b02d71de15e4f931ab5
Krzysztof Sakwerda <ksakwerda@gmail.com>
2025-08-08 15:39:58 +0200

    fix: yargs usage in cli after update

--- src/lib/cli/index.ts
+++ src/lib/cli/index.ts
@@ -1,13 +1,15 @@
+import { hideBin } from 'yargs/helpers';
+
 import setupNextFonts from './setupNextFonts';
 import setup from './setup';
 import copyFonts from './copyFonts';
 
 const yargs = require('yargs');
 
-yargs
+yargs(hideBin(process.argv))
   .command({
     command: 'setup',
-    desc: 'Setup copy-fonts script and gitignore copied files',
+    describe: 'Setup copy-fonts script and gitignore copied files',
     builder: (args: typeof yargs) =>
       args.option('path', {
         alias: 'p',
@@ -20,7 +22,7 @@ yargs
   })
   .command({
     command: 'copy-fonts',
-    desc: 'Copy fonts from the package to required destination',
+    describe: 'Copy fonts from the package to required destination',
     builder: (args: typeof yargs) =>
       args.option('path', {
         alias: 'p',
@@ -32,7 +34,7 @@ yargs
   })
   .command({
     command: 'setup-next-fonts',
-    desc: 'Generate a component to load fonts with @next/fonts',
+    describe: 'Generate a component to load fonts with @next/fonts',
     builder: (args: typeof yargs) =>
       args
         .option('component-path', {
