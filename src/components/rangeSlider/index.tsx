0cbfee634141f49326465a86504f47ea5f785f99
braopa-smg <brane.opacic@swissmarketplace.group>
2025-08-12 10:16:11 +0200

    fix: increase touch points sliders on mobile

--- src/components/rangeSlider/index.tsx
+++ src/components/rangeSlider/index.tsx
@@ -24,8 +24,8 @@ const RangeSlider: FC<RangeSliderProps> = ({
     <RangeSliderTrack bg="gray.200">
       <RangeSliderFilledTrack />
     </RangeSliderTrack>
-    <RangeSliderThumb index={0} />
-    <RangeSliderThumb index={1} />
+    <RangeSliderThumb boxSize={{ base: 32, md: 24 }} index={0} />
+    <RangeSliderThumb boxSize={{ base: 32, md: 24 }} index={1} />
   </ChakraRangeSlider>
 );
 
