76da5c6c634acc04fb9f3786e70da934768d9620
Damir Zečević <153915313+damirzecevicsmg@users.noreply.github.com>
2025-08-18 15:50:37 +0200

    feat: Add Bookmark icon. (#1520)
    * feat: Add Bookmark icon.
    
    * fix: Change viewbox of bookmark icon.
    
    * fix: Change fill value to currentColor.

---
+++ src/components/icons/BookmarkIcon.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ComponentWithAs } from '@chakra-ui/system';
+import { createIcon, IconProps } from '@chakra-ui/react';
+
+export const BookmarkIcon: ComponentWithAs<'svg', IconProps> = createIcon({
+  displayName: 'Bookmark',
+  viewBox: '0 0 12 20',
+  path: (
+    <>
+      <title>Bookmark icon</title>
+      <path
+        fill="currentColor"
+        d="m10 14.99-4-4-4 4v-13h10v-2H0v20l6-6 6 6v-17l-2 1z"
+      />
+      <path d="M12-.01H0v20l6-6 6 6z" />
+    </>
+  ),
+  defaultProps: {
+    boxSize: 'sm',
+    fill: 'none',
+  },
+});
