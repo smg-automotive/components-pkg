511f4558cb6a1349d08de46d18d7eed526da6bba
“ilay149” <ilay.bicer@gmail.com>
2025-09-08 14:58:20 +0200

    fix: change translation to en for info-management page

--- src/components/navigation/header/config/templates.ts
+++ src/components/navigation/header/config/templates.ts
@@ -4,7 +4,7 @@ export const teaserTemplatesLinkConfig: NavigationLinkConfigProps = {
   translationKey: 'header.userMenu.manageAdditionalTitles',
   link: {
     de: '/de/info-management/additional-titles',
-    en: '/de/info-management/additional-titles',
+    en: '/en/info-management/additional-titles',
     fr: '/fr/info-management/additional-titles',
     it: '/it/info-management/additional-titles',
   },
@@ -24,7 +24,7 @@ export const commentTemplatesLinkConfig: NavigationLinkConfigProps = {
   translationKey: 'header.userMenu.manageNotes',
   link: {
     de: '/de/info-management/comment',
-    en: '/de/info-management/comment',
+    en: '/en/info-management/comment',
     fr: '/fr/info-management/comment',
     it: '/it/info-management/comment',
   },
