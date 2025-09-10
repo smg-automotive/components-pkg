a25ce27f0d0a20f48a71b83565169bafac6a6229
Nenad Ignjatović <nenad.ignjatovic@swissmarketplace.group>
2025-09-10 10:08:01 +0200

    feat: migrate two columns layout (#1550)
    * feat: moved component files and references to v3
    
    * feat: removed unnecessary export

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,3 +1,4 @@
 export { repeatArea } from './BaseGrid';
 export { PageLayout } from './Page';
 export { SingleColumnCenteredLayout } from './SingleColumnCentered';
+export { TwoColumnsLayout } from './TwoColumnsLayout';
