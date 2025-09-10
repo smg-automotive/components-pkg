48ca06daacb50c36088535a1a4349c45b9d90435
Nenad Ignjatovic <nenad.ignjatovic@swissmarketplace.group>
2025-09-10 11:31:07 +0200

    feat: moved component files and references to v3

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -2,3 +2,4 @@ export { repeatArea } from './BaseGrid';
 export { PageLayout } from './Page';
 export { SingleColumnCenteredLayout } from './SingleColumnCentered';
 export { TwoColumnsLayout } from './TwoColumnsLayout';
+export { LayoutWithVehicleReference } from './WithVehicleReference';
