36861fb56fc86c6e5cdbb5eba6c84b2248dc07f5
Zbiljic <zbiljicmilos@gmail.com>
2025-09-11 09:59:33 +0200

    feat: migrating useBreakpointValue hook (#1543)

--- src-v2/hooks/index.ts
+++ src-v2/hooks/index.ts
@@ -1,4 +1,3 @@
-export { default as useBreakpointValue } from './useBreakpointValue';
 export { default as useDebouncedOnChange } from './useDebouncedOnChange';
 export { default as useDisclosure } from './useDisclosure';
 export { default as useToast } from './UseToast';
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,1 +1,2 @@
 export { default as useMediaQuery } from './useMediaQuery/index';
+export { default as useBreakpointValue } from './useBreakpointValue';
