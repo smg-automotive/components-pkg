3180bcc080dc8f5d1f33372e4ffafbe8012b462c
Marine Berthier <marineberthier@n16175.home>
2025-09-19 14:08:34 +0200

    fix: link

--- src/components/navigation/header/config/Optimizer.tsx
+++ src/components/navigation/header/config/Optimizer.tsx
@@ -5,10 +5,10 @@ import { NavigationLinkConfigProps } from './headerLinks';
 export const optimizerLinkConfig: NavigationLinkConfigProps = {
   translationKey: 'header.userMenu.optimizer',
   link: {
-    de: '/de/productdescription/optimizer',
-    en: '/de/productdescription/optimizer',
-    fr: '/fr/productdescription/optimizer',
-    it: '/it/productdescription/optimizer',
+    de: 'https://b2b.autoscout24.ch/aboss/optimizer-2',
+    en: 'https://b2b.autoscout24.ch/aboss/optimizer-2',
+    fr: 'https://b2b.autoscout24.ch/fr/aboss/optimizer-2',
+    it: 'https://b2b.autoscout24.ch/it/aboss/optimizer-2',
   },
   visibilitySettings: {
     userType: {
@@ -24,10 +24,10 @@ export const optimizerLinkConfig: NavigationLinkConfigProps = {
     hideIfEntitlementIsPresent: Entitlement.OptimizerPro,
     singleRequiredEntitlement: [Entitlement.Optimizer],
     missingEntitlementFallbackLink: {
-      de: '/de/productdescription/optimizer',
-      en: '/de/productdescription/optimizer',
-      fr: '/fr/productdescription/optimizer',
-      it: '/it/productdescription/optimizer',
+      de: 'https://b2b.autoscout24.ch/aboss/optimizer-2',
+      en: 'https://b2b.autoscout24.ch/aboss/optimizer-2',
+      fr: 'https://b2b.autoscout24.ch/fr/aboss/optimizer-2',
+      it: 'https://b2b.autoscout24.ch/it/aboss/optimizer-2',
     },
   },
 };
