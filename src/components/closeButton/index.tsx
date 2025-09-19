d476d1d2aae260eb9f23f2593cf939c1a7500f40
Milos Zbiljic <milos.zbiljic@swissmarketplace.group>
2025-09-19 15:29:09 +0200

    chore: recipe key

--- src/components/closeButton/index.tsx
+++ src/components/closeButton/index.tsx
@@ -1,14 +1,12 @@
 import React, { FC } from 'react';
 import { CloseButton as ChakraCloseButton, useRecipe } from '@chakra-ui/react';
 
-import { closeButtonRecipe } from 'src/themes/shared/recipes/closeButton';
-
 interface CloseButtonProps {
   disabled?: boolean;
 }
 
 export const CloseButton: FC<CloseButtonProps> = ({ disabled = false }) => {
-  const recipe = useRecipe({ recipe: closeButtonRecipe });
+  const recipe = useRecipe({ key: 'closeButton' });
   const styles = recipe();
 
   return <ChakraCloseButton css={styles} disabled={disabled} />;
