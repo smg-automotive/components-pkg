597893dbb93287f885ca3599153f0e6f44ab849c
Milos Zbiljic <milos.zbiljic@swissmarketplace.group>
2025-10-06 16:21:54 +0200

    fix: optimizer b2b link

--- src/components/navigation/header/config/Optimizer.tsx
+++ src/components/navigation/header/config/Optimizer.tsx
@@ -24,7 +24,7 @@ export const optimizerLinkConfig: NavigationLinkConfigProps = {
     hideIfEntitlementIsPresent: Entitlement.OptimizerPro,
     singleRequiredEntitlement: [Entitlement.Optimizer],
     missingEntitlementFallbackLink: {
-      de: 'https://b2b.autoscout24.ch/aboss/optimizer-2',
+      de: 'https://b2b.autoscout24.ch/aboss/optimizer-2/',
       en: 'https://b2b.autoscout24.ch/aboss/optimizer-2/',
       fr: 'https://b2b.autoscout24.ch/fr/aboss/optimizer-2/',
       it: 'https://b2b.autoscout24.ch/it/aboss/optimizer-2/',
