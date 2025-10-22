8223d017ec705d24a1b8905fb6ea18e6087a9edc
Marine Berthier <marineberthier@n16175.home>
2025-10-22 15:46:34 +0200

    fix: revert

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -21,10 +21,7 @@ type ActionButton = {
 };
 
 export interface Props
-  extends Pick<
-    ModalProps,
-    'isOpen' | 'onClose' | 'motionPreset' | 'onOverlayClick'
-  > {
+  extends Pick<ModalProps, 'isOpen' | 'onClose' | 'motionPreset'> {
   title?: string;
   primaryActionButton?: ActionButton;
   secondaryActionButton?: ActionButton;
