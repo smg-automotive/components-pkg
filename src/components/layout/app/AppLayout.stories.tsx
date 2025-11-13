82cdcb87e974fd0b700bf5ab9bbfa940c807e896
Nenad Ignjatović <nenad.ignjatovic@swissmarketplace.group>
2025-11-13 08:52:24 +0100

    feat: app layout migration (#1614)
    * feat: component migrated
    
    * chore: code improvement

--- src/components/layout/app/AppLayout.stories.tsx
+++ src/components/layout/app/AppLayout.stories.tsx
@@ -2,56 +2,56 @@ import React from 'react';
 import { Meta, StoryObj } from '@storybook/react';
 import { Description, Stories, Subtitle, Title } from '@storybook/blocks';
 
-import Center from 'src/components/center';
-import Box from 'src/components/box';
+import { Center } from 'src/components/center';
+import { Box } from 'src/components/box';
 
-import AppLayoutHeader from './Header';
-import AppLayoutFooter from './Footer';
-import AppLayoutContent from './Content';
-import AppLayout from './AppLayout';
+import { AppLayoutHeader } from './Header';
+import { AppLayoutFooter } from './Footer';
+import { AppLayoutContent } from './Content';
+import { AppLayout } from './AppLayout';
 
 const meta: Meta<typeof AppLayout> = {
   title: 'Layout/App',
   component: AppLayout,
 
-  args: {
-    children: [
+  render: () => (
+    <AppLayout>
       <AppLayoutHeader key="header">
         <Box
-          height="70px"
+          height="xl"
           width="full"
           padding="2xl"
           fontWeight="bold"
-          backgroundColor="deeppink"
+          backgroundColor="purple.400"
         >
           <Center>Dummy Header</Center>
         </Box>
-      </AppLayoutHeader>,
+      </AppLayoutHeader>
       <AppLayoutContent key="content">
         <Box
-          height="70px"
+          height="xl"
           width="full"
           padding="2xl"
           fontWeight="bold"
           minHeight="full"
-          backgroundColor="lightgrey"
+          backgroundColor="gray.200"
         >
           <Center>Dummy Content</Center>
         </Box>
-      </AppLayoutContent>,
+      </AppLayoutContent>
       <AppLayoutFooter key="footer">
         <Box
-          height="70px"
+          height="xl"
           width="full"
           padding="2xl"
           fontWeight="bold"
-          backgroundColor="purple"
+          backgroundColor="purple.700"
         >
           <Center>Dummy Footer</Center>
         </Box>
-      </AppLayoutFooter>,
-    ],
-  },
+      </AppLayoutFooter>
+    </AppLayout>
+  ),
 
   parameters: {
     layout: 'fullscreen',
