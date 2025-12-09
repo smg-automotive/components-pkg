fb0ea7b8e8343c75dfa3e763afc881b89ebc8437
Nenad Rosuljas <nenad.rosuljas@swissmarketplace.group>
2025-12-09 15:49:45 +0100

    fix: remove v2 component

--- src-v2/components/tab/TabPanels.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FC, PropsWithChildren } from 'react';
-import { TabPanels as ChakraTabPanels } from '@chakra-ui/react';
-
-const TabPanels: FC<PropsWithChildren> = (props) => {
-  return <ChakraTabPanels>{props.children}</ChakraTabPanels>;
-};
-
-export default TabPanels;
