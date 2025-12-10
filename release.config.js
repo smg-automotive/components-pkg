dcb89d65c5b1e01eb0bb36d04bad7fdede1c594b
Sibin Sedlan <82575074+SombreShinobi@users.noreply.github.com>
2025-12-10 15:24:20 +0100

    chore: drop SHA from pre-release channel (#1662)
    chore(deps): drop SHA from pre-release channel

--- release.config.js
+++ release.config.js
@@ -4,15 +4,15 @@ module.exports = {
     '+([0-9])?(.{+([0-9]),x}).x',
     {
       name: '!(+([0-9])?(.{+([0-9]),x}).x|main)',
-      prerelease: `$\{ name }-${process.env.CIRCLE_SHA1}`,
+      prerelease: `$\{ name }`,
     },
     {
       name: 'renovate/ui-dependencies',
-      prerelease: `ui-dependencies-${process.env.CIRCLE_SHA1}`,
+      prerelease: `ui-dependencies`,
     },
     {
       name: 'renovate/chakra-ui',
-      prerelease: `chakra-ui-${process.env.CIRCLE_SHA1}`,
+      prerelease: `chakra-ui`,
     },
   ],
 };
