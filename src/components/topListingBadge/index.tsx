48be37e3d3674cd65e24337213041201ef194624
“ilay149” <ilay.bicer@gmail.com>
2025-12-10 10:31:56 +0100

    fix: remove old v2 files and fix <Badge> Component styling

--- src/components/topListingBadge/index.tsx
+++ src/components/topListingBadge/index.tsx
@@ -1,43 +1,45 @@
-import React, { FC, PropsWithChildren } from 'react';
-import { Badge, Box } from '@chakra-ui/react';
+import React, {FC, PropsWithChildren} from 'react';
+import {Badge, Grid, GridItem, AspectRatio} from '@chakra-ui/react';
 
-import { Grid, GridItem } from '../grid';
 
 type Props = {
-  aspectRatio?: number;
+    aspectRatio?: number;
 };
 
 export const TopListingBadge: FC<PropsWithChildren<Props>> = ({
-  children,
-  aspectRatio,
-}) => {
-  return (
-    <Grid>
-      <GridItem gridColumn={1} gridRow={1}>
-        {aspectRatio ? <Box css={{ aspectRatio }}>{children}</Box> : children}
-      </GridItem>
-
-      <GridItem
-        gridColumn={1}
-        gridRow={1}
-        zIndex="docked"
-        overflow="hidden"
-        position="relative"
-        pointerEvents="none"
-        css={{ touchAction: 'none' }}
-      >
-        <Badge
-          transform="rotate(-45deg) translate(-50%) translateY(9px)"
-          position="absolute"
-          transformOrigin="top left"
-          width="4xl"
-          textAlign="center"
-          paddingLeft="sm"
-          color="gray.900"
-        >
-          Top
-        </Badge>
-      </GridItem>
-    </Grid>
-  );
+                                                                  children,
+                                                                  aspectRatio,
+                                                              }) => {
+    return (
+        <Grid>
+            <GridItem gridColumn={1} gridRow={1}>
+                {aspectRatio ? (
+                    <AspectRatio ratio={aspectRatio}>{children}</AspectRatio>
+                ) : (
+                    children
+                )}
+            </GridItem>
+            <GridItem
+                gridColumn={1}
+                gridRow={1}
+                zIndex="docked"
+                overflow="hidden"
+                position="relative"
+                pointerEvents="none"
+                css={{touchAction: 'none'}}
+            >
+                <Badge
+                    transform="rotate(-45deg) translate(-50%) translateY(9px)"
+                    position="absolute"
+                    transformOrigin="top left"
+                    width="4xl"
+                    textAlign="center"
+                    paddingLeft="sm"
+                    color="gray.900"
+                >
+                    Top
+                </Badge>
+            </GridItem>
+        </Grid>
+    );
 };
