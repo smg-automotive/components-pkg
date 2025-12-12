68da2a1f52d10f6f330f0ce91a98acea9f8ee3f8
Dusica Gajin <dusicagajin@swissmarketplace.group>
2025-12-12 16:09:47 +0100

    chore: stabilize v3 NumberInput sync and per-field debounce

--- src/components/rangeSlider/index.tsx
+++ src/components/rangeSlider/index.tsx
@@ -1,10 +1,14 @@
+'use client';
+
 import React from 'react';
 import { Slider } from '@chakra-ui/react';
 
-export type RangeSliderProps = {
-  min?: number;
-  max?: number;
-  defaultValue?: [number, number];
+type SliderRootProps = Omit<
+  React.ComponentProps<typeof Slider.Root>,
+  'onChange'
+>;
+
+export type RangeSliderProps = SliderRootProps & {
   onChange?: (values: number[]) => void;
   onChangeEnd?: (values: number[]) => void;
 };
