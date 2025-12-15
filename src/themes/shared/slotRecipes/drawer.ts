a51211d51e4714cc85369c7164808d4e6f3e2230
Nenad Ignjatović <nenad.ignjatovic@swissmarketplace.group>
2025-12-15 08:29:49 +0100

    feat: drawer migration to v3 (#1663)

---
+++ src/themes/shared/slotRecipes/drawer.ts
@@ -0,0 +1,244 @@
+import { defineSlotRecipe } from '@chakra-ui/react';
+
+export const drawerRecipe = defineSlotRecipe({
+  slots: ['root', 'backdrop', 'positioner', 'content', 'body', 'closeTrigger'],
+  className: 'chakra-drawer',
+  base: {
+    root: {},
+    backdrop: {
+      position: 'fixed',
+      insetInlineStart: '0',
+      top: '0',
+      width: 'full',
+      height: 'full',
+      zIndex: 'overlay',
+      _open: {
+        animation: 'fade-in',
+      },
+      _closed: {
+        animation: 'fade-out',
+      },
+    },
+    positioner: {
+      display: 'flex',
+      width: 'full',
+      height: 'full',
+      position: 'fixed',
+      insetInlineStart: '0',
+      top: '0',
+      zIndex: 'modal',
+    },
+    content: {
+      bg: 'white',
+      boxShadow: 'xs',
+    },
+    body: {
+      flex: 1,
+      padding: 'lg',
+      overflowY: 'auto',
+    },
+    closeTrigger: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: 'xs',
+      height: 'xs',
+      borderRadius: 'sm',
+      color: 'gray.800',
+      fontSize: 'base',
+      cursor: 'pointer',
+      _hover: {
+        bg: 'gray.100',
+      },
+    },
+  },
+  variants: {
+    placement: {
+      top: {
+        positioner: {
+          alignItems: 'flex-start',
+          justifyContent: 'stretch',
+        },
+      },
+      bottom: {
+        positioner: {
+          alignItems: 'flex-end',
+          justifyContent: 'stretch',
+        },
+      },
+      left: {
+        positioner: {
+          alignItems: 'stretch',
+          justifyContent: 'flex-start',
+        },
+      },
+      right: {
+        positioner: {
+          alignItems: 'stretch',
+          justifyContent: 'flex-end',
+        },
+      },
+    },
+    size: {
+      xl: {
+        content: {
+          width: '4xl',
+        },
+      },
+      half: {
+        content: {
+          width: 'half',
+        },
+      },
+      full: {
+        content: {
+          width: 'full',
+        },
+      },
+    },
+  },
+  compoundVariants: [
+    {
+      placement: 'top',
+      size: 'xl',
+      css: {
+        content: {
+          width: 'full',
+          height: '2xl',
+          _open: {
+            animationName: 'slide-in-from-top',
+            animationDuration: 'slow',
+          },
+          _closed: {
+            animationName: 'slide-out-to-top',
+            animationDuration: 'fast',
+          },
+        },
+      },
+    },
+    {
+      placement: 'top',
+      size: 'half',
+      css: {
+        content: {
+          width: 'full',
+          height: 'half',
+          _open: {
+            animationName: 'slide-in-from-top',
+            animationDuration: 'slow',
+          },
+          _closed: {
+            animationName: 'slide-out-to-top',
+            animationDuration: 'fast',
+          },
+        },
+      },
+    },
+    {
+      placement: 'top',
+      size: 'full',
+      css: {
+        content: {
+          width: 'full',
+          height: 'full',
+          _open: {
+            animationName: 'slide-in-from-top',
+            animationDuration: 'slow',
+          },
+          _closed: {
+            animationName: 'slide-out-to-top',
+            animationDuration: 'fast',
+          },
+        },
+      },
+    },
+    {
+      placement: 'right',
+      css: {
+        content: {
+          _open: {
+            animationName: 'slide-in-from-right',
+            animationDuration: 'slow',
+          },
+          _closed: {
+            animationName: 'slide-out-to-right',
+            animationDuration: 'fast',
+          },
+        },
+      },
+    },
+    {
+      placement: 'bottom',
+      size: 'xl',
+      css: {
+        content: {
+          width: 'full',
+          height: '2xl',
+          _open: {
+            animationName: 'slide-in-from-bottom',
+            animationDuration: 'slow',
+          },
+          _closed: {
+            animationName: 'slide-out-to-bottom',
+            animationDuration: 'fast',
+          },
+        },
+      },
+    },
+    {
+      placement: 'bottom',
+      size: 'half',
+      css: {
+        content: {
+          width: 'full',
+          height: 'half',
+          _open: {
+            animationName: 'slide-in-from-bottom',
+            animationDuration: 'slow',
+          },
+          _closed: {
+            animationName: 'slide-out-to-bottom',
+            animationDuration: 'fast',
+          },
+        },
+      },
+    },
+    {
+      placement: 'bottom',
+      size: 'full',
+      css: {
+        content: {
+          width: 'full',
+          height: 'full',
+          _open: {
+            animationName: 'slide-in-from-bottom',
+            animationDuration: 'slow',
+          },
+          _closed: {
+            animationName: 'slide-out-to-bottom',
+            animationDuration: 'fast',
+          },
+        },
+      },
+    },
+    {
+      placement: 'left',
+      css: {
+        content: {
+          _open: {
+            animationName: 'slide-in-from-left',
+            animationDuration: 'slow',
+          },
+          _closed: {
+            animationName: 'slide-out-to-left',
+            animationDuration: 'fast',
+          },
+        },
+      },
+    },
+  ],
+  defaultVariants: {
+    placement: 'right',
+    size: 'half',
+  },
+});
--- src/themes/shared/tokens/animations.ts
+++ src/themes/shared/tokens/animations.ts
@@ -3,5 +3,6 @@ import { defineTokens } from '@chakra-ui/react';
 export const animations = defineTokens.animations({
   skeletonPulse: { value: 'skeletonPulse 1.6s linear infinite' },
   spin: { value: 'spin 0.8s linear infinite' },
-  'fade-in': { value: 'fade-in var(--fade-in-duration, 0.1s) ease-out' },
+  'fade-in': { value: 'fade-in var(--fade-in-duration, 0.1s) ease-in' },
+  'fade-out': { value: 'fade-out var(--fade-out-duration, 0.1s) ease-out' },
 });
