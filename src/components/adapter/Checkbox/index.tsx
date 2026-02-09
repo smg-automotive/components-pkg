325b6b8e4d28ba55a934a90da5f2216f1f5c46cc
Milos Zbiljic <milos.zbiljic@swissmarketplace.group>
2026-02-09 11:31:27 +0100

    feat: checkbox adapter

--- src/components/adapter/Checkbox/index.tsx
+++ src/components/adapter/Checkbox/index.tsx
@@ -1,18 +1,42 @@
 import React, { FC } from 'react';
+import { CheckboxCheckedChangeDetails } from '@chakra-ui/react';
 
 import {
   CheckboxProps,
   Checkbox as ComponentsCheckbox,
 } from 'src/components/checkbox';
 
-type Props = Omit<CheckboxProps, 'checked' | 'disabled'> & {
+type Props = Omit<CheckboxProps, 'checked' | 'disabled' | 'onChange'> & {
   isChecked?: boolean;
   isDisabled?: boolean;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
 export const Checkbox: FC<Props> = (props) => {
-  const { isChecked, isDisabled, ...rest } = props;
+  const { isChecked, isDisabled, onChange, ...rest } = props;
+
+  const handleChange = (details: CheckboxCheckedChangeDetails) => {
+    if (!onChange) return;
+
+    const input = details.checked;
+    const target = {
+      checked: input,
+    } as HTMLInputElement;
+
+    const syntheticEvent = {
+      target,
+      currentTarget: target,
+    } as React.ChangeEvent<HTMLInputElement>;
+
+    onChange(syntheticEvent);
+  };
+
   return (
-    <ComponentsCheckbox {...rest} checked={isChecked} disabled={isDisabled} />
+    <ComponentsCheckbox
+      {...rest}
+      checked={isChecked}
+      disabled={isDisabled}
+      onChange={handleChange}
+    />
   );
 };
--- src/components/adapter/Text/index.tsx
+++ src/components/adapter/Text/index.tsx
@@ -2,12 +2,18 @@ import React, { FC, PropsWithChildren } from 'react';
 
 import { Text as TextComponents, TextProps } from 'src/components/text';
 
-type Props = Omit<TextProps, 'truncated' | 'color'> & {
+type Props = Omit<TextProps, 'truncated'> & {
   isTruncated?: boolean;
   textColor?: TextProps['color'];
 };
 
 export const Text: FC<PropsWithChildren<Props>> = (props) => {
   const { isTruncated, textColor, ...rest } = props;
-  return <TextComponents {...rest} truncate={isTruncated} color={textColor} />;
+  return (
+    <TextComponents
+      {...rest}
+      truncate={isTruncated}
+      {...(textColor ? { color: textColor } : {})}
+    />
+  );
 };
