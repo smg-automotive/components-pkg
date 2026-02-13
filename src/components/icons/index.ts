1833c5846a06c03ed497b2c96cfed8cc70e8802c
nikola-suvajcevic-smg <nikola.suvajcevic@swissmarketplace.group>
2026-02-13 14:55:59 +0100

    fix: remove New Badge SVG

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -68,7 +68,6 @@ export { MessageIcon } from './MessageIcon';
 export { MissingImageIcon } from './MissingImageIcon';
 export { MotorcycleIcon } from './MotorcycleIcon';
 export { MotoScout24AppLogo } from './MotoScout24AppLogo';
-export { NewBadgeIcon } from './NewBadgeIcon';
 export { PdfIcon } from './PdfIcon';
 export { PenIcon } from './PenIcon';
 export { PlusIcon } from './PlusIcon';
