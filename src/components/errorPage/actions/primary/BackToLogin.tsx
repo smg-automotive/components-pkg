bced3784b35baed0d010cac638207f6f7cdc094b
talamcol <colin@talamona.ch>
2026-03-18 10:25:14 +0100

    fix: defaults on error pages

--- src/components/errorPage/actions/primary/BackToLogin.tsx
+++ src/components/errorPage/actions/primary/BackToLogin.tsx
@@ -5,10 +5,9 @@ import Button from 'src/components/button';
 import { ActionButtonInterface } from '../interface';
 
 const BackToLogin: FC<ActionButtonInterface> = ({ t, language = 'de' }) => {
-  const languageToUse = language === 'en' ? 'de' : language;
   const logoutLink = `/seller-web/api/auth/logout`;
-  const vehicleManagementLink = `/${languageToUse}/vehicle-management`;
-  const loginLink = `/seller-web/api/auth/login?locale=${languageToUse}&returnTo=${encodeURIComponent(vehicleManagementLink)}`;
+  const vehicleManagementLink = `/${language}/vehicle-management`;
+  const loginLink = `/seller-web/api/auth/login?locale=${language}&returnTo=${encodeURIComponent(vehicleManagementLink)}`;
   const query = `?returnTo=${encodeURIComponent(loginLink)}`;
   const logoutBeforeLoginLink = `${logoutLink}${query}`;
 
