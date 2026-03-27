6760cedb47070c39c72be4474d89fdaeab99c6a5
Radoš Tošković <rados.toskovic@swissmarketplace.group>
2026-03-27 13:19:10 +0100

    fix: extend input invalid state adapter

--- src/components/adapter/Input/index.tsx
+++ src/components/adapter/Input/index.tsx
@@ -3,9 +3,10 @@ import React, { forwardRef } from 'react';
 import { Input as InputComponents, InputProps } from 'src/components/input';
 
 type AdapterType<T> = T extends unknown
-  ? Omit<T, 'disabled' | 'readOnly'> & {
+  ? Omit<T, 'disabled' | 'readOnly' | 'invalid'> & {
       isDisabled?: boolean;
       isReadOnly?: boolean;
+      isInvalid?: boolean;
       textColor?: InputProps['color'];
     }
   : never;
@@ -13,12 +14,13 @@ type AdapterType<T> = T extends unknown
 type Props = AdapterType<InputProps>;
 
 export const Input = forwardRef<HTMLInputElement, Props>((props, ref) => {
-  const { isDisabled, isReadOnly, textColor, ...rest } = props;
+  const { isDisabled, isReadOnly, isInvalid, textColor, ...rest } = props;
 
   return (
     <InputComponents
       color={textColor}
       {...(isReadOnly ? { readOnly: true } : {})}
+      {...(isInvalid ? { invalid: true } : {})}
       disabled={isDisabled}
       ref={ref}
       {...rest}
