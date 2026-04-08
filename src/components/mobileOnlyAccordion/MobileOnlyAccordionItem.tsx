5b91859ef64b1dbd7a170d78bde434f02bb83a25
damirzecevic <damir.zecevic@swissmarketplace.group>
2026-04-08 15:14:58 +0200

    fix: Refactor MobileOnlyAccordionItem and Footer Sections to replace useMediaQuery with responsive props for cleaner implementation

--- src/components/mobileOnlyAccordion/MobileOnlyAccordionItem.tsx
+++ src/components/mobileOnlyAccordion/MobileOnlyAccordionItem.tsx
@@ -1,17 +1,18 @@
 import React, { FC, PropsWithChildren } from 'react';
 
-import useMediaQuery from 'src/hooks/useMediaQuery';
-
 import { AccordionItem, AccordionItemProps } from '../accordion/AccordionItem';
 
 export const MobileOnlyAccordionItem: FC<
   PropsWithChildren<AccordionItemProps>
 > = (props) => {
   const { children, style, ...rest } = props;
-  const isAboveMd = useMediaQuery({ above: 'md' });
 
   return (
-    <AccordionItem style={isAboveMd ? { border: 'none' } : style} {...rest}>
+    <AccordionItem
+      style={style}
+      border={{ base: undefined, md: 'none' }}
+      {...rest}
+    >
       {children}
     </AccordionItem>
   );
